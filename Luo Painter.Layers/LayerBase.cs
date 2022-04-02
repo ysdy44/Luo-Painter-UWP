be11d103954490e109964855b7577b1fc825588a
ysdy44 <ysdy44@outlook.com>
2022-04-03 01:01:32 +0800

    Add LayerBase

---
+++ Luo Painter.Layers/LayerBase.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.ComponentModel;
+using Windows.UI.Xaml;
+
+namespace Luo_Painter.Layers
+{
+    public abstract class LayerBase
+    {
+
+        public string Id { get; } = Guid.NewGuid().ToString();
+
+        public string Name
+        {
+            get => this.title;
+            set
+            {
+                this.title = value;
+                this.OnPropertyChanged(nameof(Name)); // Notify 
+            }
+        }
+        private string title;
+
+        public Visibility Visibility
+        {
+            get => this.visibility;
+            set
+            {
+                this.visibility = value;
+                this.OnPropertyChanged(nameof(Visibility)); // Notify 
+            }
+        }
+        private Visibility visibility;
+
+        public bool IsSelected
+        {
+            get => this.isSelected;
+            set
+            {
+                this.isSelected = value;
+                this.OnPropertyChanged(nameof(IsSelected)); // Notify 
+            }
+        }
+        private bool isSelected;
+
+
+        //@Notify 
+        /// <summary> Multicast event for property change notifications. </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+        /// <summary>
+        /// Notifies listeners that a property value has changed.
+        /// </summary>
+        /// <param name="propertyName"> Name of the property used to notify listeners. </param>
+        protected void OnPropertyChanged(string propertyName) => this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
