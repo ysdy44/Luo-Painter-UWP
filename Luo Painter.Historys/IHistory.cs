ca3d5a3a731df9498dad470b3b4077fefd05c590
ysdy44 <ysdy44@outlook.com>
2022-04-03 00:10:17 +0800

    Add IHistory

---
+++ Luo Painter.Historys/IHistory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Luo_Painter.Historys
+{
+    public interface IHistory: IDisposable
+    {
+        HistoryType Type { get; }
+        object UndoParameter { get; }
+        object RedoParameter { get; }
+    }
+}
