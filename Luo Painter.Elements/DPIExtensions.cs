9ff3e9ac8533cce60176b0289209cd3843407d8c
ysdy44 <ysdy44@outlook.com>
2022-04-03 00:00:53 +0800

    Add DPIExtensions

---
+++ Luo Painter.Elements/DPIExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Numerics;
+
+namespace Luo_Painter.Elements
+{
+    public static class DPIExtensions
+    {
+        public static float ConvertDipsToPixels(this float dpi, float vector) => vector * dpi / 96;
+        public static Vector2 ConvertDipsToPixels(this float dpi, Vector2 vector) => vector * dpi / 96;
+        public static Matrix3x2 ConvertDipsToPixels(this float dpi) => Matrix3x2.CreateScale(dpi / 96);
+        
+        public static float ConvertPixelsToDips(this float dpi, float vector) => vector / dpi * 96;
+        public static Vector2 ConvertPixelsToDips(this float dpi, Vector2 vector) => vector / dpi * 96;
+        public static Matrix3x2 ConvertPixelsToDips(this float dpi) => Matrix3x2.CreateScale(1 / dpi * 96);
+    }
+}
