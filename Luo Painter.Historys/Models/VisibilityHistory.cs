5672d8dcd6240fb7ff6bd48a968c2798df98c9fc
ysdy44 <ysdy44@outlook.com>
2022-04-03 00:59:06 +0800

    Add VisibilityHistory

---
+++ Luo Painter.Historys/Models/VisibilityHistory.cs
@@ -0,0 +1,26 @@
+﻿using Windows.UI.Xaml;
+
+namespace Luo_Painter.Historys.Models
+{
+    public class VisibilityHistory : IHistory
+    {
+        public HistoryType Type => HistoryType.Visibility;
+        public string Id => this.IdCore;
+        public object UndoParameter => this.UndoParameterCore;
+        public object RedoParameter => this.RedoParameterCore;
+
+        readonly string IdCore;
+        readonly Visibility UndoParameterCore;
+        readonly Visibility RedoParameterCore;
+
+        public VisibilityHistory(string id, Visibility undo, Visibility redo)
+        {
+            this.IdCore = id;
+            this.UndoParameterCore = undo;
+            this.RedoParameterCore = redo;
+        }
+        public void Dispose()
+        {
+        }
+    }
+}
