11908e28c5e0ead9cae72d55db55831c3b9d7d13
ysdy44 <ysdy44@outlook.com>
2022-04-03 00:05:00 +0800

    Add ShaderExtensions

---
+++ Luo Painter.Shaders/ShaderExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Streams;
+
+namespace Luo_Painter.Shaders
+{
+    public static class ShaderExtensions
+    {
+
+        public static async Task<byte[]> LoadAsync(this ShaderType type)
+        {
+            StorageFile file = await StorageFile.GetFileFromApplicationUriAsync(new Uri(type.GetPath()));
+            IBuffer buffer = await FileIO.ReadBufferAsync(file);
+            return buffer.ToArray();
+        }
+
+        private static string GetPath(this ShaderType type)
+        {
+            switch (type)
+            {
+                case ShaderType.Liquefaction: return "ms-appx:///Luo Painter.Shaders/Liquefaction.bin";
+                default: return null;
+            }
+        }
+
+    }
+}
