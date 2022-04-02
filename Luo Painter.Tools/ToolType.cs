f2d5a1516209cd126f582a9680316275deda59ec
ysdy44 <ysdy44@outlook.com>
2022-04-03 01:04:45 +0800

    Add ToolType

---
+++ Luo Painter.Tools/ToolType.cs
@@ -0,0 +1,58 @@
+﻿namespace Luo_Painter.Tools
+{
+    public enum ToolType
+    {
+        None,
+
+        Cursor,
+        View,
+        Crop,
+
+        Brush,
+        Transparency,
+
+        Image,
+
+        Node,
+        Pen,
+
+        TextArtistic,
+        TextFrame,
+
+        MarqueeRectangular,
+        MarqueeElliptical,
+        MarqueePolygon,
+        MarqueeFreeHand,
+        MarqueeSelectionBrush,
+        MarqueeFloodSetect,
+
+        // Geometry0
+        GeometryRectangle,
+        GeometryEllipse,
+        // Geometry1
+        GeometryRoundRect,
+        GeometryTriangle,
+        GeometryDiamond,
+        // Geometry2
+        GeometryPentagon,
+        GeometryStar,
+        GeometryCog,
+        // Geometry3
+        GeometryDount,
+        GeometryPie,
+        GeometryCookie,
+        // Geometry4
+        GeometryArrow,
+        GeometryCapsule,
+        GeometryHeart,
+
+        PatternGrid,
+        PatternDiagonal,
+        PatternSpotted,
+
+        PaintBrush,
+        PaintWatercolorPen,
+        PaintPencil,
+        PaintEraseBrush,
+    }
+}
