dc16433fe11245f89eef5da8d6154c475a74c032
ysdy44 <ysdy44@outlook.com>
2022-04-03 02:09:08 +0800

    Add RegionsInvalidatedPage

---
+++ Luo Painter.TestApp/RegionsInvalidatedPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using Luo_Painter.Elements;
+using Microsoft.Graphics.Canvas;
+using System.Numerics;
+using Windows.Foundation;
+using Windows.UI;
+using Windows.UI.Xaml.Controls;
+
+namespace Luo_Painter.TestApp
+{
+    public sealed partial class RegionsInvalidatedPage : Page
+    {
+
+        Vector2 Position;
+
+        public RegionsInvalidatedPage()
+        {
+            this.InitializeComponent();
+            this.ConstructCanvas();
+            this.ConstructOperator();
+        }
+
+        private void ConstructCanvas()
+        {
+            this.CanvasControl.SizeChanged += (s, e) =>
+            {
+                if (e.NewSize == Size.Empty) return;
+                if (e.NewSize == e.PreviousSize) return;
+            };
+            this.CanvasControl.RegionsInvalidated += (sender, args) =>
+            {
+                foreach (Rect region in args.InvalidatedRegions)
+                {
+                    using (CanvasDrawingSession ds = sender.CreateDrawingSession(region))
+                    {
+                        ds.DrawRectangle(region, Colors.Red);
+                    }
+                }
+            };
+        }
+
+        private void ConstructOperator()
+        {
+            // Single
+            this.Operator.Single_Start += (point, properties) =>
+            {
+                this.Position = point;
+                this.CanvasControl.Invalidate(); // Invalidate
+            };
+            this.Operator.Single_Delta += (point, properties) =>
+            {
+                Rect region = RectExtensions.GetRect(point, this.Position, 10);
+                this.Position = point;
+
+                if (this.CanvasControl.Size.TryIntersect(ref region))
+                {
+                    this.CanvasControl.Invalidate(region); // Invalidate
+                }
+            };
+            this.Operator.Single_Complete += (point, properties) =>
+            {
+                this.CanvasControl.Invalidate(); // Invalidate
+            };
+        }
+
+    }
+}
