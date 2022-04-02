a50508bfae7721a2da9cc29eb14f78bddf260927
ysdy44 <ysdy44@outlook.com>
2022-04-03 02:10:17 +0800

    Add ScrollerPage.xaml

---
+++ Luo Painter.TestApp/ScrollerPage.xaml.cs
@@ -0,0 +1,126 @@
+﻿using FanKit.Transformers;
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.Storage.Provider;
+using Windows.Storage.Streams;
+using Windows.UI;
+using Windows.UI.Core;
+using Windows.UI.Input.Inking;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Luo_Painter.TestApp
+{
+    public sealed partial class ScrollerPage : Page
+    {
+
+        public ScrollerPage()
+        {
+            this.InitializeComponent();
+
+            this.InkCanvas.InkPresenter.InputDeviceTypes = (CoreInputDeviceTypes)7;
+            this.InkCanvas.InkPresenter.UpdateDefaultDrawingAttributes(new InkDrawingAttributes
+            {
+                Color = Colors.Red,
+            });
+
+            this.Thumb.DragStarted += async (s, e) =>
+            {
+                await this.Scroller.RenderAsync(this.InkCanvas);
+                this.Scroller.DragStarted();
+                this.InkCanvas.Visibility = Visibility.Collapsed;
+            };
+            this.Thumb.DragDelta += (s, e) => this.Scroller.DragDelta(e.HorizontalChange, e.VerticalChange);
+            this.Thumb.DragCompleted += (s, e) => this.Scroller.DragCompleted();
+
+            this.Scroller.DragPageDownCompleted += (s, e) => this.InkCanvas.Visibility = Visibility.Visible;
+            this.Scroller.DragPageUpCompleted += (s, e) =>
+            {
+                this.InkCanvas.InkPresenter.StrokeContainer.Clear();
+                this.InkCanvas.Visibility = Visibility.Visible;
+            };
+        }
+
+        public async Task<FileUpdateStatus?> Save()
+        {
+            // Get all strokes on the InkCanvas.
+            IReadOnlyList<InkStroke> currentStrokes = InkCanvas.InkPresenter.StrokeContainer.GetStrokes();
+
+            // Strokes present on ink canvas.
+            if (currentStrokes.Count == 0) return null;
+
+            // Let users choose their ink file using a file picker.
+            // Initialize the picker.
+            FileSavePicker savePicker = new FileSavePicker
+            {
+                DefaultFileExtension = ".one",
+                SuggestedFileName = "InkSample",
+                SuggestedStartLocation = PickerLocationId.DocumentsLibrary,
+                FileTypeChoices =
+                {
+                    ["OneNote"] = new List<string>
+                    {
+                        ".one"
+                    }
+                }
+            };
+
+            // Show the file picker.
+            StorageFile file = await savePicker.PickSaveFileAsync();
+            // When chosen, picker returns a reference to the selected file.
+            if (file == null) return null;
+
+            // Prevent updates to the file until updates are 
+            // finalized with call to CompleteUpdatesAsync.
+            CachedFileManager.DeferUpdates(file);
+
+            // Open a file stream for writing.
+            // Write the ink strokes to the output stream.
+            using (IRandomAccessStream stream = await file.OpenAsync(FileAccessMode.ReadWrite))
+            using (IOutputStream outputStream = stream.GetOutputStreamAt(0))
+            {
+                await this.InkCanvas.InkPresenter.StrokeContainer.SaveAsync(outputStream);
+                await outputStream.FlushAsync();
+            }
+
+            // Finalize write so other apps can update file.
+            return await CachedFileManager.CompleteUpdatesAsync(file);
+        }
+
+        public static NodeCollection CreateNodeCollection(IEnumerable<InkStroke> inkStrokes)
+        {
+            NodeCollection nodes = new NodeCollection();
+            bool isBegin = false;
+
+            foreach (InkStroke stroke in inkStrokes)
+            {
+                foreach (InkStrokeRenderingSegment segments in stroke.GetRenderingSegments())
+                {
+                    nodes.Add(new Node
+                    {
+                        Type = isBegin ? NodeType.Node : NodeType.BeginFigure,
+                        Point = segments.Position.ToVector2(),
+                        LeftControlPoint = segments.BezierControlPoint1.ToVector2(),
+                        RightControlPoint = segments.BezierControlPoint2.ToVector2(),
+                        IsSmooth = true,
+                        IsChecked = false
+                    });
+                    isBegin = true;
+                }
+
+                nodes.Add(new Node
+                {
+                    Type = NodeType.EndFigure
+                });
+                isBegin = false;
+            }
+
+            return nodes;
+        }
+
+    }
+}
