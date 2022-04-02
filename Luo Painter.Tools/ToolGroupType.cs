6bfd904f529d49473c31779abe49af03993bb5dc
ysdy44 <ysdy44@outlook.com>
2022-04-03 01:27:39 +0800

    Add ToolGroupType

---
+++ Luo Painter.Tools/ToolGroupType.cs
@@ -0,0 +1,19 @@
+﻿namespace Luo_Painter.Tools
+{
+    public enum ToolGroupType
+    {
+        Pixel,
+        Paint,
+
+        Liquefy,
+        Adjust,
+
+        Vector,
+        Geometry,
+        Curve,
+        Text,
+        ThreeDimensions,
+
+        Crop,
+    }
+}
