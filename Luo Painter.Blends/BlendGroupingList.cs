2b61959b36a17b0fbed8308589b7e02ef3cf34e9
ysdy44 <ysdy44@outlook.com>
2022-04-03 14:38:31 +0800

    Add BlendGroupingList

---
+++ Luo Painter.Blends/BlendGroupingList.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Graphics.Canvas.Effects;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Luo_Painter.Blends
+{
+    public sealed class BlendGroupingList : List<IBlendGrouping>, IList<IBlendGrouping> { }
+    public interface IBlendGrouping : IGrouping<BlendGroupType, BlendEffectMode>
+    {
+    }
+
+    public sealed class BlendGrouping : List<BlendEffectMode>, IList<BlendEffectMode>, IBlendGrouping
+    {
+        public BlendGroupType Key { set; get; }
+    }
+    public sealed class NoneBlendGrouping : IBlendGrouping
+    {
+        public BlendGroupType Key => BlendGroupType.None;
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+        public IEnumerator<BlendEffectMode> GetEnumerator()
+        {
+            yield return BlendExtensions.None;
+        }
+    }
+}
