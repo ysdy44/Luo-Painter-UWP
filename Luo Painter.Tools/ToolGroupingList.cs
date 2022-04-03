22ae9d70e82a91c5029a378cf4e745522f392086
ysdy44 <ysdy44@outlook.com>
2022-04-03 10:55:36 +0800

    Add ToolGroupingList

---
+++ Luo Painter.Tools/ToolGroupingList.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Luo_Painter.Tools
+{
+    public sealed class ToolGroupingList : List<ToolGrouping>, IList<ToolGrouping> { }
+    public sealed class ToolGrouping : List<ToolType>, IList<ToolType>, IGrouping<ToolGroupType, ToolType>
+    {
+        public ToolGroupType Key { set; get; }
+    }
+}
