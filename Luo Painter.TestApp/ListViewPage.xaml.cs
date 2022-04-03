d77107be221e77efca04e291f1251d87aaf2d547
ysdy44 <ysdy44@outlook.com>
2022-04-03 14:40:37 +0800

    Add ListViewPage

--- Luo Painter.TestApp/ListViewPage.xaml.cs
+++ Luo Painter.TestApp/ListViewPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Luo_Painter.Tools;
+﻿using Luo_Painter.Blends;
+using Luo_Painter.Tools;
+using Microsoft.Graphics.Canvas.Effects;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
@@ -47,6 +49,51 @@ namespace Luo_Painter.TestApp
                     Content = value,
                     // Style = App.Current.Resources["AppToolTipStyle"] as Style
                 });
+            }
+        }));
+
+
+        #endregion
+    }
+    internal sealed class BlendIcon : ListViewItem
+    {
+        #region DependencyProperty
+
+
+        /// <summary> Gets or set the SelectedItem for <see cref="BlendIcon"/>. </summary>
+        public BlendEffectMode Type
+        {
+            get => (BlendEffectMode)base.GetValue(TypeProperty);
+            set => base.SetValue(TypeProperty, value);
+        }
+        /// <summary> Identifies the <see cref = "BlendIcon.Type" /> dependency property. </summary>
+        public static readonly DependencyProperty TypeProperty = DependencyProperty.Register(nameof(Type), typeof(BlendEffectMode), typeof(BlendIcon), new PropertyMetadata(BlendEffectMode.Multiply, (sender, e) =>
+        {
+            BlendIcon control = (BlendIcon)sender;
+
+            if (e.NewValue is BlendEffectMode value)
+            {
+                control.Content = new StackPanel
+                {
+                    Orientation = Orientation.Horizontal,
+                    Children =
+                    {
+                        new ContentControl
+                        {
+                            Content = value,
+                            Template = value.GetTemplate(out ResourceDictionary resource, out string title),
+                            Resources = resource,
+                        },
+                        new ContentControl
+                        {
+                            Width = 12
+                        },
+                        new TextBlock
+                        {
+                            Text = title
+                        }
+                    }
+                };
             }
         }));
 
