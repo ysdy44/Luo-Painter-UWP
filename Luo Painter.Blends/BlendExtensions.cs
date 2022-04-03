825d72ef1e1cbb65f0a6282340bcc421c7ed0569
ysdy44 <ysdy44@outlook.com>
2022-04-03 14:38:05 +0800

    Add BlendExtensions

---
+++ Luo Painter.Blends/BlendExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Graphics.Canvas.Effects;
+using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Luo_Painter.Blends
+{
+    public static class BlendExtensions
+    {
+
+        public static BlendEffectMode None = (BlendEffectMode)(-1); // None
+        public static bool IsNone(this BlendEffectMode type) => (int)type == -1;
+
+        public static ControlTemplate GetTemplate(this BlendEffectMode type, out ResourceDictionary resource, out string title)
+        {
+            if (type.IsNone())
+            {
+                title = "None";
+                resource = new ResourceDictionary
+                {
+                    Source = new Uri($"ms-appx:///Luo Painter.Blends/Icons/NoneIcon.xaml")
+                };
+                return resource[$"NoneIcon"] as ControlTemplate;
+            }
+            else
+            {
+                title = $"{type}";
+                resource = new ResourceDictionary
+                {
+                    Source = new Uri($"ms-appx:///Luo Painter.Blends/Icons/{title}Icon.xaml")
+                };
+                return resource[$"{title}Icon"] as ControlTemplate;
+            }
+        }
+
+    }
+}
