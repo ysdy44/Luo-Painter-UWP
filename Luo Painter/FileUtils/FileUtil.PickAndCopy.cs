5d08847066407b5ed9834e8ebe48ae10eac0926a
ysdy44 <ysdy44@outlook.com>
2022-04-04 09:57:30 +0800

    Add FileUtils

---
+++ Luo Painter/FileUtils/FileUtil.PickAndCopy.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+
+namespace Luo_Painter
+{
+    public static partial class FileUtil
+    {
+
+        /// <summary>
+        /// Copy file to the temporary folder, and return the copy.
+        /// </summary>
+        /// <param name="file"> The destination file. </param>
+        /// <returns> The copied file. </returns>
+        public async static Task<StorageFile> CopySingleImageFileAsync(StorageFile file)
+        {
+            if (file == null) return null;
+
+            StorageFile copyFile = await file.CopyAsync(ApplicationData.Current.TemporaryFolder, file.Name, NameCollisionOption.ReplaceExisting);
+            return copyFile;
+        }
+
+        /// <summary>
+        /// The file picker is displayed so that the user can select a file.
+        /// </summary>
+        /// <param name="location"> The destination locationId. </param>
+        /// <returns> The product file. </returns>
+        public async static Task<StorageFile> PickSingleImageFileAsync(PickerLocationId location)
+        {
+            // Picker
+            FileOpenPicker openPicker = new FileOpenPicker
+            {
+                ViewMode = PickerViewMode.Thumbnail,
+                SuggestedStartLocation = location,
+                FileTypeFilter =
+                {
+                     ".jpg", ".jpeg", ".jpe",
+                     ".png",
+                     ".gif",
+                     ".bmp", ".dib",
+                     ".tif", ".tiff",
+                }
+            };
+
+            // File
+            StorageFile file = await openPicker.PickSingleFileAsync();
+            return file;
+        }
+
+        /// <summary>
+        /// The files picker is displayed so that the user can select a files.
+        /// </summary>
+        /// <param name="location"> The destination locationId. </param>
+        /// <returns> The product files. </returns>
+        public async static Task<IReadOnlyList<StorageFile>> PickMultipleImageFilesAsync(PickerLocationId location)
+        {
+            // Picker
+            FileOpenPicker openPicker = new FileOpenPicker
+            {
+                ViewMode = PickerViewMode.Thumbnail,
+                SuggestedStartLocation = location,
+                FileTypeFilter =
+                {
+                     ".jpg", ".jpeg", ".jpe",
+                     ".png",
+                     ".gif",
+                     ".bmp", ".dib",
+                     ".tif", ".tiff",
+                }
+            };
+
+            // File
+            IReadOnlyList<StorageFile> files = await openPicker.PickMultipleFilesAsync();
+            return files;
+        }
+
+
+        /// <summary>
+        /// Filter files that are not pictures.
+        /// Then copy to the temporary folder, and return the copy.
+        /// </summary>
+        /// <param name="item"> The destination item. </param>
+        /// <returns> The product file. </returns>
+        public async static Task<StorageFile> CopySingleImageFileAsync(IStorageItem item)
+        {
+            if (item == null) return null;
+
+            if (item is StorageFile file)
+            {
+                switch (file.FileType.ToLower())
+                {
+                    case ".jpg": case ".jpeg": case ".jpe":
+                    case ".png":
+                    case ".gif":
+                    case ".bmp": case ".dib":
+                    case ".tif": case ".tiff":
+                        StorageFile copyFile = await file.CopyAsync(ApplicationData.Current.TemporaryFolder, file.Name, NameCollisionOption.ReplaceExisting);
+                        return copyFile;
+                }
+            }
+            return null;
+        }
+
+    }
+}
---
+++ Luo Painter/FileUtils/FileUtil.Sample.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.Graphics.Canvas;
+using System;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.Storage.Streams;
+
+namespace Luo_Painter
+{
+    public static partial class FileUtil
+    {
+
+        /// <summary>
+        /// Saves the entire bitmap to the specified stream
+        /// with the specified file format and quality level.
+        /// </summary>
+        /// <param name="bitmap"> The bitmap.</param>
+        /// <param name="fileChoices"> The file choices. </param>
+        /// <param name="suggestedFileName"> The suggested name of file. </param>
+        /// <param name="fileFormat"> The file format. </param>
+        /// <param name="quality"> The file quality. </param>
+        /// <returns> Saved successful? </returns>
+        public static async Task<bool?> SaveCanvasBitmapFile(CanvasBitmap bitmap, string fileChoices = ".Jpeg", string suggestedFileName = "Untitled", CanvasBitmapFileFormat fileFormat = CanvasBitmapFileFormat.Jpeg, float quality = 1.0f)
+        {
+            // FileSavePicker
+            FileSavePicker savePicker = new FileSavePicker
+            {
+                SuggestedStartLocation = PickerLocationId.Desktop,
+                SuggestedFileName = suggestedFileName,
+                FileTypeChoices =
+                {
+                    {"DB", new[] { fileChoices } }
+                }
+            };
+
+
+            // PickSaveFileAsync
+            StorageFile file = await savePicker.PickSaveFileAsync();
+            if (file == null) return null;
+
+            try
+            {
+                using (IRandomAccessStream accessStream = await file.OpenAsync(FileAccessMode.ReadWrite))
+                {
+                    await bitmap.SaveAsync(accessStream, fileFormat, quality);
+                }
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Saves the entire image to the specified stream
+        /// with the specified file format and quality level.
+        /// </summary>
+        /// <param name="resourceCreator"> The resource-creator. </param>
+        /// <param name="image"> The image. </param>
+        /// <param name="bounds"> The bounds of image. </param>
+        /// <param name="fileChoices"> The file choices. </param>
+        /// <param name="suggestedFileName"> The suggested name of file. </param>
+        /// <param name="fileFormat"> The file format. </param>
+        /// <param name="quality"> The file quality. </param>
+        /// <param name="dpi"> The file dpi. </param>
+        /// <returns> Saved successful? </returns>
+        public static async Task<bool?> SaveCanvasImageFile(ICanvasResourceCreator resourceCreator, ICanvasImage image, Rect bounds, string fileChoices = ".Jpeg", string suggestedFileName = "Untitled", CanvasBitmapFileFormat fileFormat = CanvasBitmapFileFormat.Jpeg, float quality = 1.0f, float dpi = 96)
+        {
+            // FileSavePicker
+            FileSavePicker savePicker = new FileSavePicker
+            {
+                SuggestedStartLocation = PickerLocationId.Desktop,
+                SuggestedFileName = suggestedFileName,
+                FileTypeChoices =
+                {
+                    {"JPG", new[] { fileChoices } }
+                }
+            };
+
+
+            // PickSaveFileAsync
+            StorageFile file = await savePicker.PickSaveFileAsync();
+            if (file == null) return null;
+
+            try
+            {
+                using (IRandomAccessStream accessStream = await file.OpenAsync(FileAccessMode.ReadWrite))
+                {
+                    await CanvasImage.SaveAsync(image, bounds, dpi, resourceCreator, accessStream, fileFormat, quality);
+                }
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+    }
+}
