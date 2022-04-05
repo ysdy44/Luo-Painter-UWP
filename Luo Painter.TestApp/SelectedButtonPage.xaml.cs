fe1ce05c30784f6e69bf78501df44a3a18262c09
ysdy44 <ysdy44@outlook.com>
2022-04-05 19:50:00 +0800

    Add SelectedButtonPage

---
+++ Luo Painter.TestApp/SelectedButtonPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace Luo_Painter.TestApp
+{
+    public sealed partial class SelectedButtonPage : Page
+    {
+        public SelectedButtonPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
