9ed5b6b1e590d48f2bf4d2a9ece6edd34460b771
ysdy44 <ysdy44@outlook.com>
2022-04-12 02:26:29 +0800

    Add DrawPage.Option

---
+++ Luo Painter/$DrawPages/DrawPage.Option.cs
@@ -0,0 +1,13 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace Luo_Painter
+{
+    public sealed partial class DrawPage : Page
+    {
+
+        private void ConstructOption()
+        {
+        }
+
+    }
+}
