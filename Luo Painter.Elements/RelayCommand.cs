aa624cacee5a40476b773fab520d69718aa3e795
ysdy44 <ysdy44@outlook.com>
2022-04-11 21:22:19 +0800

    Add RelayCommand

---
+++ Luo Painter.Elements/RelayCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Luo_Painter.Elements
+{
+    /// <summary>
+    /// Relay Command is used to separate UI objects and code logic.
+    /// </summary>
+    public class RelayCommand<T> : ICommand
+    {
+        //@Delegate
+        /// <summary>
+        /// Occurs when clicked.
+        /// </summary>
+        public event EventHandler<T> Click;
+        /// <summary>
+        /// Occurs when there is a change that affects whether the command should be executed
+        /// </summary>
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter) => parameter != default;
+        public void Execute(object parameter)
+        {
+            if (parameter is T item)
+            {
+                this.Click?.Invoke(this, item);//Delegate
+            }
+        }
+    }
+}
