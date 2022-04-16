674e5d64705eedf311d682444213abecb970b635
ysdy44 <ysdy44@outlook.com>
2022-04-16 17:07:54 +0800

    Add ArrangeHistory

---
+++ Luo Painter.Historys/Models/ArrangeHistory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Luo_Painter.Historys.Models
+{
+    public class ArrangeHistory : IHistory
+    {
+        public HistoryMode Mode => HistoryMode.Arrange;
+        public HistoryType Type => HistoryType.None;
+        public string[] UndoParameter { get; }
+        public string[] RedoParameter { get; }
+        public ArrangeHistory(string[] undoParameter, string[] redoParameter)
+        {
+            this.UndoParameter = undoParameter;
+            this.RedoParameter = redoParameter;
+        }
+        public void Dispose()
+        {
+            Array.Clear(this.UndoParameter, 0, this.UndoParameter.Length);
+            Array.Clear(this.RedoParameter, 0, this.RedoParameter.Length);
+        }
+    }
+}
