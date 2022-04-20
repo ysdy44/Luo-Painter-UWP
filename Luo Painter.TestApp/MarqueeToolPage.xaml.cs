bb4a845c538ecfcfd01936997443ceede0d38659
ysdy44 <ysdy44@outlook.com>
2022-04-20 23:42:01 +0800

    New MarqueeToolPage

--- Luo Painter.TestApp/MarqueeToolPage.xaml.cs
+++ Luo Painter.TestApp/MarqueeToolPage.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using FanKit.Transformers;
+using Luo_Painter.Layers.Models;
 using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.Effects;
 using System.Numerics;
 using Windows.Foundation;
 using Windows.UI;
@@ -9,58 +11,65 @@ namespace Luo_Painter.TestApp
 {
     public sealed partial class MarqueeToolPage : Page
     {
-        DottedLineImage DottedLineImage;
-        DottedLineBrush DottedLineBrush;
 
-        Vector2 StartingPosition = new Vector2();
-        TransformerRect Rect;
+        readonly CanvasDevice Device = new CanvasDevice();
+        readonly MarqueeTool MarqueeTool = new MarqueeTool();
+
+        DottedLineBrush Brush;
+        BitmapLayer BitmapLayer;
+        CanvasRenderTarget Image;
+
+        Vector2 StartingPosition;
 
         public MarqueeToolPage()
         {
             this.InitializeComponent();
-            this.ConstructLiquefaction();
+            this.ConstructMarqueeTool();
             this.ConstructOperator();
             this.ConstructCanvas();
         }
 
-        public void ConstructLiquefaction()
+        public void ConstructMarqueeTool()
         {
             this.ResetButton.Tapped += (s, e) =>
             {
-                using (CanvasDrawingSession ds = this.DottedLineImage.CreateDrawingSession())
-                {
-                    ds.Clear(Colors.Transparent);
-                }
-                this.DottedLineImage.Baking(this.CanvasControl);
+                this.BitmapLayer.Clear(Colors.Transparent);
+                this.Baking();
             };
         }
-       
+
         public void ConstructCanvas()
         {
             //Canvas
-            this.CanvasControl.SizeChanged += (s, e) =>
-            {
-                if (e.NewSize == e.PreviousSize) return;
-            };
+            this.CanvasControl.UseSharedDevice = true;
+            this.CanvasControl.CustomDevice = this.Device;
             this.CanvasControl.CreateResources += (sender, args) =>
             {
-                CanvasRenderTarget canvasRenderTarget = new CanvasRenderTarget(sender, 512, 512);
-                this.DottedLineImage = new DottedLineImage(canvasRenderTarget);
-                this.DottedLineBrush = new DottedLineBrush(sender);
-
-                this.DottedLineImage.Baking(sender);
+                //@DPI
+                this.Brush = new DottedLineBrush(sender);
+                this.Image = new CanvasRenderTarget(sender, 512, 512, 96);
+                this.BitmapLayer = new BitmapLayer(sender, 512, 512);
             };
             this.CanvasControl.Draw += (sender, args) =>
             {
-                args.DrawingSession.DrawRectangle(0, 0, 512, 512, Colors.Red);
-                args.DrawingSession.DrawDottedLine(sender, this.DottedLineBrush, this.DottedLineImage, 512, 512);
-
-                Rect rect = this.Rect.ToRect();
-                args.DrawingSession.DrawThickRectangle(rect);
+                args.DrawingSession.DrawImage(this.Image);
+                args.DrawingSession.Blend = CanvasBlend.Min;
+                args.DrawingSession.FillRectangle(0, 0, (float)sender.Size.Width, (float)sender.Size.Height, this.Brush.Brush);
             };
             this.CanvasControl.Update += (sender, args) =>
             {
-                this.DottedLineBrush.Update();
+                this.Brush.Update();
+            };
+
+
+            this.ToolCanvasControl.UseSharedDevice = true;
+            this.ToolCanvasControl.CustomDevice = this.Device;
+            this.ToolCanvasControl.CreateResources += (sender, args) =>
+            {
+            };
+            this.ToolCanvasControl.Draw += (sender, args) =>
+            {
+                args.DrawingSession.DrawMarqueeTool(sender, MarqueeToolType.Rectangular, this.MarqueeTool);
             };
         }
 
@@ -70,23 +79,58 @@ namespace Luo_Painter.TestApp
             this.Operator.Single_Start += (point, properties) =>
             {
                 this.StartingPosition = point;
-                this.Rect = new TransformerRect(point, point);
+                this.MarqueeTool.Start(point, MarqueeToolType.Rectangular, false, false);
+
+                this.CanvasControl.Paused = true;
+                this.ToolCanvasControl.Invalidate(); // Invalidate
             };
             this.Operator.Single_Delta += (point, properties) =>
             {
-                this.Rect = new TransformerRect(StartingPosition, point);
+                this.MarqueeTool.Delta(this.StartingPosition, point, MarqueeToolType.Rectangular, false, false);
+                this.ToolCanvasControl.Invalidate(); // Invalidate
             };
             this.Operator.Single_Complete += (point, properties) =>
             {
-                using (var ds = this.DottedLineImage.CreateDrawingSession())
+                using (CanvasDrawingSession ds = this.BitmapLayer.CreateSourceDrawingSession())
                 {
-                    ds.FillRectangle(this.Rect.ToRect(), Windows.UI.Colors.Gray);
+                    ds.FillMarqueeMaskl(this.CanvasControl, MarqueeToolType.Rectangular, this.MarqueeTool, this.Image.Bounds, MarqueeCompositeMode.New);
                 }
-                this.DottedLineImage.Baking(this.CanvasControl);
+                this.Baking();
 
-                this.Rect = new TransformerRect(Vector2.Zero, Vector2.Zero);
+                this.MarqueeTool.Complete(this.StartingPosition, point, MarqueeToolType.Rectangular, false, false);
+
+                this.CanvasControl.Paused = false;
+                this.ToolCanvasControl.Invalidate(); // Invalidate
             };
         }
 
-    } 
+        public void Baking()
+        {
+            using (CanvasDrawingSession ds = this.BitmapLayer.CreateTempDrawingSession())
+            {
+                ds.Clear(Colors.Transparent);
+                ds.DrawImage(new CropEffect
+                {
+                    SourceRectangle = new Rect(2, 2, this.BitmapLayer.Width - 4, this.BitmapLayer.Height - 4),
+                    Source = this.BitmapLayer.Source
+                });
+            }
+            using (CanvasDrawingSession ds = this.Image.CreateDrawingSession())
+            {
+                ds.Clear(Colors.Transparent);
+                ds.DrawImage(new InvertEffect
+                {
+                    Source = new LuminanceToAlphaEffect//Alpha
+                    {
+                        Source = new EdgeDetectionEffect//Edge
+                        {
+                            Amount = 1,
+                            Source = this.BitmapLayer.Temp
+                        }
+                    }
+                });
+            }
+        }
+
+    }
 }
