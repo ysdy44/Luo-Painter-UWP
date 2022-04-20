6974b39f79df2efda7e66d9c3d81677117985d1e
ysdy44 <ysdy44@outlook.com>
2022-04-20 21:42:32 +0800

    Add FloodSelectPage

---
+++ Luo Painter.TestApp/FloodSelectPage.xaml.cs
@@ -0,0 +1,120 @@
+﻿using Luo_Painter.Layers.Models;
+using Microsoft.Graphics.Canvas;
+using System;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml.Controls;
+
+namespace Luo_Painter.TestApp
+{
+    public sealed partial class FloodSelectPage : Page
+    {
+
+        readonly CanvasDevice Device = new CanvasDevice();
+        BitmapLayer BitmapLayer;
+
+        public FloodSelectPage()
+        {
+            this.InitializeComponent();
+            this.ConstructCanvas();
+            this.ConstructOperator();
+            this.AddButton.Click += async (s, e) =>
+            {
+                StorageFile file = await PickSingleImageFileAsync(PickerLocationId.Desktop);
+                if (file == null) return;
+
+                bool? result = await this.AddAsync(file);
+                if (result != true) return;
+
+                this.CanvasControl.Invalidate(); // Invalidate
+                this.OriginCanvasControl.Invalidate(); // Invalidate
+            };
+            this.ClearButton.Click += (s, e) =>
+            {
+                this.BitmapLayer.ClearTemp();
+                this.OriginCanvasControl.Invalidate(); // Invalidate
+            };
+        }
+
+        private void ConstructCanvas()
+        {
+            this.CanvasControl.UseSharedDevice = true;
+            this.CanvasControl.CustomDevice = this.Device;
+            this.CanvasControl.Draw += (sender, args) =>
+            {
+                if (this.BitmapLayer is null) return;
+
+                args.DrawingSession.DrawImage(this.BitmapLayer.Source);
+            };
+
+
+            this.OriginCanvasControl.UseSharedDevice = true;
+            this.OriginCanvasControl.CustomDevice = this.Device;
+            this.OriginCanvasControl.Draw += (sender, args) =>
+            {
+                if (this.BitmapLayer is null) return;
+
+                args.DrawingSession.DrawImage(this.BitmapLayer.Temp);
+            };
+        }
+
+        private void ConstructOperator()
+        {
+            // Single
+            this.Operator.Single_Start += (point, properties) =>
+            {
+            };
+            this.Operator.Single_Delta += (point, properties) =>
+            {
+            };
+            this.Operator.Single_Complete += (point, properties) =>
+            {
+                this.BitmapLayer.FloodSelect(point);
+                this.OriginCanvasControl.Invalidate(); // Invalidate
+            };
+        }
+
+        public async static Task<StorageFile> PickSingleImageFileAsync(PickerLocationId location)
+        {
+            // Picker
+            FileOpenPicker openPicker = new FileOpenPicker
+            {
+                ViewMode = PickerViewMode.Thumbnail,
+                SuggestedStartLocation = location,
+                FileTypeFilter =
+                {
+                    ".jpg",
+                    ".jpeg",
+                    ".png",
+                    ".bmp"
+                }
+            };
+
+            // File
+            StorageFile file = await openPicker.PickSingleFileAsync();
+            return file;
+        }
+
+        public async Task<bool?> AddAsync(IRandomAccessStreamReference reference)
+        {
+            if (reference == null) return null;
+
+            try
+            {
+                using (IRandomAccessStreamWithContentType stream = await reference.OpenReadAsync())
+                {
+                    CanvasBitmap bitmap = await CanvasBitmap.LoadAsync(this.CanvasControl, stream);
+                    this.BitmapLayer = new BitmapLayer(this.CanvasControl, bitmap);
+                    return true;
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+    }
+}
