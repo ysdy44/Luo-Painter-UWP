11a5e3042038a9d9bd80b7ddee0483607560100d
ysdy44 <ysdy44@outlook.com>
2022-04-24 00:31:01 +0800

    MarqueeToolPage use CanvasLinearGradientBrush

--- Luo Painter.TestApp/MarqueeToolPage.xaml.cs
+++ Luo Painter.TestApp/MarqueeToolPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using FanKit.Transformers;
 using Luo_Painter.Layers.Models;
 using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.Brushes;
 using Microsoft.Graphics.Canvas.Effects;
 using System.Numerics;
 using Windows.Foundation;
@@ -15,9 +16,9 @@ namespace Luo_Painter.TestApp
         readonly CanvasDevice Device = new CanvasDevice();
         readonly MarqueeTool MarqueeTool = new MarqueeTool();
 
-        DottedLineBrush Brush;
         BitmapLayer BitmapLayer;
-        CanvasRenderTarget Image;
+        CanvasLinearGradientBrush Brush;
+        Matrix3x2 BrushTransform = Matrix3x2.Identity;
 
         Vector2 StartingPosition;
 
@@ -34,7 +35,6 @@ namespace Luo_Painter.TestApp
             this.ResetButton.Tapped += (s, e) =>
             {
                 this.BitmapLayer.Clear(Colors.Transparent);
-                this.Baking();
             };
         }
 
@@ -45,20 +45,56 @@ namespace Luo_Painter.TestApp
             this.CanvasControl.CustomDevice = this.Device;
             this.CanvasControl.CreateResources += (sender, args) =>
             {
-                //@DPI
-                this.Brush = new DottedLineBrush(sender);
-                this.Image = new CanvasRenderTarget(sender, 512, 512, 96);
                 this.BitmapLayer = new BitmapLayer(sender, 512, 512);
+                this.Brush = new CanvasLinearGradientBrush(sender, new CanvasGradientStop[]
+                {
+                    new CanvasGradientStop
+                    {
+                        Color = Windows.UI.Colors.White,
+                        Position = 0
+                    },
+                    new CanvasGradientStop
+                    {
+                        Color = Windows.UI.Colors.White,
+                        Position = 0.49f
+                    },
+                    new CanvasGradientStop
+                    {
+                        Color = Windows.UI.Colors.Black,
+                        Position = 0.51f
+                    },
+                    new CanvasGradientStop
+                    {
+                        Color = Windows.UI.Colors.Black,
+                        Position = 1
+                    }
+                }, CanvasEdgeBehavior.Wrap, CanvasAlphaMode.Premultiplied)
+                {
+                    StartPoint = Vector2.Zero,
+                    EndPoint = new Vector2(12)
+                };
             };
             this.CanvasControl.Draw += (sender, args) =>
             {
-                args.DrawingSession.DrawImage(this.Image);
+                args.DrawingSession.DrawImage(new InvertEffect
+                {
+                    Source = new LuminanceToAlphaEffect//Alpha
+                    {
+                        Source = new EdgeDetectionEffect//Edge
+                        {
+                            Amount = 1,
+                            Source = this.BitmapLayer.Source
+                        }
+                    }
+                });
                 args.DrawingSession.Blend = CanvasBlend.Min;
-                args.DrawingSession.FillRectangle(0, 0, (float)sender.Size.Width, (float)sender.Size.Height, this.Brush.Brush);
+                args.DrawingSession.FillRectangle(0, 0, (float)sender.Size.Width, (float)sender.Size.Height, this.Brush);
             };
             this.CanvasControl.Update += (sender, args) =>
             {
-                this.Brush.Update();
+                this.BrushTransform.M31--;
+                this.BrushTransform.M32--;
+                this.Brush.Transform = this.BrushTransform;
             };
 
 
@@ -93,43 +129,14 @@ namespace Luo_Painter.TestApp
             {
                 using (CanvasDrawingSession ds = this.BitmapLayer.CreateSourceDrawingSession())
                 {
-                    ds.FillMarqueeMaskl(this.CanvasControl, MarqueeToolType.Rectangular, this.MarqueeTool, this.Image.Bounds, MarqueeCompositeMode.New);
+                    ds.FillMarqueeMaskl(this.CanvasControl, MarqueeToolType.Rectangular, this.MarqueeTool, new Rect(0, 0, 512, 512), MarqueeCompositeMode.New);
                 }
-                this.Baking();
 
                 this.MarqueeTool.Complete(this.StartingPosition, point, MarqueeToolType.Rectangular, false, false);
 
                 this.CanvasControl.Paused = false;
                 this.ToolCanvasControl.Invalidate(); // Invalidate
             };
-        }
-
-        public void Baking()
-        {
-            using (CanvasDrawingSession ds = this.BitmapLayer.CreateTempDrawingSession())
-            {
-                ds.Clear(Colors.Transparent);
-                ds.DrawImage(new CropEffect
-                {
-                    SourceRectangle = new Rect(2, 2, this.BitmapLayer.Width - 4, this.BitmapLayer.Height - 4),
-                    Source = this.BitmapLayer.Source
-                });
-            }
-            using (CanvasDrawingSession ds = this.Image.CreateDrawingSession())
-            {
-                ds.Clear(Colors.Transparent);
-                ds.DrawImage(new InvertEffect
-                {
-                    Source = new LuminanceToAlphaEffect//Alpha
-                    {
-                        Source = new EdgeDetectionEffect//Edge
-                        {
-                            Amount = 1,
-                            Source = this.BitmapLayer.Temp
-                        }
-                    }
-                });
-            }
         }
 
     }
