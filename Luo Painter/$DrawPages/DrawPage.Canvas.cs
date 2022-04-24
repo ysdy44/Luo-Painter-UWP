5f02768b37f0d37e17e37190445b4b9c8527c6f5
ysdy44 <ysdy44@outlook.com>
2022-04-24 08:45:49 +0800

    CanvasAnimatedControl Draw DottedLine

--- Luo Painter/$DrawPages/DrawPage.Canvas.cs
+++ Luo Painter/$DrawPages/DrawPage.Canvas.cs
@@ -1,14 +1,17 @@
-﻿using Luo_Painter.Elements;
+﻿using FanKit.Transformers;
+using Luo_Painter.Elements;
 using Luo_Painter.Options;
 using Luo_Painter.Layers.Models;
 using Luo_Painter.Shaders;
 using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.Brushes;
 using Microsoft.Graphics.Canvas.Effects;
 using Microsoft.Graphics.Canvas.Geometry;
 using System;
 using System.Numerics;
 using System.Threading.Tasks;
 using Windows.Foundation;
+using Windows.Graphics.Effects;
 using Windows.UI;
 using Windows.UI.Xaml.Controls;
 
@@ -50,10 +53,70 @@ namespace Luo_Painter
         }
     }
 
+    internal sealed class DottedLine
+    {
+        public readonly CanvasLinearGradientBrush DottedLineBrush;
+        public Matrix3x2 DottedLineTransform = Matrix3x2.Identity;
+
+        public DottedLine(ICanvasResourceCreator resourceCreator)
+        {
+            this.DottedLineBrush = new CanvasLinearGradientBrush(resourceCreator, new CanvasGradientStop[]
+            {
+                new CanvasGradientStop
+                {
+                    Color = Windows.UI.Colors.White,
+                    Position = 0
+                },
+                new CanvasGradientStop
+                {
+                    Color = Windows.UI.Colors.Black,
+                    Position = 1
+                }
+            }, CanvasEdgeBehavior.Mirror, CanvasAlphaMode.Premultiplied)
+            {
+                StartPoint = Vector2.Zero,
+                EndPoint = new Vector2(12)
+            };
+        }
+        public void Draw(CanvasDrawingSession ds, CanvasTransformer transformer, IGraphicsEffectSource source)
+        {
+            ds.Units = CanvasUnits.Pixels;
+            ds.DrawImage(new InvertEffect
+            {
+                Source = new LuminanceToAlphaEffect//Alpha
+                {
+                    Source = new EdgeDetectionEffect//Edge
+                    {
+                        Amount = 1,
+                        Source = new Transform2DEffect
+                        {
+                            BorderMode = EffectBorderMode.Hard,
+                            InterpolationMode = CanvasImageInterpolation.NearestNeighbor,
+                            TransformMatrix = transformer.GetMatrix(),
+                            Source = source
+                        }
+                    }
+                }
+            });
+
+            ds.Blend = CanvasBlend.Min;
+            ds.FillRectangle(0, 0, transformer.ControlWidth, transformer.ControlHeight, this.DottedLineBrush);
+        }
+        public void Update()
+        {
+            this.DottedLineTransform.M31--;
+            this.DottedLineTransform.M32--;
+            this.DottedLineBrush.Transform = this.DottedLineTransform;
+        }
+    }
+
     public sealed partial class DrawPage : Page
     {
 
         Mesh Mesh;
+        DottedLine DottedLine;
+        BitmapLayer Marquee;
+
         byte[] LiquefactionShaderCodeBytes;
         byte[] FreeTransformShaderCodeBytes;
         byte[] GradientMappingShaderCodeBytes;
@@ -62,6 +125,14 @@ namespace Luo_Painter
 
         private void ConstructCanvas()
         {
+            this.CanvasAnimatedControl.CreateResources += (sender, args) =>
+            {
+                this.DottedLine = new DottedLine(sender);
+                this.Marquee = new BitmapLayer(sender, this.Transformer.Width, this.Transformer.Height);
+            };
+            this.CanvasAnimatedControl.Draw += (sender, args) => this.DottedLine.Draw(args.DrawingSession, this.Transformer, this.Marquee.Source);
+            this.CanvasAnimatedControl.Update += (sender, args) => this.DottedLine.Update();
+
             this.CanvasControl.SizeChanged += (s, e) =>
             {
                 if (e.NewSize == Size.Empty) return;
