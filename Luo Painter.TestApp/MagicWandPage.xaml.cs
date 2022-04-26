efcf2f7a560bfd2525f6d096155561eb4bb14d57
ysdy44 <ysdy44@outlook.com>
2022-04-26 21:16:10 +0800

    DottedLine

--- Luo Painter.TestApp/MagicWandPage.xaml.cs
+++ Luo Painter.TestApp/MagicWandPage.xaml.cs
@@ -21,35 +21,36 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“空白页”项模板
-
 namespace Luo_Painter.TestApp
 {
-    /// <summary>
-    /// 可用于自身或导航至 Frame 内部的空白页。
-    /// </summary>
     public sealed partial class MagicWandPage : Page
     {
         CanvasBitmap originalImage;
         CanvasBitmap effectImage;
         Color[] originalColors;
         Color[] effectColors;
-        ScaleEffect se;
+
         int imageWidth;
         int imageHeight;
 
         byte[] dottedLineCode;
+        byte[] colorMatchCode;
         PixelShaderEffect pse;
-        CanvasRenderTarget crt;
+
+
+        CanvasRenderTarget colorMatchTarget;
+        CanvasRenderTarget matchTarget;
+        CanvasRenderTarget effectTarget;
+
+
         /// <summary>
         /// 用于记录已近检索的像素
         /// </summary>
         Dictionary<Pos, int> tempDir = new Dictionary<Pos, int>();
-        DispatcherTimer timer = new DispatcherTimer();
 
-        float timeCount = 0;
-        int scale = 5;
-
+        Vector2 offset = new Vector2();
+        float scale = 1f;
+        float radin = 0f;
         public MagicWandPage()
         {
             this.InitializeComponent();
@@ -58,179 +59,240 @@ namespace Luo_Painter.TestApp
 
         async void Init()
         {
+
+            effectCanvas.CreateResources += (s, e) =>
             {
-                dottedLineCode = await ShaderType.DottedLine.LoadAsync();
+                effectTarget = new CanvasRenderTarget(effectCanvas, new Size(1920, 1080));
+            };
+            dottedLineCode = await ShaderType.DottedLine.LoadAsync();
+            colorMatchCode = await ShaderType.ColorMatch.LoadAsync();
 
-                timer.Interval = TimeSpan.FromMilliseconds(100);
-
-                timer.Tick += (s, e) =>
-                {
-                    if (effectImage == null)
-                        return;
-                    timeCount++;
-                    se = new ScaleEffect()
-                    {
-                        Source = effectImage,
-                        Scale = new Vector2(scale),
-                    };
-
-                    using (var ds = crt.CreateDrawingSession())
-                    {
-                        ds.DrawImage(se);
-                    }
-
-                    pse = new PixelShaderEffect(dottedLineCode)
-                    {
-                        Source1 = crt,
-                        Source1Interpolation = CanvasImageInterpolation.NearestNeighbor,
-                        Properties = {
-                            ["time"] =timeCount*1.5f,
-                            ["lineWidth"]=10f,
-                            ["color1"]= new Vector3(0),
-                            ["color2"]=new Vector3(1),
-                            //
-                            ["lineGap"]=100f,
-                            ["lineSpeed"] =100f,
-                        }
-                    };
-
-                    var bounds = se.GetBounds(effectCanvas);
-
-                    effectCanvas.Invalidate();
-                };
-
-                timer.Start();
-
-                this.Unloaded += (s, e) =>
-                {
-                    timer.Stop();
-                    timer = null;
-                };
-            }
-
-
-
+            this.Unloaded += (s, e) =>
+            {
+                effectCanvas = null;
+                originalCanvas = null;
+            };
             selectPicture.Click += async (s, e) =>
             {
                 StorageFile file = await PickSingleImageFileAsync(PickerLocationId.Desktop);
                 if (file != null)
                 {
                     originalImage = await CanvasBitmap.LoadAsync(CanvasDevice.GetSharedDevice(), await file.OpenReadAsync());
-                    originalColors = originalImage.GetPixelColors();
-                    effectColors = new Color[originalColors.Length];
-                    effectCanvas.Width = originalCanvas.Width = originalImage.Size.Width;
-                    effectCanvas.Height = originalCanvas.Height = originalImage.Size.Height;
-                    effectImage = CanvasBitmap.CreateFromColors(effectCanvas, effectColors, (int)originalImage.Size.Width, (int)originalImage.Size.Height, 96f);
-                    crt = new CanvasRenderTarget(effectCanvas, new Size(effectImage.Size.Width * scale, effectImage.Size.Height * scale));
-
                     imageWidth = (int)originalImage.Size.Width;
                     imageHeight = (int)originalImage.Size.Height;
+                    originalColors = originalImage.GetPixelColors();
+                    effectColors = new Color[originalColors.Length];
+                    colorMatchTarget = new CanvasRenderTarget(CanvasDevice.GetSharedDevice(), imageWidth, imageHeight, 96f);
+                    effectImage = CanvasBitmap.CreateFromColors(effectCanvas, effectColors, (int)originalImage.Size.Width, (int)originalImage.Size.Height, 96f);
                     originalCanvas.Invalidate();
-                    effectCanvas.Invalidate();
                 }
             };
-
+            //原图画布
             originalCanvas.Draw += (s, e) =>
             {
                 if (originalImage == null)
                     return;
-                e.DrawingSession.DrawImage(originalImage);
+                var m32 = new Matrix3x2();
+                m32 = Matrix3x2.CreateScale(scale) * Matrix3x2.CreateRotation(radin, new Vector2(0.5f, 0.5f)) * Matrix3x2.CreateTranslation(offset);
+                var te = new Transform2DEffect()
+                {
+                    Source = originalImage,
+                    TransformMatrix = m32
+                };
+                e.DrawingSession.DrawImage(te);
             };
-
+            //效果画布绘制
             effectCanvas.Draw += (s, e) =>
-            {
-                if (pse == null)
-                    return;
-                e.DrawingSession.DrawImage(pse);
-            };
+                {
+                    if (effectImage == null || dottedLineCode == null)
+                        return;
+                    var m32 = new Matrix3x2();
+                    m32 = Matrix3x2.CreateScale(scale) * Matrix3x2.CreateRotation(radin, new Vector2(0.5f, 0.5f)) * Matrix3x2.CreateTranslation(offset);
 
-            PointerEventHandler func = (s, e) =>
+                    Transform2DEffect te = new Transform2DEffect()
+                    {
+                        Source = effectImage,
+                        TransformMatrix = m32,
+                    };
+                    using (var ds = effectTarget.CreateDrawingSession())
+                    {
+                        ds.Clear(Colors.Transparent);
+                        ds.DrawImage(te);
+                    }
+
+                    pse = new PixelShaderEffect(dottedLineCode)
+                    {
+                        Source1 = effectTarget,
+                        Source1Interpolation = CanvasImageInterpolation.NearestNeighbor,
+                        Properties = {
+                            ["time"] =(float)e.Timing.TotalTime.TotalSeconds,
+                            ["lineWidth"]=1f,
+                            ["color1"]= new Vector3(0),
+                            ["color2"]=new Vector3(1),
+                            ["lineGap"]=100f,
+                            ["lineSpeed"] =100f,
+                            }
+                    };
+                    e.DrawingSession.DrawImage(pse);
+                };
+
+            PointerEventHandler OnCanvasMove = (s, e) =>
              {
                  var can = (FrameworkElement)s;
                  var point = e.GetCurrentPoint(can).Position;
-                 Vector3 v3 = new Vector3((float)point.X - 15f + can.ActualOffset.X, (float)point.Y - 15f + can.ActualOffset.Y, 0);
+                 Vector3 v3 = new Vector3((float)point.X - 15f, (float)point.Y - 15f, 0);
                  e1.Translation = e2.Translation = v3;
              };
+            originalCanvas.PointerMoved += OnCanvasMove;
+            effectCanvas.PointerMoved += OnCanvasMove;
 
-            originalCanvas.PointerMoved += func;
-            effectCanvas.PointerMoved += func;
-
-            TappedEventHandler teh = (s, e) =>
+            //点击事件
+            TappedEventHandler CanvasTapped = (s, e) =>
             {
-                if (originalImage == null)
+                if (originalImage == null || colorMatchCode == null)
                     return;
                 var can = (FrameworkElement)s;
                 var point = e.GetPosition(can);
 
-                float threshold = (float)thresholdSlider.Value / 100f;
-                var tr = threshold * 255;
-                var tg = threshold * 255;
-                var tb = threshold * 255;
-                var ta = threshold * 255;
-
-                Pos cp = new Pos((int)Math.Round(point.X), (int)Math.Round(point.Y));
-                Color selectColor = originalColors[cp.X + cp.Y * imageWidth];
-
-                int count = 0;
-
-                tempDir = new Dictionary<Pos, int>();
-                tempDir.Add(cp, 1);
-                List<Pos> retrieves = new List<Pos>();
-                retrieves.Add(cp);
-                while (retrieves.Count > 0)
-                {
-                    var p = retrieves[0];
-                    retrieves.RemoveAt(0);
-                    var index = p.X + p.Y * imageWidth;
-                    Color currentColor = originalColors[index];
-                    var r = Math.Abs(currentColor.R - selectColor.R);
-                    var g = Math.Abs(currentColor.G - selectColor.G);
-                    var b = Math.Abs(currentColor.B - selectColor.B);
-                    var a = Math.Abs(currentColor.A - selectColor.A);
-                    if (tr >= r && tg >= g && tb >= b && ta >= a)
-                    {
-
-                        effectColors[index] = originalColors[index];
-                        AddRetrieves(p, retrieves);
-                        count++;
-                    }
-
-                }
-                Console.WriteLine(count);
+                var m32 = new Matrix3x2();
+                m32 = Matrix3x2.CreateScale(scale) * Matrix3x2.CreateRotation(radin, new Vector2(0.5f, 0.5f)) * Matrix3x2.CreateTranslation(offset);
+               point = Vector2.Transform(point.ToVector2(), m32).ToPoint();
+                if (point.X < 0 || point.Y < 0 || point.X > imageWidth || point.Y > imageHeight)
+                    return;
+                MagicWand(point);
                 effectImage = CanvasBitmap.CreateFromColors(effectCanvas, effectColors, (int)originalImage.Size.Width, (int)originalImage.Size.Height, 96f);
                 effectCanvas.Invalidate();
             };
+            originalCanvas.Tapped += CanvasTapped;
+            effectCanvas.Tapped += CanvasTapped;
 
-            originalCanvas.Tapped += teh;
-            effectCanvas.Tapped += teh;
+            //滚轮事件
+            PointerEventHandler WheelChanged = (s, e) =>
+            {
+                FrameworkElement element = (FrameworkElement)s;
+                var dir = e.GetCurrentPoint(element).Properties.MouseWheelDelta;
+                //缩放
+                if (e.KeyModifiers == Windows.System.VirtualKeyModifiers.Control)
+                {
+                    scale += dir < 0 ? 0.1f : -0.1f;
+                }
+                //旋转
+                else
+                {
+                    var angle = (float)(2 * Math.PI / 360);
+                    radin += dir < 0 ? angle : -angle;
+                }
+                originalCanvas.Invalidate();
+            };
+            originalCanvas.PointerWheelChanged += WheelChanged;
+            effectCanvas.PointerWheelChanged += WheelChanged;
+            originalCanvas.ManipulationMode = effectCanvas.ManipulationMode = ManipulationModes.TranslateX | ManipulationModes.TranslateY;
+            ManipulationDeltaEventHandler Detal = (s, e) =>
+            {
+                offset += e.Delta.Translation.ToVector2();
+                originalCanvas.Invalidate();
+            };
 
+            originalCanvas.ManipulationDelta += Detal;
+            effectCanvas.ManipulationDelta += Detal;
+        }
+
+        
+
+
+
+        void MagicWand(Point point)
+        {
+            float threshold = (float)thresholdSlider.Value / 100f;
+            Pos cp = new Pos((int)Math.Round(point.X), (int)Math.Round(point.Y));
+            Color selectColor = originalColors[cp.X + cp.Y * imageWidth];
+
+            using (var ds = colorMatchTarget.CreateDrawingSession())
+            {
+                var colorMatchEffect = new PixelShaderEffect(colorMatchCode)
+                {
+                    Source1 = originalImage,
+                    Properties = {
+                    ["threshold"] = threshold,
+                    ["matchColor"] = new Vector4((float)selectColor.R/255,(float)selectColor.G/255,(float)selectColor.B/255,(float)selectColor.A/255)
+                }
+                };
+                ds.Clear(Colors.Transparent);
+                ds.DrawImage(colorMatchEffect);
+            }
+            var tempColor = colorMatchTarget.GetPixelColors();
+
+
+            tempDir = new Dictionary<Pos, int>();
+            tempDir.Add(cp, 1);
+            List<Pos> retrieves = new List<Pos>();
+            retrieves.Add(cp);
+            while (retrieves.Count > 0)
+            {
+                var p = retrieves[0];
+                retrieves.RemoveAt(0);
+                var index = p.X + p.Y * imageWidth;
+                Color currentColor = tempColor[index];
+                if (currentColor.A > 0)
+                {
+                    effectColors[index] = originalColors[index];
+                    AddRetrieves(p, retrieves);
+                }
+            }
         }
 
         void AddRetrieves(Pos cp, List<Pos> retrieves)
         {
-            for (int rx = -1; rx <= 1; rx++)
+            Action<int, int> AddRetrieves = (cx, cy) =>
             {
-                for (int ry = -1; ry <= 1; ry++)
+                if (cx < 0 || cy < 0)
                 {
-                    int x = cp.X + rx;
-                    int y = cp.Y + ry;
-                    if (x >= 0 && y >= 0 && x < imageWidth && y < imageHeight)
-                    {
-                        Pos pos = new Pos(x, y);
-                        if (!tempDir.ContainsKey(pos))
-                        {
-                            //添加到以检索队列中
-                            tempDir.Add(pos, 1);
-                            retrieves.Add(pos);
-                        }
-                        else
-                        {
-                            var a = "a";
-                        }
-
-                    }
+                    var i = 2;
                 }
-            }
+
+                Pos pos = new Pos(cx, cy);
+                if (!tempDir.ContainsKey(pos))
+                {
+                    //添加到以检索队列中
+                    tempDir.Add(pos, 1);
+                    retrieves.Add(pos);
+                }
+            };
+
+            var x = cp.X;
+            var y = cp.Y;
+
+            //上边
+            if (y - 1 >= 0)
+                AddRetrieves(x, y - 1);
+            //下边
+            if (y + 1 < imageHeight)
+                AddRetrieves(x, y + 1);
+            //左边
+            if (x - 1 >= 0)
+                AddRetrieves(x - 1, y);
+            //左边
+            if (x + 1 < imageWidth)
+                AddRetrieves(x + 1, y);
+
+            //for (int rx = -1; rx <= 1; rx++)
+            //{
+            //    for (int ry = -1; ry <= 1; ry++)
+            //    {
+            //        int x = cp.X + rx;
+            //        int y = cp.Y + ry;
+            //        if (x >= 0 && y >= 0 && x < imageWidth && y < imageHeight)
+            //        {
+            //            Pos pos = new Pos(x, y);
+            //            if (!tempDir.ContainsKey(pos))
+            //            {
+            //                //添加到以检索队列中
+            //                tempDir.Add(pos, 1);
+            //                retrieves.Add(pos);
+            //            }
+            //        }
+            //    }
+            //}
         }
 
         public async static Task<StorageFile> PickSingleImageFileAsync(PickerLocationId location)
