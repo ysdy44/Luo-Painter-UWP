c65c2a67d7401fdefbea21c0f28bb923b32adf07
ysdy44 <ysdy44@outlook.com>
2022-04-27 08:46:34 +0800

    Add SwitchPresenterPage

---
+++ Luo Painter.TestApp/SwitchPresenterPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Markup;
+
+namespace Luo_Painter.TestApp
+{
+    internal enum Animal
+    {
+        Cat,
+        Dog,
+        Bunny,
+        Llama,
+        Parrot,
+        Squirrel
+    }
+
+    [ContentProperty(Name = nameof(Content))]
+    internal class AnimalCase : Elements.Case<Animal> { }
+
+    [ContentProperty(Name = nameof(SwitchCases))]
+    internal class AnimalSwitchPresenter : Elements.SwitchPresenter<Animal> { }
+
+    public sealed partial class SwitchPresenterPage : Page
+    {
+        public SwitchPresenterPage()
+        {
+            this.InitializeComponent();
+            this.SwitchPresenter.Value = Animal.Cat;
+        }
+    }
+}
