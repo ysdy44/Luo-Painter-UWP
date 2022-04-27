391d0cc11d777c8220c4947b0a09f74a79414e09
ysdy44 <ysdy44@outlook.com>
2022-04-28 00:30:03 +0800

    Add BitmapLayer.Paint

---
+++ Luo Painter.Layers/Models/BitmapLayer.Paint.cs
@@ -0,0 +1,274 @@
+﻿using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.Effects;
+using Microsoft.Graphics.Canvas.Geometry;
+using System.Numerics;
+using Windows.UI;
+
+namespace Luo_Painter.Layers.Models
+{
+    public enum InkMode
+    {
+        None,
+
+        Dry,
+        WetWithOpacity,
+        WetWithBlendMode,
+        WetWithOpacityAndBlendMode,
+
+        EraseDry,
+        EraseWetWithOpacity,
+
+        Liquefy,
+    }
+
+    public sealed partial class BitmapLayer : LayerBase, ILayer
+    {
+
+        //@Static
+        private static readonly CanvasStrokeStyle CanvasStrokeStyle = new CanvasStrokeStyle
+        {
+            StartCap = CanvasCapStyle.Round,
+            EndCap = CanvasCapStyle.Round,
+        };
+
+
+        public InkMode InkMode { get; set; }
+
+
+        public CompositeEffect GetWeting(float opacity) => new CompositeEffect
+        {
+            Sources =
+            {
+                this.OriginRenderTarget,
+                new OpacityEffect
+                {
+                    Source = this.TempRenderTarget,
+                    Opacity = opacity
+                }
+            }
+        };
+        public BlendEffect GetWeting(BlendEffectMode blendMode) => new BlendEffect
+        {
+            Background = this.OriginRenderTarget,
+            Foreground = this.TempRenderTarget,
+            Mode = blendMode
+        };
+        public BlendEffect GetWeting(float opacity, BlendEffectMode blendMode) => new BlendEffect
+        {
+            Background = this.OriginRenderTarget,
+            Foreground = new OpacityEffect
+            {
+                Source = this.TempRenderTarget,
+                Opacity = opacity
+            },
+            Mode = blendMode
+        };
+
+
+        public void FillCircleDry(Vector2 position, Vector2 targetPosition, float pressure, float targetPressure, float size, Color color)
+        {
+            using (CanvasDrawingSession ds = this.SourceRenderTarget.CreateDrawingSession())
+            {
+                //@DPI 
+                ds.Units = CanvasUnits.Pixels; /// <see cref="DPIExtensions">
+
+                ds.FillCircle(position, size * targetPressure, color);
+
+                float length = Vector2.Distance(position, targetPosition);
+                if (length > size * targetPressure / 2)
+                {
+                    float spane = size / 2 / length * pressure;
+                    for (float i = spane; i < 1f; i += spane)
+                    {
+                        Vector2 p = position * i + (1 - i) * targetPosition;
+                        float e = pressure * i + (1 - i) * targetPressure;
+                        ds.FillCircle(p, size * e, color);
+                    }
+                }
+            }
+        }
+        public void FillCircleWet(Vector2 position, Vector2 targetPosition, float pressure, float targetPressure, float size, Color color)
+        {
+            using (CanvasDrawingSession ds = this.TempRenderTarget.CreateDrawingSession())
+            {
+                //@DPI 
+                ds.Units = CanvasUnits.Pixels; /// <see cref="DPIExtensions">
+
+                ds.FillCircle(position, size * targetPressure, color);
+
+                float length = Vector2.Distance(position, targetPosition);
+                if (length > size * targetPressure / 2)
+                {
+                    float spane = size / 2 / length * pressure;
+                    for (float i = spane; i < 1f; i += spane)
+                    {
+                        Vector2 p = position * i + (1 - i) * targetPosition;
+                        float e = pressure * i + (1 - i) * targetPressure;
+                        ds.FillCircle(p, size * e, color);
+                    }
+                }
+            }
+        }
+
+
+        public ArithmeticCompositeEffect GetEraseWeting(float opacity) => new ArithmeticCompositeEffect
+        {
+            MultiplyAmount = 0,
+            Source1Amount = 1,
+            Source2Amount = -opacity,
+            Offset = 0,
+            Source1 = this.OriginRenderTarget,
+            Source2 = this.TempRenderTarget,
+        };
+
+        public void ErasingDry(Vector2 position, Vector2 targetPosition, float pressure, float targetPressure, float size)
+        {
+            using (CanvasDrawingSession ds = this.SourceRenderTarget.CreateDrawingSession())
+            {
+                //@DPI 
+                ds.Units = CanvasUnits.Pixels; /// <see cref="DPIExtensions">
+
+                ds.Blend = CanvasBlend.Copy;
+
+                ds.FillCircle(position, size * targetPressure, Colors.Transparent);
+
+                float length = Vector2.Distance(position, targetPosition);
+                if (length > size * targetPressure / 2)
+                {
+                    float spane = size / 2 / length * pressure;
+                    for (float i = spane; i < 1f; i += spane)
+                    {
+                        Vector2 p = position * i + (1 - i) * targetPosition;
+                        float e = pressure * i + (1 - i) * targetPressure;
+                        ds.FillCircle(p, size * e, Colors.Transparent);
+                    }
+                }
+            }
+        }
+        public void ErasingWet(Vector2 position, Vector2 targetPosition, float pressure, float targetPressure, float size)
+        {
+            using (CanvasDrawingSession ds = this.TempRenderTarget.CreateDrawingSession())
+            {
+                //@DPI 
+                ds.Units = CanvasUnits.Pixels; /// <see cref="DPIExtensions">
+
+                ds.FillCircle(position, size * targetPressure, Colors.White);
+
+                float length = Vector2.Distance(position, targetPosition);
+                if (length > size * targetPressure / 2)
+                {
+                    float spane = size / 2 / length * pressure;
+                    for (float i = spane; i < 1f; i += spane)
+                    {
+                        Vector2 p = position * i + (1 - i) * targetPosition;
+                        float e = pressure * i + (1 - i) * targetPressure;
+                        ds.FillCircle(p, size * e, Colors.White);
+                    }
+                }
+            }
+        }
+
+
+        public void DrawLine(CanvasDrawingSession ds, Vector2 position, Vector2 targetPosition, Color color, float strokeWidth)
+        {
+            //@DPI 
+            ds.Units = CanvasUnits.Pixels; /// <see cref="DPIExtensions">
+
+            ds.DrawLine(position, targetPosition, color, strokeWidth, BitmapLayer.CanvasStrokeStyle);
+        }
+
+
+        public void FillCircleFlipX(CanvasDrawingSession ds, Vector2 position, Vector2 targetPosition, float size, Color color, float width)
+        {
+            //@DPI 
+            ds.Units = CanvasUnits.Pixels; /// <see cref="DPIExtensions">
+
+            ds.FillCircle(position, size, color);
+            ds.FillCircle(width - position.X, position.Y, size, color);
+
+            float length = Vector2.Distance(position, targetPosition);
+            if (length > size / 2)
+            {
+                float spane = size / 2 / length;
+                for (float i = spane; i < 1f; i += spane)
+                {
+                    Vector2 p = position * i + (1 - i) * targetPosition;
+                    ds.FillCircle(p, size, color);
+                    ds.FillCircle(width - p.X, p.Y, size, color);
+                }
+            }
+        }
+
+        public void FillCircleFlipY(CanvasDrawingSession ds, Vector2 position, Vector2 targetPosition, float size, Color color, float height)
+        {
+            //@DPI 
+            ds.Units = CanvasUnits.Pixels; /// <see cref="DPIExtensions">
+
+            ds.FillCircle(position, size, color);
+            ds.FillCircle(position.X, height - position.Y, size, color);
+
+            float length = Vector2.Distance(position, targetPosition);
+            if (length > size / 2)
+            {
+                float spane = size / 2 / length;
+                for (float i = spane; i < 1f; i += spane)
+                {
+                    Vector2 p = position * i + (1 - i) * targetPosition;
+                    ds.FillCircle(p, size, color);
+                    ds.FillCircle(p.X, height - p.Y, size, color);
+                }
+            }
+        }
+
+        public void FillCircleTwo(CanvasDrawingSession ds, Vector2 position, Vector2 targetPosition, float size, Color color, float width, float height)
+        {
+            //@DPI 
+            ds.Units = CanvasUnits.Pixels; /// <see cref="DPIExtensions">
+
+            ds.FillCircle(position, size, color);
+            ds.FillCircle(width - position.X, height - position.Y, size, color);
+
+            float length = Vector2.Distance(position, targetPosition);
+            if (length > size / 2)
+            {
+                float spane = size / 2 / length;
+                for (float i = spane; i < 1f; i += spane)
+                {
+                    Vector2 p = position * i + (1 - i) * targetPosition;
+                    ds.FillCircle(p, size, color);
+                    ds.FillCircle(width - p.X, height - p.Y, size, color);
+                }
+            }
+        }
+
+        public void FillCircleFour(CanvasDrawingSession ds, Vector2 position, Vector2 targetPosition, float size, Color color, float width, float height, Vector2 center)
+        {
+            //@DPI 
+            ds.Units = CanvasUnits.Pixels; /// <see cref="DPIExtensions">
+
+            ds.FillCircle(position, size, color);
+            ds.FillCircle(width - position.X, height - position.Y, size, color);
+
+            Vector2 vector = position - center;
+            ds.FillCircle(center.X - vector.Y, center.Y + vector.X, size, color);
+            ds.FillCircle(center.X + vector.Y, center.Y - vector.X, size, color);
+
+            float length = Vector2.Distance(position, targetPosition);
+            if (length > size / 2)
+            {
+                float spane = size / 2 / length;
+                for (float i = spane; i < 1f; i += spane)
+                {
+                    Vector2 p = position * i + (1 - i) * targetPosition;
+                    ds.FillCircle(p, size, color);
+                    ds.FillCircle(width - p.X, height - p.Y, size, color);
+
+                    Vector2 v = p - center;
+                    ds.FillCircle(center.X - v.Y, center.Y + v.X, size, color);
+                    ds.FillCircle(center.X + v.Y, center.Y - v.X, size, color);
+                }
+            }
+        }
+
+    }
+}
--- Luo Painter.Layers/Models/BitmapLayer.Source.cs
+++ Luo Painter.Layers/Models/BitmapLayer.Source.cs
@@ -5,25 +5,8 @@ using Windows.UI;
 
 namespace Luo_Painter.Layers.Models
 {
-    public enum InkMode
-    {
-        None,
-
-        Dry,
-        WetWithOpacity,
-        WetWithBlendMode,
-        WetWithOpacityAndBlendMode,
-
-        EraseDry,
-        EraseWetWithOpacity,
-
-        Liquefy,
-    }
-
     public sealed partial class BitmapLayer : LayerBase, ILayer
     {
-
-        public InkMode InkMode { get; set; }
 
         public ICanvasImage Origin => this.OriginRenderTarget;
         public ICanvasImage Source => this.SourceRenderTarget;
@@ -76,77 +59,6 @@ namespace Luo_Painter.Layers.Models
                 ds.Units = CanvasUnits.Pixels; /// <see cref="DPIExtensions">
 
                 ds.Clear(Colors.Transparent);
-            }
-        }
-
-
-        public CompositeEffect GetWeting(float opacity) => new CompositeEffect
-        {
-            Sources =
-            {
-                this.OriginRenderTarget,
-                new OpacityEffect
-                {
-                    Source = this.TempRenderTarget,
-                    Opacity = opacity
-                }
-            }
-        };
-        public BlendEffect GetWeting(BlendEffectMode blendMode) => new BlendEffect
-        {
-            Background = this.OriginRenderTarget,
-            Foreground = this.TempRenderTarget,
-            Mode = blendMode
-        };
-        public BlendEffect GetWeting(float opacity, BlendEffectMode blendMode) => new BlendEffect
-        {
-            Background = this.OriginRenderTarget,
-            Foreground = new OpacityEffect
-            {
-                Source = this.TempRenderTarget,
-                Opacity = opacity
-            },
-            Mode = blendMode
-        };
-
-        public void FillCircleDry(Vector2 position, Vector2 targetPosition, float pressure, float targetPressure, float size, Color color)
-        {
-            using (CanvasDrawingSession ds = this.SourceRenderTarget.CreateDrawingSession())
-            {
-                ds.FillCircle(position, targetPosition, pressure, targetPressure, size, color);
-            }
-        }
-        public void FillCircleWet(Vector2 position, Vector2 targetPosition, float pressure, float targetPressure, float size, Color color)
-        {
-            using (CanvasDrawingSession ds = this.TempRenderTarget.CreateDrawingSession())
-            {
-                ds.FillCircle(position, targetPosition, pressure, targetPressure, size, color);
-            }
-        }
-
-
-        public ArithmeticCompositeEffect GetEraseWeting(float opacity) => new ArithmeticCompositeEffect
-        {
-            MultiplyAmount = 0,
-            Source1Amount = 1,
-            Source2Amount = -opacity,
-            Offset = 0,
-            Source1 = this.OriginRenderTarget,
-            Source2 = this.TempRenderTarget,
-        };
-
-        public void ErasingDry(Vector2 position, Vector2 targetPosition, float pressure, float targetPressure, float size)
-        {
-            using (CanvasDrawingSession ds = this.SourceRenderTarget.CreateDrawingSession())
-            {
-                ds.ErasingDry(position, targetPosition, pressure, targetPressure, size);
-            }
-        }
-        public void ErasingWet(Vector2 position, Vector2 targetPosition, float pressure, float targetPressure, float size)
-        {
-            using (CanvasDrawingSession ds = this.TempRenderTarget.CreateDrawingSession())
-            {
-                ds.ErasingWet(position, targetPosition, pressure, targetPressure, size);
             }
         }
 
