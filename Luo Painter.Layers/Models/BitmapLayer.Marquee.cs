4bf5daa1655423ca840074b4e1b3167c96e4cfa5
ysdy44 <ysdy44@outlook.com>
2022-04-29 23:45:10 +0800

    BitmapLayer.Marquee Add Copy() Clear() Invert()

--- Luo Painter.Layers/Models/BitmapLayer.Marquee.cs
+++ Luo Painter.Layers/Models/BitmapLayer.Marquee.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Graphics.Canvas;
+﻿using Luo_Painter.Historys;
+using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.Effects;
 using System.Numerics;
+using Windows.Foundation;
 using Windows.UI;
 
 namespace Luo_Painter.Layers.Models
@@ -25,6 +28,51 @@ namespace Luo_Painter.Layers.Models
                     ds.DrawLine(position, targetPosition, Colors.DodgerBlue, strokeWidth, BitmapLayer.CanvasStrokeStyle);
                 }
             }
+        }
+
+        public void Copy(BitmapLayer bitmapLayer)
+        {
+            this.SourceRenderTarget.CopyPixelsFromBitmap(bitmapLayer.SourceRenderTarget);
+        }
+        public void Copy(BitmapLayer bitmapLayer, BitmapLayer marquee)
+        {
+            this.TempRenderTarget.CopyPixelsFromBitmap(marquee.SourceRenderTarget);
+            using (CanvasDrawingSession ds = this.CreateSourceDrawingSession())
+            {
+                ds.Blend = CanvasBlend.Copy;
+                ds.DrawImage(new AlphaMaskEffect
+                {
+                    Source = bitmapLayer.SourceRenderTarget,
+                    AlphaMask = this.Temp
+                });
+            }
+        }
+
+        public IHistory Clear(BitmapLayer marquee, Color[] interpolationColors)
+        {
+            PixelBounds bounds = marquee.CreateInterpolationBounds(interpolationColors);
+            Rect rect = bounds.ToRect(BitmapLayer.Unit);
+
+            this.TempRenderTarget.CopyPixelsFromBitmap(marquee.SourceRenderTarget);
+            using (CanvasDrawingSession ds = this.CreateSourceDrawingSession())
+            {
+                ds.DrawImage(this.Temp, (float)rect.Left, (float)rect.Top, rect, 1, CanvasImageInterpolation.NearestNeighbor, CanvasComposite.DestinationOut);
+            }
+            this.Hit(interpolationColors);
+
+            return this.GetBitmapHistory();
+        }
+
+        public IHistory Invert(Color color)
+        {
+            this.TempRenderTarget.CopyPixelsFromBitmap(this.SourceRenderTarget);
+            using (CanvasDrawingSession ds = this.CreateSourceDrawingSession())
+            {
+                ds.Clear(color);
+                ds.DrawImage(this.Temp, 0, 0, new Rect(0, 0, this.Width, this.Height), 1, CanvasImageInterpolation.NearestNeighbor, CanvasComposite.DestinationOut);
+            }
+
+            return this.GetBitmapResetHistory();
         }
 
     }
