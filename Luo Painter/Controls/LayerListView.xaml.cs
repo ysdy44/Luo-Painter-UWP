c9c05a18849f0b9042599c431022ad2cc8f92f63
ysdy44 <ysdy44@outlook.com>
2022-05-02 23:52:47 +0800

    Add Layer, ToolListViews

---
+++ Luo Painter/Controls/LayerListView.xaml.cs
@@ -0,0 +1,140 @@
+﻿using Luo_Painter.Elements;
+using Luo_Painter.Layers;
+using System;
+using System.Collections.Generic;
+using Windows.ApplicationModel.Resources;
+using Windows.Foundation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Luo_Painter.Controls
+{
+    internal class LayerCommand : RelayCommand<ILayer> { }
+
+    public sealed partial class LayerListView : Grid
+    {
+        //@Delegate
+        public event RoutedEventHandler AddClick
+        {
+            remove => this.AddButton.Click -= value;
+            add => this.AddButton.Click += value;
+        }
+        public event EventHandler<ILayer> VisualClick
+        {
+            remove => this.VisualCommand.Click -= value;
+            add => this.VisualCommand.Click += value;
+        }
+        public event DragItemsStartingEventHandler DragItemsStarting
+        {
+            remove => this.ListView.DragItemsStarting -= value;
+            add => this.ListView.DragItemsStarting += value;
+        }
+        public event TypedEventHandler<ListViewBase, DragItemsCompletedEventArgs> DragItemsCompleted
+        {
+            remove => this.ListView.DragItemsCompleted -= value;
+            add => this.ListView.DragItemsCompleted += value;
+        }
+
+        double StartingX;
+        SplitViewPanePlacement Placement => (this.TranslateTransform.X > 70) ? SplitViewPanePlacement.Left : SplitViewPanePlacement.Right;
+        private double GetX(double value) => Math.Max(-70 - 70, Math.Min(0, value));
+
+        public bool isExpanded;
+        public bool IsExpanded
+        {
+            get => this.isExpanded;
+            set
+            {
+                this.isExpanded = value;
+
+                if (this.IsShow)
+                {
+                    if (value)
+                        this.ExpandStoryboard.Begin();
+                    else
+                        this.NoneStoryboard.Begin();
+                }
+            }
+        }
+
+        #region DependencyProperty
+
+
+        /// <summary> Gets or set the state for <see cref="LayerListView"/>. </summary>
+        public bool IsShow
+        {
+            get => (bool)base.GetValue(IsShowProperty);
+            set => base.SetValue(IsShowProperty, value);
+        }
+        /// <summary> Identifies the <see cref = "LayerListView.IsShow" /> dependency property. </summary>
+        public static readonly DependencyProperty IsShowProperty = DependencyProperty.Register(nameof(IsShow), typeof(bool), typeof(LayerListView), new PropertyMetadata(true, (sender, e) =>
+        {
+            LayerListView control = (LayerListView)sender;
+
+            if (e.NewValue is bool value)
+            {
+                if (value)
+                {
+                    if (control.IsExpanded)
+                        control.ExpandStoryboard.Begin();
+                    else
+                        control.NoneStoryboard.Begin();
+                }
+                else
+                    control.HideStoryboard.Begin();
+            }
+        }));
+
+
+        #endregion
+
+        public FrameworkElement PlacementTarget => this.AddButton;
+        public IList<object> SelectedItems => this.ListView.SelectedItems;
+        public object SelectedItem => this.ListView.SelectedItem;
+        public int SelectedIndex
+        {
+            get => this.ListView.SelectedIndex;
+            set => this.ListView.SelectedIndex = value;
+        }
+        public object ItemsSource { set => this.ListView.ItemsSource = value; }
+
+        //@Construct
+        public LayerListView()
+        {
+            this.InitializeComponent();
+            this.SplitButton.ManipulationStarted += (s, e) =>
+            {
+                this.StartingX = this.TranslateTransform.X;
+                switch (this.Placement)
+                {
+                    case SplitViewPanePlacement.Left: this.SplitIcon.Symbol = Symbol.AlignLeft; break;
+                    case SplitViewPanePlacement.Right: this.SplitIcon.Symbol = Symbol.AlignRight; break;
+                }
+                this.SplitButton.IsEnabled = false;
+            };
+            this.SplitButton.ManipulationDelta += (s, e) =>
+            {
+                this.TranslateTransform.X = this.GetX(this.StartingX + e.Cumulative.Translation.X);
+                switch (this.Placement)
+                {
+                    case SplitViewPanePlacement.Left: this.SplitIcon.Symbol = Symbol.AlignLeft; break;
+                    case SplitViewPanePlacement.Right: this.SplitIcon.Symbol = Symbol.AlignRight; break;
+                }
+            };
+            this.SplitButton.ManipulationCompleted += (s, e) =>
+            {
+                this.IsExpanded = this.Placement is SplitViewPanePlacement.Left;
+                this.SplitIcon.Symbol = Symbol.GlobalNavigationButton;
+                this.SplitButton.IsEnabled = true;
+            };
+
+            this.SplitButton.Click += (s, e) => this.IsExpanded = this.Placement is SplitViewPanePlacement.Right;
+        }
+
+        //@Strings
+        public void ConstructStrings(ResourceLoader resource)
+        {
+        }
+
+    }
+}
---
+++ Luo Painter/Controls/ToolListView.xaml.cs
@@ -0,0 +1,172 @@
+﻿using Luo_Painter.Edits;
+using Luo_Painter.Elements;
+using Luo_Painter.Tools;
+using System;
+using Windows.ApplicationModel.Resources;
+using Windows.Foundation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Luo_Painter.Controls
+{
+    internal sealed class ToolIcon : ContentControl
+    {
+        #region DependencyProperty
+
+
+        /// <summary> Gets or set the type for <see cref="ToolIcon"/>. </summary>
+        public ToolType Type
+        {
+            get => (ToolType)base.GetValue(TypeProperty);
+            set => base.SetValue(TypeProperty, value);
+        }
+        /// <summary> Identifies the <see cref = "ToolIcon.Type" /> dependency property. </summary>
+        public static readonly DependencyProperty TypeProperty = DependencyProperty.Register(nameof(Type), typeof(ToolType), typeof(ToolIcon), new PropertyMetadata(ToolType.None, (sender, e) =>
+        {
+            ToolIcon control = (ToolIcon)sender;
+
+            if (e.NewValue is ToolType value)
+            {
+                control.Content = value;
+                control.Template = value.GetTemplate(out ResourceDictionary resource);
+                control.Resources = resource;
+            }
+        }));
+
+
+        #endregion
+
+        public ToolIcon()
+        {
+            base.Loaded += (s, e) =>
+            {
+                ListViewItem parent = SelectedButtonPresenter.FindAncestor(this);
+                if (parent is null) return;
+                ToolTipService.SetToolTip(parent, new ToolTip
+                {
+                    Content = this.Type,
+                    Style = App.Current.Resources["AppToolTipStyle"] as Style
+                });
+            };
+        }
+    }
+
+    internal sealed class ToolGroupingList : GroupingList<ToolGrouping, ToolGroupType, ToolType> { }
+    internal sealed class ToolGrouping : Grouping<ToolGroupType, ToolType> { }
+
+    public sealed partial class ToolListView : Grid
+    {
+        //@Delegate
+        public event TypedEventHandler<ToolGroupType, ToolType> ItemClick;
+
+        double StartingX;
+        SplitViewPanePlacement Placement => (this.TranslateTransform.X < -70) ? SplitViewPanePlacement.Left : SplitViewPanePlacement.Right;
+        private double GetX(double value) => Math.Max(-70 - 70, Math.Min(0, value));
+
+        public bool isExpanded;
+        public bool IsExpanded
+        {
+            get => this.isExpanded;
+            set
+            {
+                this.isExpanded = value;
+
+                if (this.IsShow)
+                {
+                    if (value)
+                        this.ExpandStoryboard.Begin();
+                    else
+                        this.NoneStoryboard.Begin();
+                }
+            }
+        }
+
+        #region DependencyProperty
+
+
+        /// <summary> Gets or set the state for <see cref="ToolListView"/>. </summary>
+        public bool IsShow
+        {
+            get => (bool)base.GetValue(IsShowProperty);
+            set => base.SetValue(IsShowProperty, value);
+        }
+        /// <summary> Identifies the <see cref = "ToolListView.IsShow" /> dependency property. </summary>
+        public static readonly DependencyProperty IsShowProperty = DependencyProperty.Register(nameof(IsShow), typeof(bool), typeof(ToolListView), new PropertyMetadata(true, (sender, e) =>
+        {
+            ToolListView control = (ToolListView)sender;
+
+            if (e.NewValue is bool value)
+            {
+                if (value)
+                {
+                    if (control.IsExpanded)
+                        control.ExpandStoryboard.Begin();
+                    else
+                        control.NoneStoryboard.Begin();
+                }
+                else
+                    control.HideStoryboard.Begin();
+            }
+        }));
+
+
+        #endregion
+
+        //@Construct
+        public ToolListView()
+        {
+            this.InitializeComponent();
+
+            this.SplitButton.ManipulationStarted += (s, e) =>
+            {
+                this.StartingX = this.TranslateTransform.X;
+                switch (this.Placement)
+                {
+                    case SplitViewPanePlacement.Left: this.SplitIcon.Symbol = Symbol.AlignLeft; break;
+                    case SplitViewPanePlacement.Right: this.SplitIcon.Symbol = Symbol.AlignRight; break;
+                }
+                this.SplitButton.IsEnabled = false;
+            };
+            this.SplitButton.ManipulationDelta += (s, e) =>
+            {
+                this.TranslateTransform.X = this.GetX(this.StartingX + e.Cumulative.Translation.X);
+                switch (this.Placement)
+                {
+                    case SplitViewPanePlacement.Left: this.SplitIcon.Symbol = Symbol.AlignLeft; break;
+                    case SplitViewPanePlacement.Right: this.SplitIcon.Symbol = Symbol.AlignRight; break;
+                }
+            };
+            this.SplitButton.ManipulationCompleted += (s, e) =>
+            {
+                this.IsExpanded = this.Placement is SplitViewPanePlacement.Right;
+                this.SplitIcon.Symbol = Symbol.GlobalNavigationButton;
+                this.SplitButton.IsEnabled = true;
+            };
+
+            this.SplitButton.Click += (s, e) => this.IsExpanded = this.Placement is SplitViewPanePlacement.Left;
+
+            this.ListView.ItemClick += (s, e) =>
+            {
+                if (this.ItemClick is null) return;
+
+                if (e.ClickedItem is ToolType item)
+                {
+                    this.ItemClick(this.ToolCollection[item], item); // Delegate
+                }
+            };
+        }
+
+        //@Strings
+        public void ConstructStrings(ResourceLoader resource)
+        {
+        }
+
+        public void Construct(ToolType type)
+        {
+            this.ListView.SelectedIndex = this.ToolCollection.IndexOf(type);
+
+            this.ItemClick?.Invoke(this.ToolCollection[type], type); // Delegate
+        }
+
+    }
+}
