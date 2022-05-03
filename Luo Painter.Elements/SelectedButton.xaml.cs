f2cc3a74e637cea24055eb0456e66502eaf708ed
ysdy44 <ysdy44@outlook.com>
2022-05-03 19:06:51 +0800

    LayerListView Add SelectedButton

--- Luo Painter.Elements/SelectedButton.xaml.cs
+++ Luo Painter.Elements/SelectedButton.xaml.cs
@@ -5,12 +5,19 @@ using Windows.UI.Xaml.Media;
 
 namespace Luo_Painter.Elements
 {
-    public sealed class SelectedButtonPresenter : ContentPresenter
+    // ContentPresenter : 
+    // In a ListViewItem,
+    // if it's based on a ContentPresenter or ContentControl,
+    // it crashes on dragging items.
+    //
+    // Grid : 
+    // OK, but why?
+    public sealed class SelectedButtonPresenter : Grid // ContentPresenter
     {
         public SelectedButtonPresenter()
         {
             // base.Unloaded += (s, e) => this.Content = null;
-            base.Loaded += (s, e) => this.Content = new SelectedButton(this);
+            base.Loaded += (s, e) => this.Children.Add(new SelectedButton(this));
         }
 
         //@Static
@@ -27,8 +34,8 @@ namespace Luo_Painter.Elements
     {
 
         /// <summary> Please binding the <see cref="SelectorItem.IsSelected" /> of Ancestor in SelectedButton.xaml.</summary>
-        readonly ListViewItem Ancestor; 
-     
+        readonly ListViewItem Ancestor;
+
         internal SelectedButton(SelectedButtonPresenter presenter)
         {
             this.InitializeComponent();
