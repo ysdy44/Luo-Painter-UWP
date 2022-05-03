787d15a02b1be9971be73900e6d7dcb7149443e8
ysdy44 <ysdy44@outlook.com>
2022-05-03 11:22:26 +0800

    Add ToastTip

---
+++ Luo Painter/Controls/ToastTip.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Luo_Painter.Controls
+{
+    public sealed partial class ToastTip : UserControl
+    {
+
+        readonly DispatcherTimer Timer = new DispatcherTimer
+        {
+            Interval = TimeSpan.FromSeconds(1)
+        };
+        
+        //@Construct
+        public ToastTip()
+        {
+            this.InitializeComponent();
+            this.HideStoryboard.Completed += (s, e) =>
+            {
+                base.Visibility = Visibility.Collapsed;
+            };
+            this.Timer.Tick += (s, e) =>
+            {
+                this.Timer.Stop();
+                this.HideStoryboard.Begin(); // Storyboard
+            };
+        }
+
+        public void Tip(string title, string subtitle)
+        {
+            this.TitleTextBlock.Text = title;
+            this.SubtitleTextBlock.Text = subtitle;
+            base.Visibility = Visibility.Visible;
+
+            this.HideStoryboard.Stop(); // Storyboard
+            this.ShowStoryboard.Begin(); // Storyboard
+
+            this.Timer.Stop();
+            this.Timer.Start();
+        }
+
+    }
+}
