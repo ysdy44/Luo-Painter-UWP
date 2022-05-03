e9492416175c995120b6962c6fd81f899445eb9e
ysdy44 <ysdy44@outlook.com>
2022-05-03 11:22:52 +0800

    DrawPage Use ToastTip

--- Luo Painter/$DrawPages/DrawPage.Tip.cs
+++ Luo Painter/$DrawPages/DrawPage.Tip.cs
@@ -12,35 +12,13 @@ namespace Luo_Painter
     public sealed partial class DrawPage : Page
     {
 
-        readonly DispatcherTimer TipTimer = new DispatcherTimer
-        {
-            Interval = TimeSpan.FromSeconds(1)
-        };
-
         private void ConstructTip()
         {
-            this.HideTipStoryboard.Completed += (s, e) =>
-            {
-                this.TipBorder.Visibility = Visibility.Collapsed;
-            };
-            this.TipTimer.Tick += (s, e) =>
-            {
-                this.TipTimer.Stop();
-                this.HideTipStoryboard.Begin(); // Storyboard
-            };
         }
 
         public void Tip(string title, string subtitle)
         {
-            this.TipTitleTextBlock.Text = title;
-            this.TipSubtitleTextBlock.Text = subtitle;
-            this.TipBorder.Visibility = Visibility.Visible;
-
-            this.HideTipStoryboard.Stop(); // Storyboard
-            this.ShowTipStoryboard.Begin(); // Storyboard
-
-            this.TipTimer.Stop();
-            this.TipTimer.Start();
+            this.ToastTip.Tip(title, subtitle);
         }
 
 
