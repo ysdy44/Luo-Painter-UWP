cf09256dfc7ed448b3ce8b475c68adce3d08d5d5
ysdy44 <ysdy44@outlook.com>
2022-05-05 21:51:20 +0800

    DrawPage.Method Add AddAsync

--- Luo Painter/$DrawPages/DrawPage.Method.cs
+++ Luo Painter/$DrawPages/DrawPage.Method.cs
@@ -1,10 +1,14 @@
-﻿using Luo_Painter.Layers;
+﻿using Luo_Painter.Historys.Models;
+using Luo_Painter.Layers;
 using Luo_Painter.Layers.Models;
 using Microsoft.Graphics.Canvas;
 using Microsoft.Graphics.Canvas.Effects;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Numerics;
 using System.Threading.Tasks;
+using Windows.Storage;
 using Windows.Storage.Streams;
 using Windows.UI;
 using Windows.UI.Xaml;
@@ -117,6 +121,78 @@ namespace Luo_Painter
             }
             return background;
         }
+
+
+        public void Remove()
+        {
+            int index = this.LayerListView.SelectedIndex;
+            if (index < 0) return;
+            if (index + 1 > this.ObservableCollection.Count) return;
+
+            foreach (string id in this.Ids().ToArray())
+            {
+                if (this.Layers.ContainsKey(id))
+                {
+                    ILayer layer = this.Layers[id];
+                    this.ObservableCollection.Remove(layer);
+                }
+            }
+
+            int index2 = System.Math.Min(index, this.ObservableCollection.Count - 1);
+            this.LayerListView.SelectedIndex = index2;
+            this.LayerTool.SetLayer(this.LayerListView.SelectedItem as ILayer);
+
+            this.CanvasControl.Invalidate(); // Invalidate
+        }
+
+        public async void AddAsync(IEnumerable<IStorageFile> items)
+        {
+            if (items is null) return;
+            if (items.Count() == 0) return;
+
+            int count = 0;
+            int index = this.LayerListView.SelectedIndex;
+            string[] undo = this.ObservableCollection.Select(c => c.Id).ToArray();
+
+            foreach (IRandomAccessStreamReference item in items)
+            {
+                CanvasBitmap bitmap = null;
+                try
+                {
+                    using (IRandomAccessStreamWithContentType stream = await item.OpenReadAsync())
+                    {
+                        bitmap = await CanvasBitmap.LoadAsync(this.CanvasDevice, stream);
+                    }
+                }
+                catch (Exception)
+                {
+                }
+                if (bitmap is null) continue;
+
+                BitmapLayer bitmapLayer = new BitmapLayer(this.CanvasDevice, bitmap, this.Transformer.Width, this.Transformer.Height);
+                this.Layers.Add(bitmapLayer.Id, bitmapLayer);
+                if (index >= 0)
+                    this.ObservableCollection.Insert(index + count, bitmapLayer);
+                else
+                    this.ObservableCollection.Add(bitmapLayer);
+
+                count++;
+            }
+
+            if (count == 0) return;
+            if (count > 1) this.Tip("Add Images", $"{count}"); // Tip
+
+            // History
+            string[] redo = this.ObservableCollection.Select(c => c.Id).ToArray();
+            int removes = this.History.Push(new ArrangeHistory(undo, redo));
+
+            this.CanvasControl.Invalidate(); // Invalidate
+
+            this.UndoButton.IsEnabled = this.History.CanUndo;
+            this.RedoButton.IsEnabled = this.History.CanRedo;
+            this.LayerListView.SelectedIndex = System.Math.Max(0, index);
+        }
+
 
         public async Task<CanvasBitmap> AddAsync(IRandomAccessStreamReference reference)
         {
