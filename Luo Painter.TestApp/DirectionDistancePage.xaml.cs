42fe73540aa498f6344a716fe30cadf39909a072
chifanxunjue <ludingzhi158@live.com>
2022-05-06 21:48:12 +0800

    DirectionDistancePage.xaml.cs: change SDF,add BlackOrWhiteEffect

--- Luo Painter.TestApp/DirectionDistancePage.xaml.cs
+++ Luo Painter.TestApp/DirectionDistancePage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Luo_Painter.Shaders;
 using Microsoft.Graphics.Canvas;
 using Microsoft.Graphics.Canvas.Effects;
+using Microsoft.Graphics.Canvas.UI.Xaml;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -36,7 +37,11 @@ namespace Luo_Painter.TestApp
         int imageWidth;
         int imageHeight;
         DistanceData[,] distanceDatas;
+        CanvasRenderTarget originalTarget;
+        byte[] blackOrWhiteCode;
         double distance = 0.2;
+        float thresholse = 0;
+        double colorThresholdse = 128;
         public DirectionDistancePage()
         {
             this.InitializeComponent();
@@ -53,9 +58,10 @@ namespace Luo_Painter.TestApp
 
             action.Click += (s, e) =>
             {
-                if (originalImage == null)
+                if (originalTarget == null)
                     return;
-                GenarateDirectionDistanceEffect();
+                GenerateSDF();
+                //GenarateDirectionDistanceEffect();
             };
 
             export.Click += async (s, e) =>
@@ -80,7 +86,7 @@ namespace Luo_Painter.TestApp
                 }
                 };
                 StorageFile saveFile = await savePicker.PickSaveFileAsync();
-                if(saveFile != null)
+                if (saveFile != null)
                 {
                     var type = saveFile.FileType == ".png" ? CanvasBitmapFileFormat.Png : CanvasBitmapFileFormat.Jpeg;
                     using (IRandomAccessStream stream = await saveFile.OpenAsync(FileAccessMode.ReadWrite))
@@ -90,16 +96,39 @@ namespace Luo_Painter.TestApp
                     }
                 }
             };
+
+            thresholseSlide.ValueChanged += (s, e) =>
+            {
+                GetThresholdse();
+                DrawBlackOrWhiteTarget();
+            };
+
+            colorSlide.ValueChanged += (s, e) =>
+            {
+                GetColorThresholdse();
+            };
         }
-        private void InitCanvas()
+
+        void GetThresholdse()
         {
+            thresholse = (float)thresholseSlide.Value;
+        }
 
-
+        void GetColorThresholdse()
+        {
+            colorThresholdse = colorSlide.Value;
+        }
+        private async void InitCanvas()
+        {
+            blackOrWhiteCode = await ShaderType.BlackOrWhite.LoadAsync();
+            GetThresholdse();
             originalCanvas.Draw += (s, e) =>
             {
-                if (originalImage == null)
+                if (originalTarget == null)
                     return;
-                e.DrawingSession.DrawImage(originalImage);
+                var te = EvalImageCentered(originalCanvas.ActualWidth, originalCanvas.ActualHeight, imageWidth, imageHeight);
+                te.Source = originalTarget;
+                e.DrawingSession.DrawImage(te);
             };
             bool pressed = false;
             Point pos = new Point();
@@ -109,42 +138,48 @@ namespace Luo_Painter.TestApp
                 if (effectImage == null)
                     return;
 
+                var mat= EvalCenterdTransform(originalCanvas.ActualWidth, originalCanvas.ActualHeight, imageWidth, imageHeight);
                 var te = new Transform2DEffect()
                 {
                     Source = effectImage,
-                    TransformMatrix = Matrix3x2.CreateScale(scale),
+                    TransformMatrix = mat,
                 };
-
                 e.DrawingSession.DrawImage(te);
-                if (pressed)
-                {
-                    int x = (int)Math.Round(pos.X);
-                    int y = (int)Math.Round(pos.Y);
-                    if (x > imageWidth - 1 || y > imageHeight - 1)
-                        return;
-                    var data = distanceDatas[x, y];
-                    if (data.RecentItem is null)
-                        return;
-                    var p = data.RecentItem.Position;
-                    var startPosition = new Vector2(x, y);
-                    var endPosition = new Vector2(data.Recent.X * imageWidth, data.Recent.Y * imageHeight);
+                //if (pressed)
+                //{
+                   
 
-                    e.DrawingSession.DrawLine(startPosition, endPosition, Colors.Blue, 2f);
 
-                }
+                //    var p = Vector2.Transform(pos.ToVector2(), mat);
+                    
+                //    int x = (int)Math.Round(p.X);
+                //    int y = (int)Math.Round(p.Y);
+
+                    
+
+                //    if (x > imageWidth - 1 || y > imageHeight - 1)
+                //        return;
+                //    var data = distanceDatas[x, y];
+                //    if (data.RecentItem is null)
+                //        return;
+                  
+                //    var startPosition = new Vector2(x, y);
+                //    var endPosition = new Vector2(data.Recent.X * imageWidth, data.Recent.Y * imageHeight);
+                //    e.DrawingSession.DrawLine(startPosition, endPosition, Colors.Blue, 2f);
+                //}
             };
 
+            //弃用
             effectCanvas.PointerWheelChanged += (s, e) =>
             {
-
-                FrameworkElement element = (FrameworkElement)s;
-                var dir = e.GetCurrentPoint(element).Properties.MouseWheelDelta;
-                //缩放
-                if (e.KeyModifiers == Windows.System.VirtualKeyModifiers.Control)
-                {
-                    scale += dir > 0 ? 0.1f : -0.1f;
-                }
-                effectCanvas.Invalidate();
+                //FrameworkElement element = (FrameworkElement)s;
+                //var dir = e.GetCurrentPoint(element).Properties.MouseWheelDelta;
+                ////缩放
+                //if (e.KeyModifiers == Windows.System.VirtualKeyModifiers.Control)
+                //{
+                //    scale += dir > 0 ? 0.1f : -0.1f;
+                //}
+                //effectCanvas.Invalidate();
             };
 
             effectCanvas.PointerPressed += (s, e) =>
@@ -158,7 +193,6 @@ namespace Luo_Painter.TestApp
                 pressed = false;
                 effectCanvas.Invalidate();
             };
-
             effectCanvas.PointerMoved += (s, e) =>
             {
                 if (pressed)
@@ -174,7 +208,7 @@ namespace Luo_Painter.TestApp
 
         void GenarateDirectionDistanceEffect()
         {
-            var sourceColor = originalImage.GetPixelColors();
+            var sourceColor = originalTarget.GetPixelColors();
             var targetColor = new Color[sourceColor.Length];
             distance = disSlider.Value / 100d;
             for (int x = 0; x < imageWidth; x++)
@@ -294,7 +328,7 @@ namespace Luo_Painter.TestApp
 
                     if (item.Distance < double.PositiveInfinity)
                     {
-                        Color color = ShaderFunction.Lerp(Colors.Black, Colors.White, ShaderFunction.Smoothstep(0, distance, item.Distance));
+                        Color color = ShaderFunction.Lerp(Colors.Gray, Colors.White, ShaderFunction.Smoothstep(0, distance, item.Distance));
 
                         targetColor[index] = color;
                     }
@@ -304,7 +338,108 @@ namespace Luo_Painter.TestApp
             effectCanvas.Invalidate();
         }
 
+        void GenerateSDF()
+        {
+            var sourceColor = originalTarget.GetPixelColors();
+            var targetColor = new Color[sourceColor.Length];
+            distance = disSlider.Value / 100d;
 
+            SDFData[,] grid1 = new SDFData[imageWidth, imageHeight], grid2 = new SDFData[imageWidth, imageHeight];
+
+            for (int x = 0; x < imageWidth; x++)
+            {
+                for (int y = 0; y < imageHeight; y++)
+                {
+                    var index = x+imageWidth*y;
+                    var col = sourceColor[index];
+                    if (col.G < 128)
+                    {
+                        grid1[x, y] = new SDFData(0, 0);
+                        grid2[x, y] = new SDFData(9999, 9999);
+                    }
+                    else
+                    {
+                        grid1[x, y] = new SDFData(9999, 9999);
+                        grid2[x, y] = new SDFData(0, 0);
+                    }
+                }
+            }
+
+            GenerateSDF(ref grid1);
+            GenerateSDF(ref grid2);
+            int d =(int) disSlider.Value ;
+
+            for (int y = 0; y < imageHeight; y++)
+            {
+                for (int x = 0; x < imageWidth; x++)
+                {
+                    int dist1 = (int)(Math.Sqrt((double)grid1[x, y].DistSq()));
+                    int dist2 = (int)(Math.Sqrt((double)grid2[x, y].DistSq()));
+                    int dist = dist1 - dist2;
+                    int c = dist*d + (int)colorThresholdse;
+                    if (c < 0) c = 0;
+                    if(c > 255) c = 255;
+                    targetColor[x + y * imageWidth] = Color.FromArgb(255, (byte)c, (byte)c, (byte)c);
+                }
+            }
+
+            effectImage = CanvasBitmap.CreateFromColors(effectCanvas, targetColor, imageWidth, imageHeight);
+            effectCanvas.Invalidate();
+        }
+
+        void GenerateSDF(ref SDFData[,] datas)
+        {
+            for (int y = 0; y < imageHeight; y++)
+            {
+                for (int x = 0; x < imageWidth; x++)
+                {
+                    Compare(ref datas, ref datas[x, y], x, y, -1, 0);
+                    Compare(ref datas, ref datas[x, y], x, y, 0, -1);
+                    Compare(ref datas, ref datas[x, y], x, y, -1, -1);
+                    Compare(ref datas, ref datas[x, y], x, y, 1, 1);
+                }
+
+                for (int x= imageWidth - 1; x >= 0; x--)
+                {
+                    Compare(ref datas, ref datas[x, y], x, y, 1, 0);
+
+                }
+            }
+
+            for (int y = imageHeight-1;y>= 0; y--)
+            {
+                for (int x = imageWidth-1;x>=0; x--)
+                {
+          
+                    Compare(ref datas, ref datas[x, y], x, y, 1, 0);
+                    Compare(ref datas, ref datas[x, y], x, y, 0, 1);
+                    Compare(ref datas, ref datas[x, y], x, y, -1, 1);
+                    Compare(ref datas,ref datas[x, y], x, y, 1, 1);
+ 
+                }
+                for (int x = imageWidth - 1; x >= 0; x--)
+                {
+                    Compare(ref datas, ref datas[x, y], x, y, -1, 0);
+                }
+            }
+        }
+
+        void Compare(ref SDFData[,] datas,ref SDFData data,int x,int y,int ox,int oy)
+        {
+
+            var other = Get(ref datas, x+ox, y+oy);
+            other.Dx += ox;
+            other.Dy += oy;
+            if (other.DistSq() < data.DistSq())
+                datas[x, y] = other;
+        }
+
+        SDFData Get(ref SDFData[,] datas,int x,int y)
+        {
+            if (x >= 0 && y >= 0 && x < imageWidth && y < imageHeight)
+                return datas[x, y];
+            return new SDFData(9999, 9999);
+        }
 
         /// <summary>
         /// 查找最近
@@ -312,7 +447,6 @@ namespace Luo_Painter.TestApp
         void FindTheNearest(int x, int y, int tx, int ty, DistanceData[,] distanceDatas)
         {
             var self = distanceDatas[x, y];
-
             var neighbor = distanceDatas[tx, ty];
             //有颜色即是黑色
             if (neighbor.Color.R < 255)
@@ -349,12 +483,12 @@ namespace Luo_Painter.TestApp
             var file = await PickSingleImageFileAsync(PickerLocationId.Desktop);
             if (file != null)
                 originalImage = await CanvasBitmap.LoadAsync(CanvasDevice.GetSharedDevice(), await file.OpenReadAsync());
-
+            originalTarget = new CanvasRenderTarget(originalCanvas, originalImage.Size);
+            DrawBlackOrWhiteTarget();
             imageWidth = (int)originalImage.SizeInPixels.Width;
             imageHeight = (int)originalImage.SizeInPixels.Height;
             distanceDatas = new DistanceData[imageWidth, imageHeight];
-            effectCanvas.Invalidate();
-            originalCanvas.Invalidate();
+
         }
         public async static Task<StorageFile> PickSingleImageFileAsync(PickerLocationId location)
         {
@@ -377,6 +511,65 @@ namespace Luo_Painter.TestApp
             return file;
         }
 
+        async void DrawBlackOrWhiteTarget()
+        {
+            if (originalTarget == null)
+                return;
+            if (blackOrWhiteCode == null)
+            {
+                blackOrWhiteCode = await ShaderType.BlackOrWhite.LoadAsync();
+
+            }
+            PixelShaderEffect pse = new PixelShaderEffect(blackOrWhiteCode)
+            {
+                Source1 = originalImage,
+                Properties = { ["threshold"] = thresholse }
+            };
+            using (var ds = originalTarget.CreateDrawingSession())
+            {
+                ds.Clear(Colors.Transparent);
+                ds.DrawImage(pse);
+            }
+            originalCanvas.Invalidate();
+        }
+
+
+        public static Transform2DEffect EvalImageCentered(double canvasWidth, double canvasHeight, double imageWidth, double imageHeight)
+        {
+            var matrix3X2 = EvalCenterdTransform(canvasWidth, canvasHeight, imageWidth, imageHeight);
+            Transform2DEffect te = new Transform2DEffect()
+            {
+                TransformMatrix = matrix3X2
+            };
+            return te;
+        }
+
+        public static Matrix3x2 EvalCenterdTransform(double canvasWidth, double canvasHeight, double imageWidth, double imageHeight)
+        {
+            float f = (float)Math.Min(canvasWidth / imageWidth, canvasHeight / imageWidth);
+            float ox = (float)(canvasWidth - imageWidth * f) / 2;
+            float oy = (float)(canvasHeight - imageHeight * f) / 2;
+            Matrix3x2 matrix3X2 = Matrix3x2.CreateScale(f) * Matrix3x2.CreateTranslation(ox, oy);
+            return matrix3X2;
+        }
+    }
+
+    public struct SDFData
+    {
+        public int Dx;
+        public int Dy;
+
+        public SDFData(int x,int y)
+        {
+            Dx = x;
+            Dy = y;
+        }
+
+        public int DistSq()
+        {
+            return Dx * Dx + Dy * Dy;
+        }
+        
     }
 
     public class DistanceData
