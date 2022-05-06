3ce4fd5e00d3b57104a2448a3db3deb38df4c237
ysdy44 <ysdy44@outlook.com>
2022-05-06 08:47:43 +0800

    Add ExpanderPage

---
+++ Luo Painter.TestApp/ExpanderPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace Luo_Painter.TestApp
+{
+    public sealed partial class TitelGrid : Grid
+    {
+        UIElement Root;
+        double X;
+        double Y;
+        public TitelGrid()
+        {
+            base.Unloaded += (s, e) => this.Root = null;
+            base.Loaded += (s, e) => this.Root = base.Parent as UIElement;
+            base.ManipulationMode = ManipulationModes.TranslateX | ManipulationModes.TranslateY;
+            base.ManipulationStarted += (s, e) =>
+            {
+                this.X = Canvas.GetLeft(this.Root);
+                this.Y = Canvas.GetTop(this.Root);
+            };
+            base.ManipulationDelta += (s, e) =>
+            {
+                Canvas.SetLeft(this.Root, this.X + e.Cumulative.Translation.X);
+                Canvas.SetTop(this.Root, this.Y + e.Cumulative.Translation.Y);
+            };
+        }
+    }
+
+    public sealed partial class ExpanderPage : Page
+    {
+        public ExpanderPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
