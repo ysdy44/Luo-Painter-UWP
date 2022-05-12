eb1b8a9ce1380773296baaddfd02814d6cc29c83
ysdy44 <ysdy44@outlook.com>
2022-05-12 21:20:04 +0800

    EditButton Add FullScreen

--- Luo Painter/Controls/EditButton.xaml.cs
+++ Luo Painter/Controls/EditButton.xaml.cs
@@ -2,6 +2,7 @@
 using Luo_Painter.Elements;
 using System;
 using Windows.ApplicationModel.Resources;
+using Windows.Foundation;
 using Windows.Graphics.Imaging;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -10,110 +11,125 @@ namespace Luo_Painter.Controls
 {
     internal class EditTypeCommand : RelayCommand<EditType> { }
 
+    public struct EditPosition
+    {
+        public uint Index;
+        public uint X;
+        public uint Y;
+    }
+
     public sealed partial class EditButton : Canvas
     {
 
-        readonly BitmapSize HSize = new BitmapSize { Width = 400, Height = 412 };
-        readonly BitmapSize HItemSize = new BitmapSize { Width = 128, Height = 40 };
-        readonly BitmapSize[] HCoordinates = new BitmapSize[]
+        readonly uint HWidth = 400;
+        readonly uint HHeight = 412;
+        readonly EditPosition[] HCoordinates = new EditPosition[]
         {
-            new BitmapSize{ Width = 4, Height = 4 }, /// <see cref="EditGroupType.Edit"/>
-            new BitmapSize{ Width = 276, Height = 248 }, /// <see cref="EditGroupType.Group"/>
-            new BitmapSize{ Width = 140, Height = 4 }, /// <see cref="EditGroupType.Select"/>
-            new BitmapSize{ Width = 276, Height = 4 }, /// <see cref="EditGroupType.Combine"/>
+            new EditPosition{ Index = 0, X = 4 , Y = 4 }, /// <see cref="EditGroupType.Edit"/>
+            new EditPosition{ Index = 2, X = 4 , Y = 248 }, /// <see cref="EditGroupType.Group"/>
+            new EditPosition{ Index = 1, X = 4 , Y = 4 }, /// <see cref="EditGroupType.Select"/>
+            new EditPosition{ Index = 2, X = 4 , Y = 4 }, /// <see cref="EditGroupType.Combine"/>
 
-            new BitmapSize{ Width = 0, Height = 28 }, /// <see cref="EditType.Cut"/>
-            new BitmapSize{ Width = 0, Height = 70 }, /// <see cref="EditType.Duplicate"/>
-            new BitmapSize{ Width = 0, Height = 112 }, /// <see cref="EditType.Copy"/>
-            new BitmapSize{ Width = 0, Height = 154 }, /// <see cref="EditType.Paste"/>
-            new BitmapSize{ Width = 0, Height = 196 }, /// <see cref="EditType.Clear"/>
-            new BitmapSize{ Width = 0, Height = 238 }, /// <see cref="EditType.Remove"/>
-            new BitmapSize{ Width = 0, Height = 286 }, /// <see cref="EditType.Extract"/>
-            new BitmapSize{ Width = 0, Height = 328 }, /// <see cref="EditType.Merge"/>
-            new BitmapSize{ Width = 0, Height = 370 }, /// <see cref="EditType.Flatten"/>
+            new EditPosition{ Index = 0, Y = 28 }, /// <see cref="EditType.Cut"/>
+            new EditPosition{ Index = 0, Y = 70 }, /// <see cref="EditType.Duplicate"/>
+            new EditPosition{ Index = 0, Y = 112 }, /// <see cref="EditType.Copy"/>
+            new EditPosition{ Index = 0, Y = 154 }, /// <see cref="EditType.Paste"/>
+            new EditPosition{ Index = 0, Y = 196 }, /// <see cref="EditType.Clear"/>
+            new EditPosition{ Index = 0, Y = 238 }, /// <see cref="EditType.Remove"/>
+            new EditPosition{ Index = 0, Y = 286 }, /// <see cref="EditType.Extract"/>
+            new EditPosition{ Index = 0, Y = 328 }, /// <see cref="EditType.Merge"/>
+            new EditPosition{ Index = 0, Y = 370 }, /// <see cref="EditType.Flatten"/>
 
-            new BitmapSize{ Width = 272, Height = 271}, /// <see cref="EditType.Group"/>
-            new BitmapSize{ Width = 272, Height = 313 }, /// <see cref="EditType.Ungroup"/>
-            new BitmapSize{ Width = 272, Height = 361 }, /// <see cref="EditType.Release"/>
+            new EditPosition{ Index = 2, Y = 271}, /// <see cref="EditType.Group"/>
+            new EditPosition{ Index = 2, Y = 313 }, /// <see cref="EditType.Ungroup"/>
+            new EditPosition{ Index = 2, Y = 361 }, /// <see cref="EditType.Release"/>
 
-            new BitmapSize{ Width = 136, Height = 28 }, /// <see cref="EditType.All"/>
-            new BitmapSize{ Width = 136, Height = 70 }, /// <see cref="EditType.Deselect"/>
-            new BitmapSize{ Width = 136, Height = 112 }, /// <see cref="EditType.Invert"/>
-            new BitmapSize{ Width = 136, Height = 154 }, /// <see cref="EditType.Pixel"/>
-            new BitmapSize{ Width = 136, Height = 202 }, /// <see cref="EditType.Feather"/>
-            new BitmapSize{ Width = 136, Height = 244 }, /// <see cref="EditType.Transform"/>
-            new BitmapSize{ Width = 136, Height = 286 }, /// <see cref="EditType.Grow"/>
-            new BitmapSize{ Width = 136, Height = 328 }, /// <see cref="EditType.Shrink"/>
+            new EditPosition{ Index = 1, Y = 28 }, /// <see cref="EditType.All"/>
+            new EditPosition{ Index = 1, Y = 70 }, /// <see cref="EditType.Deselect"/>
+            new EditPosition{ Index = 1, Y = 112 }, /// <see cref="EditType.Invert"/>
+            new EditPosition{ Index = 1, Y = 154 }, /// <see cref="EditType.Pixel"/>
+            new EditPosition{ Index = 1, Y = 202 }, /// <see cref="EditType.Feather"/>
+            new EditPosition{ Index = 1, Y = 244 }, /// <see cref="EditType.Transform"/>
+            new EditPosition{ Index = 1, Y = 286 }, /// <see cref="EditType.Grow"/>
+            new EditPosition{ Index = 1, Y = 328 }, /// <see cref="EditType.Shrink"/>
 
-            new BitmapSize{ Width = 272, Height = 28 }, /// <see cref="EditType.Union"/>
-            new BitmapSize{ Width = 272, Height = 70 }, /// <see cref="EditType.Exclude"/>
-            new BitmapSize{ Width = 272, Height = 112 }, /// <see cref="EditType.Xor"/>
-            new BitmapSize{ Width = 272, Height = 154 }, /// <see cref="EditType.Intersect"/>
-            new BitmapSize{ Width = 272, Height = 202 }, /// <see cref="EditType.ExpandStroke"/>
+            new EditPosition{ Index = 2, Y = 28 }, /// <see cref="EditType.Union"/>
+            new EditPosition{ Index = 2, Y = 70 }, /// <see cref="EditType.Exclude"/>
+            new EditPosition{ Index = 2, Y = 112 }, /// <see cref="EditType.Xor"/>
+            new EditPosition{ Index = 2, Y = 154 }, /// <see cref="EditType.Intersect"/>
+            new EditPosition{ Index = 2, Y = 202 }, /// <see cref="EditType.ExpandStroke"/>
         };
 
-        readonly BitmapSize VEditSize = new BitmapSize { Width = 320, Height = 612 };
-        readonly BitmapSize VEditItemSize = new BitmapSize { Width = 156, Height = 40 };
-        readonly BitmapSize[] VEditCoordinates = new BitmapSize[]
+        readonly uint VWidth = 320;
+        readonly uint VHeight = 612;
+        readonly EditPosition[] VEditCoordinates = new EditPosition[]
         {
-            new BitmapSize{ Width = 4, Height = 4 }, /// <see cref="EditGroupType.Edit"/>
-            new BitmapSize{ Width = 4, Height = 415 }, /// <see cref="EditGroupType.Group"/>
-            new BitmapSize{ Width = 168, Height = 4 }, /// <see cref="EditGroupType.Select"/>
-            new BitmapSize{ Width = 168, Height = 373 }, /// <see cref="EditGroupType.Combine"/>
+            new EditPosition{ Index = 0, X = 4 , Y = 4 }, /// <see cref="EditGroupType.Edit"/>
+            new EditPosition{ Index = 0, X = 4 , Y = 415 }, /// <see cref="EditGroupType.Group"/>
+            new EditPosition{ Index = 1, X = 4 , Y = 4 }, /// <see cref="EditGroupType.Select"/>
+            new EditPosition{ Index = 1, X = 4 , Y = 373 }, /// <see cref="EditGroupType.Combine"/>
 
-            new BitmapSize{ Width = 0, Height = 28 }, /// <see cref="EditType.Cut"/>
-            new BitmapSize{ Width = 0, Height = 70 }, /// <see cref="EditType.Duplicate"/>
-            new BitmapSize{ Width = 0, Height = 112 }, /// <see cref="EditType.Copy"/>
-            new BitmapSize{ Width = 0, Height = 154 }, /// <see cref="EditType.Paste"/>
-            new BitmapSize{ Width = 0, Height = 196 }, /// <see cref="EditType.Clear"/>
-            new BitmapSize{ Width = 0, Height = 244 }, /// <see cref="EditType.Remove"/>
-            new BitmapSize{ Width = 0, Height = 286 }, /// <see cref="EditType.Extract"/>
-            new BitmapSize{ Width = 0, Height = 328 }, /// <see cref="EditType.Merge"/>
-            new BitmapSize{ Width = 0, Height = 370 }, /// <see cref="EditType.Flatten"/>
+            new EditPosition{ Index = 0, Y = 28 }, /// <see cref="EditType.Cut"/>
+            new EditPosition{ Index = 0, Y = 70 }, /// <see cref="EditType.Duplicate"/>
+            new EditPosition{ Index = 0, Y = 112 }, /// <see cref="EditType.Copy"/>
+            new EditPosition{ Index = 0, Y = 154 }, /// <see cref="EditType.Paste"/>
+            new EditPosition{ Index = 0, Y = 196 }, /// <see cref="EditType.Clear"/>
+            new EditPosition{ Index = 0, Y = 238 }, /// <see cref="EditType.Remove"/>
+            new EditPosition{ Index = 0, Y = 286 }, /// <see cref="EditType.Extract"/>
+            new EditPosition{ Index = 0, Y = 328 }, /// <see cref="EditType.Merge"/>
+            new EditPosition{ Index = 0, Y = 370 }, /// <see cref="EditType.Flatten"/>
 
-            new BitmapSize{ Width = 0, Height = 439 }, /// <see cref="EditType.Group"/>
-            new BitmapSize{ Width = 0, Height = 481 }, /// <see cref="EditType.Ungroup"/>
-            new BitmapSize{ Width = 0, Height = 529 }, /// <see cref="EditType.Release"/>
+            new EditPosition{ Index = 0, Y = 439 }, /// <see cref="EditType.Group"/>
+            new EditPosition{ Index = 0, Y = 481 }, /// <see cref="EditType.Ungroup"/>
+            new EditPosition{ Index = 0, Y = 529 }, /// <see cref="EditType.Release"/>
 
-            new BitmapSize{ Width = 164, Height = 28 }, /// <see cref="EditType.All"/>
-            new BitmapSize{ Width = 164, Height = 70 }, /// <see cref="EditType.Deselect"/>
-            new BitmapSize{ Width = 164, Height = 112 }, /// <see cref="EditType.Invert"/>
-            new BitmapSize{ Width = 164, Height = 154 }, /// <see cref="EditType.Pixel"/>
-            new BitmapSize{ Width = 164, Height = 202 }, /// <see cref="EditType.Feather"/>
-            new BitmapSize{ Width = 164, Height = 244 }, /// <see cref="EditType.Transform"/>
-            new BitmapSize{ Width = 164, Height = 286 }, /// <see cref="EditType.Grow"/>
-            new BitmapSize{ Width = 164, Height = 328 }, /// <see cref="EditType.Shrink"/>
+            new EditPosition{ Index = 1, Y = 28 }, /// <see cref="EditType.All"/>
+            new EditPosition{ Index = 1, Y = 70 }, /// <see cref="EditType.Deselect"/>
+            new EditPosition{ Index = 1, Y = 112 }, /// <see cref="EditType.Invert"/>
+            new EditPosition{ Index = 1, Y = 154 }, /// <see cref="EditType.Pixel"/>
+            new EditPosition{ Index = 1, Y = 202 }, /// <see cref="EditType.Feather"/>
+            new EditPosition{ Index = 1, Y = 244 }, /// <see cref="EditType.Transform"/>
+            new EditPosition{ Index = 1, Y = 286 }, /// <see cref="EditType.Grow"/>
+            new EditPosition{ Index = 1, Y = 328 }, /// <see cref="EditType.Shrink"/>
        
-            new BitmapSize{ Width = 164, Height = 397 }, /// <see cref="EditType.Union"/>
-            new BitmapSize{ Width = 164, Height = 439 }, /// <see cref="EditType.Exclude"/>
-            new BitmapSize{ Width = 164, Height = 481 }, /// <see cref="EditType.Xor"/>
-            new BitmapSize{ Width = 164, Height = 523 }, /// <see cref="EditType.Intersect"/>
-            new BitmapSize{ Width = 164, Height = 571 }, /// <see cref="EditType.ExpandStroke"/>
+            new EditPosition{ Index = 1, Y = 397 }, /// <see cref="EditType.Union"/>
+            new EditPosition{ Index = 1, Y = 439 }, /// <see cref="EditType.Exclude"/>
+            new EditPosition{ Index = 1, Y = 481 }, /// <see cref="EditType.Xor"/>
+            new EditPosition{ Index = 1, Y = 523 }, /// <see cref="EditType.Intersect"/>
+            new EditPosition{ Index = 1, Y = 571 }, /// <see cref="EditType.ExpandStroke"/>
         };
 
-        public void Resizing(Orientation orientation)
+        public void Resizing(Orientation orientation, bool fullScreen)
         {
             switch (orientation)
             {
-                case Orientation.Vertical: this.Resizing(this.VEditSize, this.VEditItemSize, this.VEditCoordinates); break;
-                case Orientation.Horizontal: this.Resizing(this.HSize, this.HItemSize, this.HCoordinates); break;
-                default: break;
+                case Orientation.Vertical:
+                    base.Width = fullScreen ? double.NaN : this.VWidth;
+                    base.Height = this.VHeight;
+                    this.Resizing(this.VEditCoordinates, ((fullScreen ? base.ActualWidth : this.VWidth) - 8) / 2);
+                    break;
+                case Orientation.Horizontal:
+                    base.Width = fullScreen ? double.NaN : this.HWidth;
+                    base.Height = this.HHeight;
+                    this.Resizing(this.HCoordinates, ((fullScreen ? base.ActualWidth : this.HWidth) - 8 - 8) / 3);
+                    break;
+                default:
+                    break;
             }
         }
 
-        public void Resizing(BitmapSize size, BitmapSize itemSize, BitmapSize[] coordinates)
+        public void Resizing(EditPosition[] coordinates, double itemWidth)
         {
-            base.Width = size.Width;
-            base.Height = size.Height;
+            double column = itemWidth + 8;
+
             for (int i = 0; i < base.Children.Count; i++)
             {
                 FrameworkElement item = base.Children[i] as FrameworkElement;
-                item.Width = itemSize.Width;
-                item.Height = itemSize.Height;
+                item.Width = itemWidth;
 
-                BitmapSize p = coordinates[i];
-                Canvas.SetLeft(item, p.Width);
-                Canvas.SetTop(item, p.Height);
+                EditPosition p = coordinates[i];
+                Canvas.SetLeft(item, p.X + p.Index * column);
+                Canvas.SetTop(item, p.Y);
             }
         }
     }
@@ -166,25 +182,57 @@ namespace Luo_Painter.Controls
             get => (Orientation)base.GetValue(OrientationProperty);
             set => base.SetValue(OrientationProperty, value);
         }
-        /// <summary> Identifies the <see cref = "EditButton.Type" /> dependency property. </summary>
+        /// <summary> Identifies the <see cref = "EditButton.Orientation" /> dependency property. </summary>
         public static readonly DependencyProperty OrientationProperty = DependencyProperty.Register(nameof(Orientation), typeof(Orientation), typeof(EditButton), new PropertyMetadata(Orientation.Horizontal, (sender, e) =>
         {
             EditButton control = (EditButton)sender;
+            if (control.IsLoaded is false) return;
 
             if (e.NewValue is Orientation value)
             {
-                control.Resizing(value);
+                control.Resizing(value, control.FullScreen);
+            }
+        }));
+
+
+        /// <summary> Gets a state that indicates whether the <see cref = "EditButton" />. </summary>
+        public bool FullScreen
+        {
+            get => (bool)base.GetValue(TypeProperty);
+            set => base.SetValue(TypeProperty, value);
+        }
+        /// <summary> Identifies the <see cref = "EditButton.FullScreen" /> dependency property. </summary>
+        public static readonly DependencyProperty TypeProperty = DependencyProperty.Register(nameof(FullScreen), typeof(bool), typeof(EditButton), new PropertyMetadata(false, (sender, e) =>
+        {
+            EditButton control = (EditButton)sender;
+
+            if (e.NewValue is bool value)
+            {
+                if (control.IsLoaded) control.Resizing(control.Orientation, value);
+                if (value)
+                    control.SizeChanged += control.EditButton_SizeChanged;
+                else
+                    control.SizeChanged -= control.EditButton_SizeChanged;
             }
         }));
 
 
         #endregion
-        
+
         //@Construct
         public EditButton()
         {
             this.InitializeComponent();
-            this.Resizing(this.Orientation);
+            base.Loaded += (s, e) => this.Resizing(this.Orientation, this.FullScreen);
+        }
+
+        private void EditButton_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (e.NewSize == Size.Empty) return;
+            if (e.NewSize == e.PreviousSize) return;
+            if (e.NewSize.Width == e.PreviousSize.Width) return;
+
+            this.Resizing(this.Orientation, this.FullScreen);
         }
 
         //@Strings
