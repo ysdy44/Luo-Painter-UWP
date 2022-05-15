1a5f6ee90709a2fcd488e92b0f48698fe41265f2
ysdy44 <ysdy44@outlook.com>
2022-05-15 23:21:22 +0800

    Add FootType

---
+++ Luo Painter/FootType.cs
@@ -0,0 +1,33 @@
+﻿namespace Luo_Painter
+{
+    public enum FootType
+    {
+        None,
+
+        // Edit
+        Feather,
+        MarqueeTransform,
+        Grow,
+        Shrink,
+
+        // Option
+        Exposure,
+        Brightness,
+        Saturation,
+        HueRotation,
+        Contrast,
+        Temperature,
+        HighlightsAndShadows,
+        LuminanceToAlpha,
+
+        // More Option
+        Transform,
+        GradientMapping,
+        RippleEffect,
+
+        // Tool
+        Marquee,
+        Selection,
+        Paint,
+    }
+}
