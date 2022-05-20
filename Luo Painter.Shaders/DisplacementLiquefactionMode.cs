27b69621995d98a200caa5d65138a77002358ba9
ysdy44 <ysdy44@outlook.com>
2022-05-20 21:15:59 +0800

    Add DisplacementLiquefactionMode

---
+++ Luo Painter.Shaders/DisplacementLiquefactionMode.cs
@@ -0,0 +1,15 @@
+﻿namespace Luo_Painter.TestApp
+{
+    public enum DisplacementLiquefactionMode : int
+    {
+        Reset = 0,
+
+        Push = 1,
+
+        Pinch = 2,
+        Expand = 3,
+
+        TwirlLeft = 4,
+        TwirlRight = 5,
+    }
+}
