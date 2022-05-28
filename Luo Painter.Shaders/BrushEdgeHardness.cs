1a443c7723321b6e1b9b221982d0fbeb47af45fc
ysdy44 <ysdy44@outlook.com>
2022-05-28 13:09:24 +0800

    Add BrushEdgeHardness

---
+++ Luo Painter.Shaders/BrushEdgeHardness.cs
@@ -0,0 +1,23 @@
+﻿namespace Luo_Painter.Shaders
+{
+    /// <summary>
+    /// Hardness of the brush edge.
+    /// </summary>
+    public enum BrushEdgeHardness : int
+    {
+        /// <summary> f (x) = 1 </summary>
+        None = 0,
+
+        /// <summary> f (x) = cos(x) </summary>
+        Cosine = 1,
+
+        /// <summary> f (x) = cos(x2) </summary>
+        Quadratic = 2,
+
+        /// <summary> f (x) = cos(x3) </summary>
+        Cube = 3,
+
+        /// <summary> f (x) = cos(x4) </summary>
+        Quartic = 4,
+    }
+}
