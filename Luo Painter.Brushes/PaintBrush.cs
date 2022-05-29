81537f2556bb8ae3ba299279237c294a89f35d78
ysdy44 <ysdy44@outlook.com>
2022-05-29 13:45:42 +0800

    Add PaintBrush

---
+++ Luo Painter.Brushes/PaintBrush.cs
@@ -0,0 +1,17 @@
+﻿namespace Luo_Painter.Brushes
+{
+    public sealed class PaintBrush
+    {
+        public double Size { get; set; } = 12;
+        public double Opacity { get; set; } = 1;
+        public double Space { get; set; } = 0.25;
+        public BrushEdgeHardness Hardness { get; set; } = BrushEdgeHardness.None;
+
+        public string Render { get; set; }
+        public string Thumbnail { get; set; }
+        public string Source { get; set; }
+
+        public string Title { get; set; }
+        public string Subtitle => ((int)this.Size).ToString();
+    }
+}
