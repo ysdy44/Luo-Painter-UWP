7ea6ae40e2d4911570c4bc3ee9325dc7ec81b5d1
ysdy44 <ysdy44@outlook.com>
2022-06-02 23:45:27 +0800

    Add PaintTexture

---
+++ Luo Painter.Brushes/PaintTexture.cs
@@ -0,0 +1,10 @@
+﻿namespace Luo_Painter.Brushes
+{
+    public sealed class PaintTexture
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public string Thumbnail { get; set; }
+        public string Source { get; set; }
+    }
+}
