923a88f0a171e22f7ef307a3ba3ee18aeca4f58f
ysdy44 <ysdy44@outlook.com>
2022-06-04 02:14:59 +0800

    BrushEdgeHardnessWithTexture.hlsl supports Rotation

--- Luo Painter.Shaders/BrushEdgeHardness.hlsl
+++ Luo Painter.Shaders/BrushEdgeHardness.hlsl
@@ -1,4 +1,4 @@
-#define D2D_INPUT_COUNT 0
+#define D2D_INPUT_COUNT 1
 #define D2D_INPUT0_COMPLEX
 #define D2D_REQUIRES_SCENE_POSITION
 #include "d2d1effecthelpers.hlsli"
@@ -16,6 +16,24 @@ float Cosine(float value)
     return cos(value * 3.14159274) / 2 + 0.5;
 }
 
+float GetRadiusAlpha(float x)
+{
+    // Cosine
+    if(hardness == 1) return Cosine(x);
+    
+    // Quadratic
+    if(hardness == 2) return  Cosine(x*x);
+    
+    // Cube
+    if(hardness == 3) return  Cosine(x*x*x);
+    
+    // Quartic
+    if(hardness == 4) return  Cosine(x*x*x*x);
+
+    // None
+    return 1;
+}
+
 D2D_PS_ENTRY(main)
 {
     float2 p = D2DGetScenePosition().xy;
@@ -27,20 +45,7 @@ D2D_PS_ENTRY(main)
     // None
     if(hardness == 0) return float4(color.r, color.g, color.b, pressure);
 
-    float x = dist / radius;
-    
-    // Cosine
-    if(hardness == 1) return float4(color.r, color.g, color.b, pressure * Cosine(x));
-    
-    // Quadratic
-    if(hardness == 2) return float4(color.r, color.g, color.b, pressure * Cosine(x*x));
-    
-    // Cube
-    if(hardness == 3) return float4(color.r, color.g, color.b, pressure * Cosine(x*x*x));
-    
-    // Quartic
-    if(hardness == 4) return float4(color.r, color.g, color.b, pressure * Cosine(x*x*x*x));
-
-    // None
-    return float4(color.r, color.g, color.b, pressure);
+    // Alpha : 0~1
+    float a = pressure * GetRadiusAlpha(dist / radius);
+    return float4(color.r*a, color.g*a, color.b*a, a);
 }
--- Luo Painter.Shaders/BrushEdgeHardnessWithTexture.hlsl
+++ Luo Painter.Shaders/BrushEdgeHardnessWithTexture.hlsl
@@ -4,6 +4,8 @@
 #include "d2d1effecthelpers.hlsli"
 
 int hardness;
+bool rotate;
+float2 normalization;
 
 float4 color;
 float pressure;
@@ -16,6 +18,52 @@ float Cosine(float value)
     return cos(value * 3.14159274) / 2 + 0.5;
 }
 
+float GetRadiusAlpha(float x)
+{
+    // Cosine
+    if(hardness == 1) return Cosine(x);
+    
+    // Quadratic
+    if(hardness == 2) return  Cosine(x*x);
+    
+    // Cube
+    if(hardness == 3) return  Cosine(x*x*x);
+    
+    // Quartic
+    if(hardness == 4) return  Cosine(x*x*x*x);
+
+    // None
+    return 1;
+}
+
+float GetTextureAlpha(float2 p)
+{
+    // Vector : -1~0~1
+    float vectX = (p.x - targetPosition.x) / radius;
+    float vectY = (p.y - targetPosition.y) / radius;
+    
+    if (rotate)
+    {
+        // Rotate: x <-> y
+        float sin2 = normalization.x;
+        float cos2 = normalization.y;
+
+        // Vector : -1~0~1
+        float vectX2 = vectX * cos2 - vectY * sin2;
+        float vectY2 = vectX * sin2 + vectY * cos2;
+
+        // Sample : 0~0.5~1
+        float2 sample = float2(vectX2 / 2 + 0.5, vectY2 / 2 + 0.5);
+        return D2DSampleInput(0, sample).a;
+    }
+    else
+    {
+        // Sample : 0~0.5~1
+        float2 sample = float2(vectX / 2 + 0.5, vectY / 2 + 0.5);
+        return D2DSampleInput(0, sample).a;
+    }
+}
+
 D2D_PS_ENTRY(main)
 {
     float2 p = D2DGetScenePosition().xy;
@@ -24,29 +72,14 @@ D2D_PS_ENTRY(main)
     float dist = distance(targetPosition, p);
 	if (dist > radius) return float4(0, 0, 0, 0);
 
-    // Texture : 0~1
-    float textureX = (p.x - targetPosition.x) / radius / 2 + 0.5;
-    float textureY = (p.y - targetPosition.y) / radius / 2 + 0.5;
-    float alpha = D2DSampleInput(0, float2(textureX, textureY)).a;
+    // Alpha : 0~1
+    float alpha = GetTextureAlpha(p);
     if(alpha == 0.0) return float4(0, 0, 0, 0);
 
     // None
     if(hardness == 0) return float4(color.r, color.g, color.b, alpha * pressure);
 
-    float x = dist / radius;
-    
-    // Cosine
-    if(hardness == 1) return float4(color.r*(alpha * pressure * Cosine(x)), color.g*(alpha * pressure * Cosine(x)), color.b*(alpha * pressure * Cosine(x)), alpha * pressure * Cosine(x));
-    
-    // Quadratic
-    if(hardness == 2) return float4(color.r*(alpha * pressure *  Cosine(x*x)), color.g*(alpha * pressure *  Cosine(x*x)), color.b*(alpha * pressure *  Cosine(x*x)), alpha * pressure * Cosine(x*x));
-    
-    // Cube
-    if(hardness == 3) return float4(color.r*(alpha * pressure *  Cosine(x*x*x)), color.g*(alpha * pressure *  Cosine(x*x*x)), color.b*(alpha * pressure *  Cosine(x*x*x)), alpha * pressure * Cosine(x*x*x));
-    
-    // Quartic
-    if(hardness == 4) return float4(color.r*(alpha * pressure *  Cosine(x*x*x*x)), color.g*(alpha * pressure *  Cosine(x*x*x*x)), color.b*(alpha * pressure *  Cosine(x*x*x*x)), alpha * pressure * Cosine(x*x*x*x));
-
-    // None
-    return float4(color.r*(alpha * pressure ), color.g*(alpha * pressure ), color.b*(alpha * pressure ), alpha * pressure);
+    // Alpha : 0~1
+    float a = alpha * pressure * GetRadiusAlpha(dist / radius);
+    return float4(color.r*a, color.g*a, color.b*a, a);
 }
