0f55a018a9e4950e31d2f343492738f9fd81f979
ysdy44 <ysdy44@outlook.com>
2022-06-08 22:29:27 +0800

    Add AdjustmentMenu

---
+++ Luo Painter/Luo Painter.Menus/AdjustmentMenu.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Luo_Painter.Elements;
+using Luo_Painter.Options;
+using System;
+using Windows.ApplicationModel.Resources;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace Luo_Painter.Menus
+{
+    internal sealed class AdjustmentGroupingList : GroupingList<AdjustmentGrouping, OptionType, OptionType> { }
+    internal sealed class AdjustmentGrouping : Grouping<OptionType, OptionType> { }
+
+    internal sealed class AdjustmentImage : TButton<OptionType>
+    {
+        protected override void OnTypeChanged(OptionType value)
+        {
+            base.Content = value.ToString();
+            // https://docs.microsoft.com/en-us/windows/uwp/debug-test-perf/optimize-animations-and-media
+            {
+                BitmapImage bitmap = new BitmapImage();
+                base.Background = new ImageBrush
+                {
+                    ImageSource = bitmap
+                };
+                bitmap.UriSource = new Uri(value.GetThumbnail());
+            }
+        }
+    }
+    
+    public sealed partial class AdjustmentMenu : Expander
+    {
+        //@Delegate
+        public event EventHandler<OptionType> ItemClick
+        {
+            remove => this.Command.Click -= value;
+            add => this.Command.Click += value;
+        }
+
+        //@Construct
+        public AdjustmentMenu()
+        {
+            this.InitializeComponent();
+        }
+
+        //@Strings
+        public void ConstructStrings(ResourceLoader resource)
+        {
+        }
+
+    }
+}
