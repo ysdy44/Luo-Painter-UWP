af32600af160a3c832641af1a53862f6a9c3659b
ysdy44 <ysdy44@outlook.com>
2022-06-13 20:49:57 +0800

    Add BitmapPixelBrushPage

---
+++ Luo Painter.TestApp/BitmapPixelBrushPage.xaml.cs
@@ -0,0 +1,154 @@
+﻿using Luo_Painter.Elements;
+using Luo_Painter.Layers.Models;
+using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.Effects;
+using System.Numerics;
+using Windows.UI;
+using Windows.UI.Xaml.Controls;
+
+namespace Luo_Painter.TestApp
+{
+    public sealed partial class BitmapPixelBrushPage : Page
+    {
+        readonly CanvasDevice Device = new CanvasDevice();
+        BitmapLayer BitmapLayer;
+
+        int X;
+        int Y;
+
+        int Step = 10;
+
+        public BitmapPixelBrushPage()
+        {
+            this.InitializeComponent();
+            this.ConstructInk();
+            this.ConstructCanvas();
+            this.ConstructOperator();
+        }
+
+
+        private void ConstructInk()
+        {
+            this.StepSlider.ValueChanged += (s, e) =>
+            {
+                this.Step = (int)e.NewValue;
+            };
+            this.ClearButton.Click += (s, e) =>
+            {
+                this.BitmapLayer.Clear(Colors.Transparent, BitmapType.Origin);
+                this.BitmapLayer.Clear(Colors.Transparent, BitmapType.Source);
+
+                this.CanvasControl.Invalidate(); // Invalidate
+                this.OriginCanvasControl.Invalidate(); // Invalidate
+                this.SourceCanvasControl.Invalidate(); // Invalidate
+                this.TempCanvasControl.Invalidate(); // Invalidate
+            };
+        }
+
+        private void ConstructCanvas()
+        {
+            this.CanvasControl.UseSharedDevice = true;
+            this.CanvasControl.CustomDevice = this.Device;
+            this.CanvasControl.CreateResources += (sender, args) =>
+            {
+                this.BitmapLayer = new BitmapLayer(this.Device, 512, 512);
+            };
+            this.CanvasControl.Draw += (sender, args) =>
+            {
+                //@DPI 
+                args.DrawingSession.Units = CanvasUnits.Pixels; /// <see cref="DPIExtensions">
+
+                args.DrawingSession.FillRectangle(0, 0, this.BitmapLayer.Width, this.BitmapLayer.Height, Colors.White);
+
+                args.DrawingSession.DrawImage(this.BitmapLayer.Source);
+            };
+
+
+            this.OriginCanvasControl.UseSharedDevice = true;
+            this.OriginCanvasControl.CustomDevice = this.Device;
+            this.OriginCanvasControl.Draw += (sender, args) =>
+            {
+                //@DPI 
+                args.DrawingSession.Units = CanvasUnits.Pixels; /// <see cref="DPIExtensions">
+
+                args.DrawingSession.Clear(Colors.White);
+                args.DrawingSession.DrawImage(new ScaleEffect
+                {
+                    Source = this.BitmapLayer.Origin,
+                    Scale = new Vector2(this.CanvasControl.Dpi.ConvertDipsToPixels(100) / 512)
+                });
+            };
+
+            this.SourceCanvasControl.UseSharedDevice = true;
+            this.SourceCanvasControl.CustomDevice = this.Device;
+            this.SourceCanvasControl.Draw += (sender, args) =>
+            {
+                //@DPI 
+                args.DrawingSession.Units = CanvasUnits.Pixels; /// <see cref="DPIExtensions">
+
+                args.DrawingSession.Clear(Colors.White);
+                args.DrawingSession.DrawImage(new ScaleEffect
+                {
+                    Source = this.BitmapLayer.Source,
+                    Scale = new Vector2(this.CanvasControl.Dpi.ConvertDipsToPixels(100) / 512)
+                });
+            };
+
+            this.TempCanvasControl.UseSharedDevice = true;
+            this.TempCanvasControl.CustomDevice = this.Device;
+            this.TempCanvasControl.Draw += (sender, args) =>
+            {
+                //@DPI 
+                args.DrawingSession.Units = CanvasUnits.Pixels; /// <see cref="DPIExtensions">
+
+                args.DrawingSession.Clear(Colors.White);
+                args.DrawingSession.DrawImage(new ScaleEffect
+                {
+                    Source = this.BitmapLayer.Temp,
+                    Scale = new Vector2(this.CanvasControl.Dpi.ConvertDipsToPixels(100) / 512)
+                });
+            };
+        }
+
+        private void ConstructOperator()
+        {
+            // Single
+            this.Operator.Single_Start += (point, properties) =>
+            {
+                this.X = -1;
+                this.Y = -1;
+
+                this.CanvasControl.Invalidate(); // Invalidate
+                this.OriginCanvasControl.Invalidate(); // Invalidate
+                this.SourceCanvasControl.Invalidate(); // Invalidate
+                this.TempCanvasControl.Invalidate(); // Invalidate
+            };
+            this.Operator.Single_Delta += (point, properties) =>
+            {
+                Vector2 position = this.CanvasControl.Dpi.ConvertDipsToPixels(point);
+                int x = (int)(position.X / this.Step);
+                int y = (int)(position.Y / this.Step);
+
+                if (this.X == x && this.Y == y) return;
+                using (CanvasDrawingSession ds = this.BitmapLayer.CreateDrawingSession())
+                {
+                    ds.FillRectangle(x * this.Step, y * this.Step, this.Step, this.Step, this.ColorPicker.Color);
+                }
+
+                this.X = x;
+                this.Y = y;
+
+                this.CanvasControl.Invalidate(); // Invalidate
+                this.OriginCanvasControl.Invalidate(); // Invalidate
+                this.SourceCanvasControl.Invalidate(); // Invalidate
+                this.TempCanvasControl.Invalidate(); // Invalidate
+            };
+            this.Operator.Single_Complete += (point, properties) =>
+            {
+                this.BitmapLayer.RenderThumbnail();
+                this.CanvasControl.Invalidate(); // Invalidate
+            };
+        }
+
+    }
+}
