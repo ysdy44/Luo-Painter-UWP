02a493809d32fd8b31754d2f9189443af5fcc2de
ysdy44 <ysdy44@outlook.com>
2022-06-14 08:35:42 +0800

    Add HistoryMenu

---
+++ Luo Painter/Luo Painter.Menus/HistoryMenu.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Luo_Painter.Elements;
+using Windows.ApplicationModel.Resources;
+
+namespace Luo_Painter.Menus
+{
+    public sealed partial class HistoryMenu : Expander
+    {
+
+        public object ItemsSource { set => this.ListView.ItemsSource = value; }
+
+        //@Construct
+        public HistoryMenu()
+        {
+            this.InitializeComponent();
+        }
+
+        //@Strings
+        public void ConstructStrings(ResourceLoader resource)
+        {
+        }
+
+    }
+}
