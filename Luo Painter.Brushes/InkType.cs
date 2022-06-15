e07919ca75e19602f15ad4c9f882eba5431f1b41
ysdy44 <ysdy44@outlook.com>
2022-06-15 22:20:54 +0800

    InkType Add Dry/Wet, Blend/Blur/Mix and Mask/Pattern/Opacity

--- Luo Painter.Brushes/InkType.cs
+++ Luo Painter.Brushes/InkType.cs
@@ -6,50 +6,139 @@ namespace Luo_Painter.Brushes
     public enum InkType
     {
         None = 0,
-        Pattern = 1,
-        Opacity = 2,
-        Blend = 4,
 
-        BrushDry = 16,
-        BrushWetPattern = BrushDry | Pattern,
-        BrushWetOpacity = BrushDry | Opacity,
-        BrushWetPatternOpacity = BrushDry | Pattern | Opacity,
-        BrushWetBlend = BrushDry | Blend,
-        BrushWetPatternBlend = BrushDry | Pattern | Blend,
-        BrushWetOpacityBlend = BrushDry | Opacity | Blend,
-        BrushWetPatternOpacityBlend = BrushDry | Pattern | Opacity | Blend,
+        // Dry
+        Dry = 1,
+        // Wet
+        Wet = 1 << 1,
+        WetBlur = 1 << 2,
+        // WetComposite
+        WetComposite = 1 << 3,
+        WetCompositeBlend = 1 << 4 | WetComposite,
+        WetCompositeEraseOpacity = 1 << 5 | WetComposite,
 
-        MaskBrushDry = 32,
-        MaskBrushWetPattern = MaskBrushDry | Pattern,
-        MaskBrushWetOpacity = MaskBrushDry | Opacity,
-        MaskBrushWetPatternOpacity = MaskBrushDry | Pattern | Opacity,
-        MaskBrushWetBlend = MaskBrushDry | Blend,
-        MaskBrushWetPatternBlend = MaskBrushDry | Pattern | Blend,
-        MaskBrushWetOpacityBlend = MaskBrushDry | Opacity | Blend,
-        MaskBrushWetPatternOpacityBlend = MaskBrushDry | Pattern | Opacity | Blend,
+        // Mode
+        Blend = 1 << 6,
+        Blur = 1 << 7,
+        Mix = 1 << 8,
 
-        CircleDry = 64,
-        CircleWetPattern = CircleDry | Pattern,
-        CircleWetOpacity = CircleDry | Opacity,
-        CircleWetPatternOpacity = CircleDry | Pattern | Opacity,
-        CircleWetBlend = CircleDry | Blend,
-        CircleWetPatternBlend = CircleDry | Pattern | Blend,
-        CircleWetOpacityBlend = CircleDry | Opacity | Blend,
-        CircleWetPatternOpacityBlend = CircleDry | Pattern | Opacity | Blend,
+        // Property
+        Mask = 1 << 9,
+        Pattern = 1 << 10,
+        Opacity = 1 << 11,
 
-        LineDry = 128,
-        LineWetPattern = LineDry | Pattern,
-        LineWetOpacity = LineDry | Opacity,
-        LineWetPatternOpacity = LineDry | Pattern | Opacity,
-        LineWetBlend = LineDry | Blend,
-        LineWetPatternBlend = LineDry | Pattern | Blend,
-        LineWetOpacityBlend = LineDry | Opacity | Blend,
-        LineWetPatternOpacityBlend = LineDry | Pattern | Opacity | Blend,
 
-        EraseDry = 256,
-        EraseWetOpacity = EraseDry | Opacity,
-        EraseWetPatternOpacity = EraseDry | Pattern | Opacity,
+        Brush = 1 << 12,
+        #region Brush
 
-        Liquefy = 512,
+
+        BrushDry = Brush | Dry,
+
+        BrushWetPattern = Brush | Wet | Pattern,
+        BrushWetOpacity = Brush | Wet | Opacity,
+        BrushWetPatternOpacity = Brush | Wet | Pattern | Opacity,
+
+        BrushWetBlend = Brush | WetCompositeBlend | Blend,
+        BrushWetPatternBlend = Brush | WetCompositeBlend | Pattern | Blend,
+        BrushWetOpacityBlend = Brush | WetCompositeBlend | Opacity | Blend,
+        BrushWetPatternOpacityBlend = Brush | WetCompositeBlend | Pattern | Opacity | Blend,
+
+        BrushWetBlur = Brush | WetBlur | Blur,
+        BrushWetPatternBlur = Brush | WetBlur | Pattern | Blur,
+
+        BrushDryMix = Brush | Dry | Mix,
+        BrushWetPatternMix = Brush | Wet | Pattern | Mix,
+
+
+        #endregion
+
+
+        #region MaskBrush
+
+
+        MaskBrushDry = Brush | Dry | Mask,
+
+        MaskBrushWetPattern = Brush | Wet | Mask | Pattern,
+        MaskBrushWetOpacity = Brush | Wet | Mask | Opacity,
+        MaskBrushWetPatternOpacity = Brush | Wet | Mask | Pattern | Opacity,
+
+        MaskBrushWetBlend = Brush | WetCompositeBlend | Mask | Blend,
+        MaskBrushWetPatternBlend = Brush | WetCompositeBlend | Mask | Pattern | Blend,
+        MaskBrushWetOpacityBlend = Brush | WetCompositeBlend | Mask | Opacity | Blend,
+        MaskBrushWetPatternOpacityBlend = Brush | WetCompositeBlend | Mask | Pattern | Opacity | Blend,
+
+        MaskBrushWetBlur = Brush | WetBlur | Mask | Blur,
+        MaskBrushWetPatternBlur = Brush | WetBlur | Mask | Pattern | Blur,
+
+        MaskBrushDryMix = Brush | Dry | Mask | Mix,
+        MaskBrushWetPatternMix = Brush | Wet | Mask | Pattern | Mix,
+
+
+        #endregion
+
+
+        Circle = 1 << 14,
+        #region Circle
+
+
+        CircleDry = Circle | Dry,
+
+        CircleWetPattern = Circle | Wet | Pattern,
+        CircleWetOpacity = Circle | Wet | Opacity,
+        CircleWetPatternOpacity = Circle | Wet | Pattern | Opacity,
+
+        CircleWetBlend = Circle | WetCompositeBlend | Blend,
+        CircleWetPatternBlend = Circle | WetCompositeBlend | Pattern | Blend,
+        CircleWetOpacityBlend = Circle | WetCompositeBlend | Opacity | Blend,
+        CircleWetPatternOpacityBlend = Circle | WetCompositeBlend | Pattern | Opacity | Blend,
+
+        CircleWetBlur = Circle | WetBlur | Blur,
+        CircleWetPatternBlur = Circle | WetBlur | Pattern | Blur,
+
+        CircleDryMix = Circle | Dry | Mix,
+        CircleWetPatternMix = Circle | Wet | Pattern | Mix,
+
+
+        #endregion
+
+
+        Line = 1 << 15,
+        #region Line
+
+
+        LineDry = Line | Dry,
+
+        LineWetPattern = Line | Wet | Pattern,
+        LineWetOpacity = Line | Wet | Opacity,
+        LineWetPatternOpacity = Line | Wet | Pattern | Opacity,
+
+        LineWetBlend = Line | WetCompositeBlend | Blend,
+        LineWetPatternBlend = Line | WetCompositeBlend | Pattern | Blend,
+        LineWetOpacityBlend = Line | WetCompositeBlend | Opacity | Blend,
+        LineWetPatternOpacityBlend = Line | WetCompositeBlend | Pattern | Opacity | Blend,
+
+        LineWetBlur = Line | WetBlur | Blur,
+        LineWetPatternBlur = Line | WetBlur | Pattern | Blur,
+
+        LineDryMix = Line | Dry | Mix,
+        LineWetPatternMix = Line | Wet | Pattern | Mix,
+
+
+        #endregion
+
+
+        Erase = 1 << 16,
+        #region Erase
+
+        EraseDry = Erase | Dry,
+
+        EraseWetOpacity = Erase | WetCompositeEraseOpacity | Opacity,
+        EraseWetPatternOpacity = Erase | WetCompositeEraseOpacity | Pattern | Opacity,
+
+
+        #endregion
+
+
+        Liquefy = 1 << 17 | Dry,
     }
 }
