dcff9eff5e44a35338391828c4d9efc004a0dec2
ysdy44 <ysdy44@outlook.com>
2022-06-19 13:14:06 +0800

    ArrangeHistory Add SetupSizes

--- Luo Painter.Historys/Models/ArrangeHistory.cs
+++ Luo Painter.Historys/Models/ArrangeHistory.cs
@@ -1,22 +1,32 @@
 ﻿using System;
+using Windows.Graphics.Imaging;
 
 namespace Luo_Painter.Historys.Models
 {
+    public sealed class SetupSizes
+    {
+        public BitmapSize UndoParameter;
+        public BitmapSize RedoParameter;
+    }
+
     public class ArrangeHistory : IHistory
     {
         public HistoryMode Mode => HistoryMode.Arrange;
         public HistoryType Type => HistoryType.None;
         public string[] UndoParameter { get; }
         public string[] RedoParameter { get; }
-        public ArrangeHistory(string[] undoParameter, string[] redoParameter)
+        public SetupSizes Sizes { get; private set; }
+        public ArrangeHistory(string[] undoParameter, string[] redoParameter, SetupSizes sizes = null)
         {
             this.UndoParameter = undoParameter;
             this.RedoParameter = redoParameter;
+            this.Sizes = sizes;
         }
         public void Dispose()
         {
             Array.Clear(this.UndoParameter, 0, this.UndoParameter.Length);
             Array.Clear(this.RedoParameter, 0, this.RedoParameter.Length);
+            this.Sizes = null;
         }
     }
 }
