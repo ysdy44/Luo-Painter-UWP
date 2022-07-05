adac8c944858c5532ffcc22c9da0e74e084b34bd
ysdy44 <ysdy44@outlook.com>
2022-07-05 23:31:15 +0800

    LayerObservableCollection Add GetSelectedIndex

--- Luo Painter.Layers/Layers/LayerObservableCollection.cs
+++ Luo Painter.Layers/Layers/LayerObservableCollection.cs
@@ -6,6 +6,43 @@ namespace Luo_Painter.Layers
     public sealed class LayerObservableCollection : ObservableCollection<ILayer>
     {
 
+        public int GetSelectedIndex(int index, int depth)
+        {
+            if (base.Count - 1 >= index)
+            {
+                ILayer item = base[index];
+                if (item.Depth == depth) return index; // Next
+            }
+
+            index--;
+            if (index < 0) return -1; // Empty
+
+            {
+                ILayer item = base[index];
+                if (item.Depth + 1 == depth) return index; // Parent
+            }
+
+            for (int i = index; i >= 0; i--)
+            {
+                ILayer item = base[i];
+                if (item.Depth == depth) return i; // Last
+            }
+
+            return 0; // ?
+        }
+
+        public ILayer GetParent(ILayer layer)
+        {
+            foreach (ILayer item in this)
+            {
+                foreach (ILayer chil in item.Children)
+                {
+                    if (chil.Id == layer.Id) return item;
+                }
+            }
+            return null;
+        }
+
         public void AddChild(ILayer layer)
         {
             base.Add(layer);
@@ -19,7 +56,7 @@ namespace Luo_Painter.Layers
             }
         }
 
-        private void RemoveChild(ILayer layer)
+        public void RemoveChild(ILayer layer)
         {
             base.Remove(layer);
             this.RemoveChildren(layer);
@@ -52,7 +89,7 @@ namespace Luo_Painter.Layers
             layer.Arrange(0);
             this.RemoveChildren(layer);
 
-            ILayer parent = this.FirstOrDefault(c => c.Children.Contains(layer));
+            ILayer parent = this.GetParent(layer);
             if (parent is null) return;
 
             parent.Children.Remove(layer);
@@ -72,7 +109,7 @@ namespace Luo_Painter.Layers
             layer.Arrange(neighbor.Depth);
             this.InsertChildren(index, layer);
 
-            ILayer parent = this.FirstOrDefault(c => c.Children.Contains(neighbor));
+            ILayer parent = this.GetParent(neighbor);
             if (parent is null) return;
 
             index = parent.Children.IndexOf(neighbor);
