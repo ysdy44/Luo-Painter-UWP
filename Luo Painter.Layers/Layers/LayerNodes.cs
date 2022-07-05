acf8da4f81b9a7b9bdf9a162a3809b0930bedb3e
ysdy44 <ysdy44@outlook.com>
2022-07-05 21:59:20 +0800

    Add LayerNodes

---
+++ Luo Painter.Layers/Layers/LayerNodes.cs
@@ -0,0 +1,17 @@
+﻿using Luo_Painter.Historys;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Luo_Painter.Layers
+{
+    public sealed class LayerNodes : List<ILayer>
+    {
+        public Layerage[] Convert() => LayerNodes.Convert(this);
+        private static Layerage[] Convert(IList<ILayer> layers) => (layers.Count is 0) ? null : layers.Select(LayerNodes.Convert).ToArray();
+        private static Layerage Convert(ILayer layer) => new Layerage
+        {
+            Id = layer.Id,
+            Children = LayerNodes.Convert(layer.Children)
+        };
+    }
+}
