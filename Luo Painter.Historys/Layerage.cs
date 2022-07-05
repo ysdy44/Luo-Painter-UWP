4cd1156e886a29a0c031b10cc39d8d10e701e140
ysdy44 <ysdy44@outlook.com>
2022-07-05 21:40:25 +0800

    Add Layerage

---
+++ Luo Painter.Historys/Layerage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Luo_Painter.Historys
+{
+    public sealed class Layerage : IDisposable
+    {
+
+        public string Id { get; set; }
+        public Layerage[] Children { get; set; }
+
+        public override string ToString() => this.Id;
+
+        public void Dispose()
+        {
+            this.Id = null;
+            if (this.Children is null) return;
+
+            foreach (Layerage item in this.Children)
+            {
+                item.Dispose();
+            }
+
+            Array.Clear(this.Children, 0, this.Children.Length);
+            this.Children = null;
+        }
+
+    }
+}
