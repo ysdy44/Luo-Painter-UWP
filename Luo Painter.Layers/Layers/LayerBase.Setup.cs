71ce2cb78300b8fbd51b8753a573c4f8b8b2ce2d
ysdy44 <ysdy44@outlook.com>
2022-07-07 23:53:17 +0800

    Add LayerBase.Setup

---
+++ Luo Painter.Layers/Layers/LayerBase.Setup.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.Graphics.Canvas;
+using System.Numerics;
+
+namespace Luo_Painter.Layers
+{
+    public abstract partial class LayerBase : IRender
+    {
+
+        protected abstract ILayer CloneSelf(ICanvasResourceCreator resourceCreator);
+
+        protected abstract ILayer CropSelf(ICanvasResourceCreator resourceCreator, int width, int height);
+        protected abstract ILayer CropSelf(ICanvasResourceCreator resourceCreator, int width, int height, Vector2 offset);
+        protected abstract ILayer CropSelf(ICanvasResourceCreator resourceCreator, int width, int height, Matrix3x2 matrix, CanvasImageInterpolation interpolation);
+
+        protected abstract ILayer SkretchSelf(ICanvasResourceCreator resourceCreator, int width, int height, CanvasImageInterpolation interpolation);
+
+        protected abstract ILayer FlipHorizontalSelf(ICanvasResourceCreator resourceCreator);
+        protected abstract ILayer FlipVerticalSelf(ICanvasResourceCreator resourceCreator);
+        protected abstract ILayer LeftTurnSelf(ICanvasResourceCreator resourceCreator);
+        protected abstract ILayer RightTurnSelf(ICanvasResourceCreator resourceCreator);
+        protected abstract ILayer OverTurnSelf(ICanvasResourceCreator resourceCreator);
+
+
+        public ILayer Clone(ICanvasResourceCreator resourceCreator)
+        {
+            ILayer layer = this.CloneSelf(resourceCreator);
+            layer.CopyWith(this);
+            foreach (ILayer item in this.Children) layer.Children.Add(item.Clone(resourceCreator));
+            return layer;
+        }
+
+        public ILayer Crop(ICanvasResourceCreator resourceCreator, int width, int height)
+        {
+            ILayer layer = this.CropSelf(resourceCreator, width, height);
+            layer.CopyWith(this);
+            foreach (ILayer item in this.Children) layer.Children.Add(item.Crop(resourceCreator, width, height));
+            return layer;
+        }
+        public ILayer Crop(ICanvasResourceCreator resourceCreator, int width, int height, Vector2 offset)
+        {
+            ILayer layer = this.CropSelf(resourceCreator, width, height, offset);
+            layer.CopyWith(this);
+            foreach (ILayer item in this.Children) layer.Children.Add(item.Crop(resourceCreator, width, height, offset));
+            return layer;
+        }
+        public ILayer Crop(ICanvasResourceCreator resourceCreator, int width, int height, Matrix3x2 matrix, CanvasImageInterpolation interpolation)
+        {
+            ILayer layer = this.CropSelf(resourceCreator, width, height, matrix, interpolation);
+            layer.CopyWith(this);
+            foreach (ILayer item in this.Children) layer.Children.Add(item.Crop(resourceCreator, width, height, matrix, interpolation));
+            return layer;
+        }
+
+        public ILayer Skretch(ICanvasResourceCreator resourceCreator, int width, int height, CanvasImageInterpolation interpolation)
+        {
+            ILayer layer = this.SkretchSelf(resourceCreator, width, height, interpolation);
+            layer.CopyWith(this);
+            foreach (ILayer item in this.Children) layer.Children.Add(item.Skretch(resourceCreator, width, height, interpolation));
+            return layer;
+        }
+
+        public ILayer FlipHorizontal(ICanvasResourceCreator resourceCreator)
+        {
+            ILayer layer = this.FlipHorizontalSelf(resourceCreator);
+            layer.CopyWith(this);
+            foreach (ILayer item in this.Children) layer.Children.Add(item.FlipHorizontal(resourceCreator));
+            return layer;
+        }
+        public ILayer FlipVertical(ICanvasResourceCreator resourceCreator)
+        {
+            ILayer layer = this.FlipVerticalSelf(resourceCreator);
+            layer.CopyWith(this);
+            foreach (ILayer item in this.Children) layer.Children.Add(item.FlipVertical(resourceCreator));
+            return layer;
+        }
+        public ILayer LeftTurn(ICanvasResourceCreator resourceCreator)
+        {
+            ILayer layer = this.LeftTurnSelf(resourceCreator);
+            layer.CopyWith(this);
+            foreach (ILayer item in this.Children) layer.Children.Add(item.LeftTurn(resourceCreator));
+            return layer;
+        }
+        public ILayer RightTurn(ICanvasResourceCreator resourceCreator)
+        {
+            ILayer layer = this.RightTurnSelf(resourceCreator);
+            layer.CopyWith(this);
+            foreach (ILayer item in this.Children) layer.Children.Add(item.RightTurn(resourceCreator));
+            return layer;
+        }
+        public ILayer OverTurn(ICanvasResourceCreator resourceCreator)
+        {
+            ILayer layer = this.OverTurnSelf(resourceCreator);
+            layer.CopyWith(this);
+            foreach (ILayer item in this.Children) layer.Children.Add(item.OverTurn(resourceCreator));
+            return layer;
+        }
+
+    }
+}
--- Luo Painter.Layers/Models/BitmapLayer.Setup.cs
+++ Luo Painter.Layers/Models/BitmapLayer.Setup.cs
@@ -7,41 +7,24 @@ namespace Luo_Painter.Layers.Models
     public sealed partial class BitmapLayer : LayerBase, ILayer
     {
 
-        public ILayer Clone(ICanvasResourceCreator resourceCreator)
+        protected override ILayer CloneSelf(ICanvasResourceCreator resourceCreator) => new BitmapLayer(resourceCreator, this);
+
+        protected override ILayer CropSelf(ICanvasResourceCreator resourceCreator, int width, int height ) => new BitmapLayer(resourceCreator, this.SourceRenderTarget, width, height);
+        protected override ILayer CropSelf(ICanvasResourceCreator resourceCreator, int width, int height, Vector2 offset) => new BitmapLayer(resourceCreator, this.SourceRenderTarget, width, height, offset);
+        protected override ILayer CropSelf(ICanvasResourceCreator resourceCreator, int width, int height, Matrix3x2 matrix, CanvasImageInterpolation interpolation) => new BitmapLayer(resourceCreator, new Transform2DEffect
         {
-            BitmapLayer layer = new BitmapLayer(resourceCreator, this);
-            layer.CopyWith(this);
-            return layer;
-        }
-        public ILayer Crop(ICanvasResourceCreator resourceCreator, int width, int height)
-        {
-            BitmapLayer layer = new BitmapLayer(resourceCreator, this.SourceRenderTarget, width, height);
-            layer.CopyWith(this);
-            return layer;
-        }
-        public ILayer Crop(ICanvasResourceCreator resourceCreator, int width, int height, Vector2 offset)
-        {
-            BitmapLayer layer = new BitmapLayer(resourceCreator, this.SourceRenderTarget, width, height, offset);
-            layer.CopyWith(this);
-            return layer;
-        }
-        public ILayer Crop(ICanvasResourceCreator resourceCreator, int width, int height, Matrix3x2 matrix, CanvasImageInterpolation interpolation)
-        {
-            BitmapLayer layer = new BitmapLayer(resourceCreator, new Transform2DEffect
-            {
-                Source = this.SourceRenderTarget,
-                TransformMatrix = matrix,
-                InterpolationMode = interpolation
-            }, width, height);
-            layer.CopyWith(this);
-            return layer;
-        }
-        public ILayer Skretch(ICanvasResourceCreator resourceCreator, int width, int height, CanvasImageInterpolation interpolation) => this.Crop(resourceCreator, width, height, Matrix3x2.CreateScale(width / (float)this.Width, height / (float)this.Height), interpolation);
-        public ILayer FlipHorizontal(ICanvasResourceCreator resourceCreator) => this.Crop(resourceCreator, this.Width, this.Height, Matrix3x2.CreateScale(-1, 1, this.Center), CanvasImageInterpolation.NearestNeighbor);
-        public ILayer FlipVertical(ICanvasResourceCreator resourceCreator) => this.Crop(resourceCreator, this.Width, this.Height, Matrix3x2.CreateScale(1, -1, this.Center), CanvasImageInterpolation.NearestNeighbor);
-        public ILayer LeftTurn(ICanvasResourceCreator resourceCreator) => this.Crop(resourceCreator, this.Height, this.Width, Matrix3x2.CreateRotation(-FanKit.Math.PiOver2) * Matrix3x2.CreateTranslation(0, this.Width), CanvasImageInterpolation.NearestNeighbor);
-        public ILayer RightTurn(ICanvasResourceCreator resourceCreator) => this.Crop(resourceCreator, this.Height, this.Width, Matrix3x2.CreateRotation(FanKit.Math.PiOver2) * Matrix3x2.CreateTranslation(this.Height, 0), CanvasImageInterpolation.NearestNeighbor);
-        public ILayer OverTurn(ICanvasResourceCreator resourceCreator) => this.Crop(resourceCreator, this.Width, this.Height, Matrix3x2.CreateScale(-1, -1, this.Center), CanvasImageInterpolation.NearestNeighbor);
+            Source = this.SourceRenderTarget,
+            TransformMatrix = matrix,
+            InterpolationMode = interpolation
+        }, width, height);
+
+        protected override ILayer SkretchSelf(ICanvasResourceCreator resourceCreator, int width, int height, CanvasImageInterpolation interpolation) => this.Crop(resourceCreator, width, height, Matrix3x2.CreateScale(width / (float)this.Width, height / (float)this.Height), interpolation);
+
+        protected override ILayer FlipHorizontalSelf(ICanvasResourceCreator resourceCreator) => this.Crop(resourceCreator, this.Width, this.Height, Matrix3x2.CreateScale(-1, 1, this.Center), CanvasImageInterpolation.NearestNeighbor);
+        protected override ILayer FlipVerticalSelf(ICanvasResourceCreator resourceCreator) => this.Crop(resourceCreator, this.Width, this.Height, Matrix3x2.CreateScale(1, -1, this.Center), CanvasImageInterpolation.NearestNeighbor);
+        protected override ILayer LeftTurnSelf(ICanvasResourceCreator resourceCreator) => this.Crop(resourceCreator, this.Height, this.Width, Matrix3x2.CreateRotation(-FanKit.Math.PiOver2) * Matrix3x2.CreateTranslation(0, this.Width), CanvasImageInterpolation.NearestNeighbor);
+        protected override ILayer RightTurnSelf(ICanvasResourceCreator resourceCreator) => this.Crop(resourceCreator, this.Height, this.Width, Matrix3x2.CreateRotation(FanKit.Math.PiOver2) * Matrix3x2.CreateTranslation(this.Height, 0), CanvasImageInterpolation.NearestNeighbor);
+        protected override ILayer OverTurnSelf(ICanvasResourceCreator resourceCreator) => this.Crop(resourceCreator, this.Width, this.Height, Matrix3x2.CreateScale(-1, -1, this.Center), CanvasImageInterpolation.NearestNeighbor);
 
     }
 }
