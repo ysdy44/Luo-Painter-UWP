88c75684b387fc5b7f3091da06246ce25376f2d4
ysdy44 <ysdy44@outlook.com>
2022-07-09 19:07:37 +0800

    Add AddMenu

---
+++ Luo Painter/Luo Painter.Menus/AddMenu.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Luo_Painter.Elements;
+using Luo_Painter.Options;
+using System;
+using Windows.ApplicationModel.Resources;
+
+namespace Luo_Painter.Menus
+{
+    public sealed partial class AddMenu : Expander
+    {
+        //@Delegate
+        public event EventHandler<OptionType> ItemClick
+        {
+            remove => this.Command.Click -= value;
+            add => this.Command.Click += value;
+        }
+
+        //@Construct
+        public AddMenu()
+        {
+            this.InitializeComponent();
+        }
+
+        //@Strings
+        public void ConstructStrings(ResourceLoader resource)
+        {
+        }
+
+    }
+}
