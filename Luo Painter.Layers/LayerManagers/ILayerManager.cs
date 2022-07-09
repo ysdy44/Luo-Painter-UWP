acc5dd0f4d043669540c0a87605c929940e928a7
ysdy44 <ysdy44@outlook.com>
2022-07-09 17:42:22 +0800

    Add ILayerManager

---
+++ Luo Painter.Layers/LayerManagers/ILayerManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Luo_Painter.Layers
+{
+    public interface ILayerManager
+    {
+        LayerDictionary Layers { get; }
+        LayerNodes Nodes { get; }
+        LayerObservableCollection ObservableCollection { get; }
+        IList<string> ClipboardLayers { get; }
+
+        int LayerSelectedIndex { get; set; }
+        object LayerSelectedItem { get; set; }
+        IList<object> LayerSelectedItems { get; }
+    }
+}
