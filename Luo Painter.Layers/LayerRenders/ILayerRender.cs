4de1bcd406176607b088f5198093ca3a3df17a0a
ysdy44 <ysdy44@outlook.com>
2022-07-10 11:04:56 +0800

    Add ILayerRender

---
+++ Luo Painter.Layers/LayerRenders/ILayerRender.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Graphics.Canvas;
+using System.Numerics;
+
+namespace Luo_Painter.Layers
+{
+    public interface ILayerRender
+    {
+        ICanvasImage Render(ICanvasImage background);
+        ICanvasImage Render(ICanvasImage background, Matrix3x2 matrix, CanvasImageInterpolation interpolationMode);
+        ICanvasImage Render(ICanvasImage background, Matrix3x2 matrix, CanvasImageInterpolation interpolationMode, string id, ICanvasImage mezzanine);
+
+        ICanvasImage Render(ICanvasImage previousImage, ICanvasImage currentImage);
+    }
+}
