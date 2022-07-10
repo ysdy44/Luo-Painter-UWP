2766b0680ff47c235f744f58d5853f7373ea9b1f
ysdy44 <ysdy44@outlook.com>
2022-07-10 11:20:05 +0800

    Add GroupLayer

--- Luo Painter.Layers/LayerType.cs
+++ Luo Painter.Layers/LayerType.cs
@@ -3,5 +3,6 @@
     public enum LayerType
     {
         Bitmap,
+        Group,
     }
 }
---
+++ Luo Painter.Layers/Models/GroupLayer.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Graphics.Canvas;
+using System.Numerics;
+
+namespace Luo_Painter.Layers.Models
+{
+    public sealed class GroupLayer : LayerBase, ILayer
+    {
+
+        public LayerType Type => LayerType.Group;
+
+        public ICanvasImage this[BitmapType type] => null;
+
+
+        //@Construct
+        public GroupLayer(ICanvasResourceCreator resourceCreator, int width, int height) : base(resourceCreator, width, height)
+        {
+        }
+
+        public ICanvasImage Render(ICanvasImage background) => base.Children.Render(background);
+        public ICanvasImage Render(ICanvasImage background, Matrix3x2 matrix, CanvasImageInterpolation interpolationMode) => base.Children.Render(background, matrix, interpolationMode);
+        public ICanvasImage Render(ICanvasImage background, Matrix3x2 matrix, CanvasImageInterpolation interpolationMode, string id, ICanvasImage mezzanine) => base.Children.Render(background, matrix, interpolationMode, id, mezzanine);
+
+        public bool FillContainsPoint(Vector2 point)
+        {
+            foreach (ILayer item in base.Children)
+            {
+                if (item.FillContainsPoint(point)) return true;
+            }
+            return false;
+        }
+
+        protected override ILayer CloneSelf(ICanvasResourceCreator resourceCreator) => new GroupLayer(resourceCreator, base.Width, base.Height);
+
+        protected override ILayer CropSelf(ICanvasResourceCreator resourceCreator, int width, int height) => new GroupLayer(resourceCreator, width, height);
+        protected override ILayer CropSelf(ICanvasResourceCreator resourceCreator, int width, int height, Vector2 offset) => new GroupLayer(resourceCreator, width, height);
+        protected override ILayer CropSelf(ICanvasResourceCreator resourceCreator, int width, int height, Matrix3x2 matrix, CanvasImageInterpolation interpolation) => new GroupLayer(resourceCreator, width, height);
+
+        protected override ILayer SkretchSelf(ICanvasResourceCreator resourceCreator, int width, int height, CanvasImageInterpolation interpolation) => new GroupLayer(resourceCreator, width, height);
+
+        protected override ILayer FlipHorizontalSelf(ICanvasResourceCreator resourceCreator) => new GroupLayer(resourceCreator, base.Width, base.Height);
+        protected override ILayer FlipVerticalSelf(ICanvasResourceCreator resourceCreator) => new GroupLayer(resourceCreator, base.Width, base.Height);
+        protected override ILayer LeftTurnSelf(ICanvasResourceCreator resourceCreator) => new GroupLayer(resourceCreator, base.Width, base.Height);
+        protected override ILayer RightTurnSelf(ICanvasResourceCreator resourceCreator) => new GroupLayer(resourceCreator, base.Width, base.Height);
+        protected override ILayer OverTurnSelf(ICanvasResourceCreator resourceCreator) => new GroupLayer(resourceCreator, base.Width, base.Height);
+
+    }
+}
