6abeb426ad233bc4a44df4b1ccf8ee6aeccc1f00
ysdy44 <ysdy44@outlook.com>
2022-07-10 11:07:36 +0800

    LayerNodes Base on ILayerRender

--- Luo Painter.Layers/Layers/LayerNodes.cs
+++ Luo Painter.Layers/Layers/LayerNodes.cs
@@ -1,10 +1,14 @@
 ﻿using Luo_Painter.Historys;
+using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.Effects;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
+using Windows.UI.Xaml;
 
 namespace Luo_Painter.Layers
 {
-    public sealed class LayerNodes : List<ILayer>
+    public sealed class LayerNodes : List<ILayer>, ILayerRender
     {
         public Layerage[] Convert() => LayerNodes.Convert(this);
         private static Layerage[] Convert(IList<ILayer> layers) => (layers.Count is 0) ? null : layers.Select(LayerNodes.Convert).ToArray();
@@ -12,6 +16,58 @@ namespace Luo_Painter.Layers
         {
             Id = layer.Id,
             Children = LayerNodes.Convert(layer.Children)
+        };
+
+        public ICanvasImage Render(ICanvasImage background)
+        {
+            for (int i = base.Count - 1; i >= 0; i--)
+            {
+                ILayer item = base[i];
+                switch (item.Visibility)
+                {
+                    case Visibility.Visible:
+                        background = item.Render(background);
+                        break;
+                }
+            }
+            return background;
+        }
+        public ICanvasImage Render(ICanvasImage background, Matrix3x2 matrix, CanvasImageInterpolation interpolationMode)
+        {
+            for (int i = base.Count - 1; i >= 0; i--)
+            {
+                ILayer item = base[i];
+                switch (item.Visibility)
+                {
+                    case Visibility.Visible:
+                        background = item.Render(background, matrix, interpolationMode);
+                        break;
+                }
+            }
+            return background;
+        }
+        public ICanvasImage Render(ICanvasImage background, Matrix3x2 matrix, CanvasImageInterpolation interpolationMode, string id, ICanvasImage mezzanine)
+        {
+            for (int i = base.Count - 1; i >= 0; i--)
+            {
+                ILayer item = base[i];
+                switch (item.Visibility)
+                {
+                    case Visibility.Visible:
+                        background = item.Render(background, matrix, interpolationMode, id, mezzanine);
+                        break;
+                }
+            }
+            return background;
+        }
+
+        public ICanvasImage Render(ICanvasImage previousImage, ICanvasImage currentImage) => new CompositeEffect
+        {
+            Sources =
+            {
+                previousImage,
+                currentImage
+            }
         };
     }
 }
