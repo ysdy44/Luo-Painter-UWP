e77babc55ab5101c1e2f89f31953ab4dfe62932c
ysdy44 <ysdy44@outlook.com>
2022-07-16 00:15:32 +0800

    Add Docker

---
+++ Luo Painter.Elements/Docker.xaml.cs
@@ -0,0 +1,171 @@
+﻿using System.Linq;
+using Windows.Foundation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Markup;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+
+namespace Luo_Painter.Elements
+{
+    [TemplatePart(Name = nameof(RootGrid), Type = typeof(Border))]
+    [TemplatePart(Name = nameof(HideStoryboard), Type = typeof(Storyboard))]
+    [TemplatePart(Name = nameof(ShowStoryboard), Type = typeof(Storyboard))]
+    [TemplatePart(Name = nameof(HideDoubleAnimation), Type = typeof(DoubleAnimation))]
+    [TemplatePart(Name = nameof(TranslateTransform), Type = typeof(TranslateTransform))]
+    [TemplatePart(Name = nameof(PrimaryButton), Type = typeof(Button))]
+    [TemplatePart(Name = nameof(SecondaryButton), Type = typeof(Button))]
+    [ContentProperty(Name = nameof(Content))]
+    public class Docker : ContentControl
+    {
+        //@Delegate
+        public event RoutedEventHandler PrimaryButtonClick;
+        public event RoutedEventHandler SecondaryButtonClick;
+
+        Border RootGrid;
+        Storyboard HideStoryboard;
+        Storyboard ShowStoryboard;
+        DoubleAnimation HideDoubleAnimation;
+
+        TranslateTransform TranslateTransform;
+
+        Button PrimaryButton;
+        Button SecondaryButton;
+
+        #region DependencyProperty
+
+
+        /// <summary> Gets or set the text for <see cref="Docker"/>'s PrimaryButton. </summary>
+        public string PrimaryButtonText
+        {
+            get => (string)base.GetValue(PrimaryButtonTextProperty);
+            set => base.SetValue(PrimaryButtonTextProperty, value);
+        }
+        /// <summary> Identifies the <see cref = "Docker.PrimaryButtonText" /> dependency property. </summary>
+        public static readonly DependencyProperty PrimaryButtonTextProperty = DependencyProperty.Register(nameof(PrimaryButtonText), typeof(string), typeof(Docker), new PropertyMetadata(string.Empty));
+
+
+        /// <summary> Gets or set the text for <see cref="Docker"/>'s SecondaryButton. </summary>
+        public string SecondaryButtonText
+        {
+            get => (string)base.GetValue(SecondaryButtonTextProperty);
+            set => base.SetValue(SecondaryButtonTextProperty, value);
+        }
+        /// <summary> Identifies the <see cref = "Docker.SecondaryButtonText" /> dependency property. </summary>
+        public static readonly DependencyProperty SecondaryButtonTextProperty = DependencyProperty.Register(nameof(SecondaryButtonText), typeof(string), typeof(Docker), new PropertyMetadata(string.Empty));
+
+
+        /// <summary> Gets or set the orientation for <see cref="Docker"/>. </summary>
+        public Orientation Orientation
+        {
+            get => (Orientation)base.GetValue(OrientationProperty);
+            set => base.SetValue(OrientationProperty, value);
+        }
+        /// <summary> Identifies the <see cref = "Docker.Orientation" /> dependency property. </summary>
+        public static readonly DependencyProperty OrientationProperty = DependencyProperty.Register(nameof(Orientation), typeof(Orientation), typeof(Docker), new PropertyMetadata(Orientation.Horizontal, (sender, e) =>
+        {
+            Docker control = (Docker)sender;
+
+            if (e.NewValue is Orientation value)
+            {
+                control.UpdateStoryboard();
+            }
+        }));
+
+        /// <summary> Gets or set the state for <see cref="Docker"/>. </summary>
+        public bool IsShow
+        {
+            get => (bool)base.GetValue(IsShowProperty);
+            set => base.SetValue(IsShowProperty, value);
+        }
+        /// <summary> Identifies the <see cref = "Docker.IsShow" /> dependency property. </summary>
+        public static readonly DependencyProperty IsShowProperty = DependencyProperty.Register(nameof(IsShow), typeof(bool), typeof(Docker), new PropertyMetadata(false, (sender, e) =>
+        {
+            Docker control = (Docker)sender;
+
+            if (e.NewValue is bool value)
+            {
+                if (value)
+                {
+                    if (control.ShowStoryboard is null) return;
+                    control.ShowStoryboard.Begin();
+                }
+                else
+                {
+                    if (control.HideStoryboard is null) return;
+                    control.HideStoryboard.Begin();
+                }
+            }
+        }));
+
+
+        #endregion
+
+        //@Construct     
+        /// <summary>
+        /// Initializes a Docker. 
+        /// </summary>
+        public Docker()
+        {
+            this.DefaultStyleKey = typeof(Docker);
+            base.SizeChanged += (s, e) =>
+            {
+                if (e.NewSize == Size.Empty) return;
+                if (e.NewSize == e.PreviousSize) return;
+                if (e.NewSize.Height == e.PreviousSize.Height) return;
+
+                this.UpdateStoryboard();
+                this.UpdateTranslateTransform();
+            };
+        }
+
+        /// <inheritdoc/>
+        protected override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            if (this.RootGrid is null is false)
+            {
+                this.HideStoryboard = null;
+                this.ShowStoryboard = null;
+                this.HideDoubleAnimation = null;
+            }
+            this.RootGrid = base.GetTemplateChild(nameof(RootGrid)) as Border;
+            if (this.RootGrid is null is false)
+            {
+                this.HideStoryboard = this.RootGrid.Resources[nameof(HideStoryboard)] as Storyboard;
+                this.ShowStoryboard = this.RootGrid.Resources[nameof(ShowStoryboard)] as Storyboard;
+                if (this.HideStoryboard is null is false)
+                {
+                    this.HideDoubleAnimation = this.HideStoryboard.Children.First() as DoubleAnimation;
+                    this.UpdateStoryboard();
+                }
+            }
+
+            this.TranslateTransform = base.GetTemplateChild(nameof(TranslateTransform)) as TranslateTransform;
+            this.UpdateTranslateTransform();
+
+            if (this.PrimaryButton is null is false) this.PrimaryButton.Click -= this.PrimaryButtonClick;
+            this.PrimaryButton = base.GetTemplateChild(nameof(PrimaryButton)) as Button;
+            if (this.PrimaryButton is null is false) this.PrimaryButton.Click += this.PrimaryButtonClick;
+
+            if (this.SecondaryButton is null is false) this.SecondaryButton.Click -= this.SecondaryButtonClick;
+            this.SecondaryButton = base.GetTemplateChild(nameof(SecondaryButton)) as Button;
+            if (this.SecondaryButton is null is false) this.SecondaryButton.Click += this.SecondaryButtonClick;
+        }
+
+        private void UpdateStoryboard()
+        {
+            if (this.HideDoubleAnimation is null) return;
+            this.HideDoubleAnimation.To = base.ActualHeight;
+        }
+
+        private void UpdateTranslateTransform()
+        {
+            if (this.TranslateTransform is null) return;
+            if (this.IsShow) return;
+            this.TranslateTransform.Y = base.ActualHeight;
+        }
+
+    }
+}
