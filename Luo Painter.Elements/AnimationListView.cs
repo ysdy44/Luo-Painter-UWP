c376f26209ae004afefd9a3b77153bb199c89a70
ysdy44 <ysdy44@outlook.com>
2022-07-20 02:04:20 +0800

    AnimationListView 2.0.0

--- Luo Painter.Elements/AnimationListView.cs
+++ Luo Painter.Elements/AnimationListView.cs
@@ -1,22 +1,29 @@
+using System.Threading.Tasks;
+using Windows.Foundation;
 using Windows.UI.Composition;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Hosting;
 
 namespace Luo_Painter.Elements
 {
     /// <summary>
-    /// Provides a <see cref="GridView"/> with a reorder animation.
+    /// Provides a <see cref="ListView"/> with a reorder animation.
     /// </summary>
     public sealed class AnimationListView : ListView
     {
+        ScrollViewer ScrollViewer;
+        ItemsPresenter ItemsPresenter;
+
         readonly Visual ElementVisual;
         readonly Compositor Compositor;
         readonly Vector3KeyFrameAnimation OffsetAnimation;
         readonly ImplicitAnimationCollection Animation;
+
         ~AnimationListView() => this.Animation.Remove(nameof(Visual.Offset));
         //@Construct
         /// <summary>
-        /// Constructs a <see cref="GridView"/> instance with reorder animation.
+        /// Constructs a <see cref="ListView"/> instance with reorder animation.
         /// </summary>
         public AnimationListView()
         {
@@ -51,6 +58,52 @@ namespace Luo_Painter.Elements
                 Visual visual = ElementCompositionPreview.GetElementVisual(args.ItemContainer);
                 visual.ImplicitAnimations = this.Animation;
             };
+
+            base.Loaded += async (s, e) =>
+            {
+                if (this.ScrollViewer is null) return;
+                await Task.Delay(200);
+
+                base.Margin = new Thickness(0, 50, 0, 0);
+                this.ScrollViewer.ChangeView(null, 50, null);
+                //this.ScrollViewer.VerticalSnapPointsType = SnapPointsType.Mandatory;
+                //this.ScrollViewer.VerticalSnapPointsAlignment = SnapPointsAlignment.Near;
+            };
+        }
+
+        public void Resizing(Size size)
+        {
+            // Align Center
+            int p = (int)size.Width % 210;
+            base.Padding = new Thickness(p / 2, 0, p / 2, 0);
+
+            if (this.ItemsPresenter is null) return;
+            this.ItemsPresenter.MinHeight = size.Height + 50;
+        }
+
+        protected override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            if (this.ScrollViewer is null is false) this.ScrollViewer.DirectManipulationCompleted -= this.ScrollViewer_DirectManipulationCompleted;
+            this.ScrollViewer = base.GetTemplateChild(nameof(ScrollViewer)) as ScrollViewer;
+            if (this.ScrollViewer is null) return;
+            this.ScrollViewer.DirectManipulationCompleted += this.ScrollViewer_DirectManipulationCompleted;
+
+            this.ItemsPresenter = this.ScrollViewer.Content as ItemsPresenter;
+        }
+
+        private void ScrollViewer_DirectManipulationCompleted(object sender, object e)
+        {
+            double y = this.ScrollViewer.VerticalOffset;
+            if (y < 50)
+            {
+                this.ScrollViewer.ChangeView(null, 0, null);
+            }
+            else if (y < 50 + 50 + 50)
+            {
+                this.ScrollViewer.ChangeView(null, 50, null);
+            }
         }
     }
 }
