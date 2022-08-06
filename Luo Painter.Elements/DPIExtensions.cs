c18ec5dddb36c35871571cecdd0a9fbbe3fa54d8
ysdy44 <ysdy44@outlook.com>
2022-08-06 15:29:17 +0800

    DPIExtensions 2.0.0

--- Luo Painter.Elements/DPIExtensions.cs
+++ Luo Painter.Elements/DPIExtensions.cs
@@ -5,24 +5,19 @@ namespace Luo_Painter.Elements
 {
     public static class DPIExtensions
     {
-        public static float ConvertDipsToPixels(this float dpi, float vector) => vector * dpi / 96;
-        public static Vector2 ConvertDipsToPixels(this float dpi, Vector2 vector) => vector * dpi / 96;
-        public static Matrix3x2 ConvertDipsToPixels(this float dpi) => Matrix3x2.CreateScale(dpi / 96);
+        public static float ConvertPixels(this float dpi) => dpi / 96f;
+        public static float ConvertDipsToPixels(this float dpi, float vector) => vector * dpi / 96f;
+        public static Vector2 ConvertDipsToPixels(this float dpi, Vector2 vector) => vector * dpi / 96f;
+        public static Matrix3x2 ConvertDipsToPixels(this float dpi) => Matrix3x2.CreateScale(dpi / 96f);
         public static Matrix3x2 ConvertDipsToPixels(this float dpi, Matrix3x2 matrix) => matrix * dpi.ConvertDipsToPixels();
-        public static Rect ConvertDipsToPixels(this float dpi, Rect rect)
-        {
-            float s = 1 * dpi / 96;
-            return new Rect(rect.X * s, rect.Y * s, rect.Width * s, rect.Height * s);
-        }
+        public static Rect ConvertDipsToPixels(this float dpi, Rect rect) => new Rect(rect.X * dpi / 96f, rect.Y * dpi / 96f, rect.Width * dpi / 96f, rect.Height * dpi / 96f);
 
-        public static float ConvertPixelsToDips(this float dpi, float vector) => vector / dpi * 96;
-        public static Vector2 ConvertPixelsToDips(this float dpi, Vector2 vector) => vector / dpi * 96;
-        public static Matrix3x2 ConvertPixelsToDips(this float dpi) => Matrix3x2.CreateScale(1 / dpi * 96);
+        public static float ConvertDips(this float dpi) => 1f / dpi * 96f;
+        public static float ConvertPixelsToDips(this float dpi, float vector) => vector / dpi * 96f;
+        public static Vector2 ConvertPixelsToDips(this float dpi, Vector2 vector) => vector / dpi * 96f;
+        public static Matrix3x2 ConvertPixelsToDips(this float dpi) => Matrix3x2.CreateScale(1 / dpi * 96f);
         public static Matrix3x2 ConvertPixelsToDips(this float dpi, Matrix3x2 matrix) => matrix * dpi.ConvertPixelsToDips();
-        public static Rect ConvertPixelsToDips(this float dpi, Rect rect)
-        {
-            float s = 1 / dpi * 96;
-            return new Rect(rect.X * s, rect.Y * s, rect.Width * s, rect.Height * s);
-        }
+        public static Rect ConvertPixelsToDips(this float dpi, Rect rect) => new Rect(rect.X / dpi * 96f, rect.Y / dpi * 96f, rect.Width / dpi * 96f, rect.Height / dpi * 96f);
+
     }
 }
