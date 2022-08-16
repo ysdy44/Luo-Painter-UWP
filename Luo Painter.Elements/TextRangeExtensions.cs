a89265156a4e9e2c9e9bd615aa6ebd5873357435
ysdy44 <ysdy44@outlook.com>
2022-08-16 21:01:27 +0800

    Add TextRangeExtensions

---
+++ Luo Painter.Elements/TextRangeExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Numerics;
+using Windows.Foundation;
+using Windows.UI.Text.Core;
+
+namespace Luo_Painter.Elements
+{
+    public static class TextRangeExtensions
+    {
+        public static int First(this CoreTextRange range) => System.Math.Min(range.StartCaretPosition, range.EndCaretPosition);
+        public static int Last(this CoreTextRange range) => System.Math.Max(range.StartCaretPosition, range.EndCaretPosition);
+        public static int Last(this CoreTextRange range, int maxLength) => System.Math.Min(maxLength, System.Math.Max(range.StartCaretPosition, range.EndCaretPosition));
+        public static int Length(this CoreTextRange range) => System.Math.Abs(range.EndCaretPosition - range.StartCaretPosition);
+        public static int Length(this CoreTextRange range, int maxLength) => System.Math.Abs(System.Math.Min(maxLength, range.EndCaretPosition) - range.StartCaretPosition);
+        public static bool HasSelection(this CoreTextRange range) => range.StartCaretPosition != range.EndCaretPosition;
+        public static bool HitTest(this Rect rect, Vector2 position) => position.X > rect.X + rect.Width / 2;
+    }
+}
