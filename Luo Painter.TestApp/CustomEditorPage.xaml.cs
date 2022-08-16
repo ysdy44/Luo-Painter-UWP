afd5f2869d06ed048256a0db9a64bd55d64ff07d
ysdy44 <ysdy44@outlook.com>
2022-08-16 23:52:07 +0800

    CustomEditorPage 2.0.0

--- Luo Painter.TestApp/CustomEditorPage.xaml.cs
+++ Luo Painter.TestApp/CustomEditorPage.xaml.cs
@@ -3,7 +3,9 @@ using Windows.Foundation;
 using Windows.Graphics.Display;
 using Windows.System;
 using Windows.UI;
+using Windows.UI.Core;
 using Windows.UI.Text.Core;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media;
@@ -12,54 +14,123 @@ namespace Luo_Painter.TestApp
 {
     public sealed partial class CustomEditorPage : Page
     {
+        //@Key
+        private bool IsKeyDown(VirtualKey key) => Window.Current.CoreWindow.GetKeyState(key).HasFlag(CoreVirtualKeyStates.Down);
+        private bool IsCtrl => this.IsKeyDown(VirtualKey.Control);
+        private bool IsShift => this.IsKeyDown(VirtualKey.Shift);
 
-        Rect TextBounds;
-        Rect ControlBounds;
 
-        readonly CustomEditor Editor = new CustomEditor
-        {
-            Text = "The quick brown fox jumps over the lazy dog."
-        };
         readonly DispatcherTimer CaretTimer = new DispatcherTimer
         {
             Interval = System.TimeSpan.FromMilliseconds(500)
         };
 
-        //@BackRequested
-        public bool InternalFocus
-        {
-            set
-            {
-                if (value)
-                {
-                    this.Editor.SetInternalFocus();
-                    this.ContentPanel.BorderBrush = new SolidColorBrush(Colors.DodgerBlue);
-                }
-                else
-                {
-                    this.Editor.RemoveInternalFocus();
-                    this.ContentPanel.BorderBrush = null;
-                }
-            }
-        }
+        readonly InputPane InputPane = InputPane.GetForCurrentView();
+        readonly CoreTextEditContext EditContext = CoreTextServicesManager.GetForCurrentView().CreateEditContext();
+        bool InternalFocus;
+
+        string Text = "The quick brown fox jumps over the lazy dog.";
+        string TextRange(CoreTextRange range) => this.Text.Substring(range.StartCaretPosition, range.EndCaretPosition - range.StartCaretPosition);
+
+        CoreTextRange Selection;
+        int Start { get => this.Selection.StartCaretPosition; set => this.Selection.StartCaretPosition = value; }
+        int End { get => this.Selection.EndCaretPosition; set => this.Selection.EndCaretPosition = value; }
+
+        Rect TextBounds;
+        Rect ControlBounds;
 
         public CustomEditorPage()
         {
             this.InitializeComponent();
             base.Loaded += (s, e) => this.Focus(FocusState.Keyboard);
-            base.LostFocus += (s, e) => this.InternalFocus = false;
-            base.GotFocus += (s, e) => this.InternalFocus = true;
+            base.LostFocus += (s, e) =>
+            {
+                this.ContentPanel.BorderBrush = null;
+
+                if (this.InternalFocus)
+                {
+                    this.EditContext.NotifyFocusLeave();
+
+                    this.InternalFocus = false;
+                    this.InputPane.TryHide();
+                    this.UpdateTextUI();
+                }
+            };
+            base.GotFocus += (s, e) =>
+            {
+                this.ContentPanel.BorderBrush = new SolidColorBrush(Colors.DodgerBlue);
+
+                if (!this.InternalFocus)
+                {
+                    this.InternalFocus = true;
+                    this.UpdateTextUI();
+
+                    this.EditContext.NotifyFocusEnter();
+                }
+
+                this.InputPane.TryShow();
+            };
+
+
             base.KeyDown += (s, e) =>
             {
                 switch (e.Key)
                 {
-                    case VirtualKey.Back: this.Editor.KeyBack(); break;
-                    case VirtualKey.Delete: this.Editor.KeyDelete(); break;
-                    case VirtualKey.Left: this.Editor.KeyLeft(); break;
-                    case VirtualKey.Right: this.Editor.KeyRight(); break;
+                    case VirtualKey.Back: this.KeyBack(); break;
+                    case VirtualKey.Delete: this.KeyDelete(); break;
+                    case VirtualKey.Left: this.KeyLeft(this.IsShift); break;
+                    case VirtualKey.Right: this.KeyRight(this.IsShift); break;
+                    case VirtualKey.A: if (this.IsCtrl) this.SelectAll(); break;
+                    case VirtualKey.S: if (this.IsCtrl) this.SelectToStart(); break;
+                    case VirtualKey.E: if (this.IsCtrl) this.SelectToEnd(); break;
                     default: break;
                 }
             };
+
+
+            this.EditContext.InputPaneDisplayPolicy = CoreTextInputPaneDisplayPolicy.Manual;
+            this.EditContext.InputScope = CoreTextInputScope.Text;
+
+            this.EditContext.FocusRemoved += (sender, args) =>
+            {
+                this.InternalFocus = false;
+                this.InputPane.TryHide();
+                this.UpdateTextUI();
+            };
+
+            this.EditContext.SelectionRequested += (sender, args) => args.Request.Selection = this.Selection;
+            this.EditContext.SelectionUpdating += (sender, args) => this.Selection = args.Selection;
+
+            this.EditContext.TextRequested += (sender, args) =>
+            {
+                int startIndex = args.Request.Range.StartCaretPosition;
+                int endIndex = System.Math.Min(args.Request.Range.EndCaretPosition, this.Text.Length);
+                args.Request.Text = this.Text.Substring(startIndex, endIndex - startIndex);
+            };
+            this.EditContext.TextUpdating += (sender, args) =>
+            {
+                if (this.Selection.HasSelection()) this.Delete(true);
+
+                this.Text =
+                this.Text.Substring(0, args.Range.StartCaretPosition) +
+                args.Text +
+                this.Text.Substring(System.Math.Min(this.Text.Length, args.Range.EndCaretPosition));
+
+                this.Start = this.End = args.NewSelection.StartCaretPosition;
+                this.UpdateTextUI();
+            };
+
+            this.EditContext.FormatUpdating += (sender, args) =>
+            {
+                if (args.TextColor is null) { }
+
+                if (args.BackgroundColor is null) { }
+
+                if (args.UnderlineType is null) { }
+            };
+
+            this.EditContext.CompositionStarted += (s, e) => { };
+            this.EditContext.CompositionCompleted += (s, e) => { };
 
             this.CaretTimer.Tick += (s, e) =>
             {
@@ -71,7 +142,7 @@ namespace Luo_Painter.TestApp
                 }
             };
 
-            this.Editor.LayoutRequested += (sender, args) =>
+            this.EditContext.LayoutRequested += (sender, args) =>
             {
                 double scale = DisplayInformation.GetForCurrentView().RawPixelsPerViewPixel;
 
@@ -93,41 +164,140 @@ namespace Luo_Painter.TestApp
                 this.ControlBounds.Height = this.ContentPanel.ActualHeight * scale;
                 args.Request.LayoutBounds.ControlBounds = this.ControlBounds;
             };
-            this.Editor.UpdateTextUIAction += () =>
+        }
+
+        private void UpdateTextUI()
+        {
+            CoreTextRange range = this.Selection;
+
+            this.BeforeSelectionTextBlock.Text = this.Text.Substring(0, range.StartCaretPosition) + "\ufeff";
+            if (this.Selection.HasSelection())
             {
-                CoreTextRange range = this.Editor.Selection;
+                this.Caret.Visibility = Visibility.Collapsed;
+                this.CaretTimer.Stop();
 
-                this.BeforeSelectionTextBlock.Text = this.Editor.Text.Substring(0, range.StartCaretPosition) + "\ufeff";
-                if (this.Editor.HasSelection())
+                this.SelectionTextBlock.Text = this.TextRange(range) + "\ufeff";
+            }
+            else
+            {
+                this.SelectionTextBlock.Text = "";
+
+                if (this.InternalFocus)
                 {
-                    this.Caret.Visibility = Visibility.Collapsed;
-                    this.CaretTimer.Stop();
-
-                    this.SelectionTextBlock.Text = this.Editor.TextRange(range) + "\ufeff";
+                    this.Caret.Visibility = Visibility.Visible;
+                    this.CaretTimer.Start();
                 }
                 else
                 {
-                    this.SelectionTextBlock.Text = "";
-
-                    if (this.Editor.InternalFocus)
-                    {
-                        this.Caret.Visibility = Visibility.Visible;
-                        this.CaretTimer.Start();
-                    }
-                    else
-                    {
-                        this.Caret.Visibility = Visibility.Collapsed;
-                        this.CaretTimer.Stop();
-                    }
+                    this.Caret.Visibility = Visibility.Collapsed;
+                    this.CaretTimer.Stop();
                 }
+            }
 
-                this.AfterSelectionTextBlock.Text = this.Editor.Text.Substring(range.EndCaretPosition) + "\ufeff";
+            this.AfterSelectionTextBlock.Text = this.Text.Substring(range.EndCaretPosition) + "\ufeff";
 
-                this.FullTextBlock.Text = this.Editor.Text;
-                this.HasSelectionTextBlock.Text = this.Editor.HasSelection().ToString();
-                this.SelectionStartIndexTextBlock.Text = range.StartCaretPosition.ToString();
-                this.SelectionEndIndexTextBlock.Text = range.EndCaretPosition.ToString();
-            };
+            this.FullTextBlock.Text = this.Text;
+            this.HasSelectionTextBlock.Text = this.Selection.HasSelection().ToString();
+            this.SelectionStartIndexTextBlock.Text = range.StartCaretPosition.ToString();
+            this.SelectionEndIndexTextBlock.Text = range.EndCaretPosition.ToString();
+        }
+
+
+        public void KeyBack() => this.Delete(true);
+        public void KeyDelete() => this.Delete(false);
+
+        public void KeyLeft(bool isShift = false) => this.Move(true, isShift);
+        public void KeyRight(bool isShift = false) => this.Move(false, isShift);
+
+        public void SelectAll() => this.Move(0, this.Text.Length);
+        public void SelectToStart() => this.Move(0, 0);
+        public void SelectToEnd() => this.Move(this.Text.Length, this.Text.Length);
+
+
+        private void Move(int start, int end)
+        {
+            if (this.Start == start && this.End == end) return;
+
+            this.Start = start;
+            this.End = end;
+
+            this.EditContext.NotifySelectionChanged(this.Selection);
+            this.UpdateTextUI();
+        }
+
+        private void Move(bool isLeft, bool isShift)
+        {
+            if (isLeft)
+            {
+                if (this.End <= 0) return;
+                this.End--;
+            }
+            else
+            {
+                if (this.End >= this.Text.Length) return;
+                this.End++;
+            }
+
+            if (isShift is false)
+            {
+                this.Start = this.End;
+            }
+
+            this.EditContext.NotifySelectionChanged(this.Selection);
+            this.UpdateTextUI();
+        }
+
+        private void Delete(bool isLeft)
+        {
+            if (this.Selection.HasSelection())
+            {
+                int first = this.Selection.First();
+                int last = this.Selection.Last();
+
+                if (first <= 0)
+                {
+                    if (last >= this.Text.Length)
+                        this.Text = string.Empty;
+                    else
+                        this.Text = this.Text.Substring(last);
+
+                    this.End = this.Start = 0;
+                }
+                else
+                {
+                    if (last >= this.Text.Length)
+                        this.Text = this.Text.Substring(0, first);
+                    else
+                        this.Text = this.Text.Substring(0, first) + this.Text.Substring(last);
+
+                    this.End = this.Start = first;
+                }
+            }
+            else
+            {
+                int index = this.Selection.First();
+
+                if (isLeft)
+                {
+                    if (index <= 0) return;
+                    else if (index >= this.Text.Length)
+                        this.Text = this.Text.Substring(0, index - 1);
+                    else
+                        this.Text = this.Text.Substring(0, index - 1) + this.Text.Substring(index);
+                    this.Start = this.End = index - 1;
+                }
+                else
+                {
+                    if (index >= this.Text.Length) return;
+                    else if (index <= 0)
+                        this.Text = this.Text.Substring(index + 1);
+                    else
+                        this.Text = this.Text.Substring(0, index) + this.Text.Substring(index + 1);
+                }
+            }
+
+            this.EditContext.NotifySelectionChanged(this.Selection);
+            this.UpdateTextUI();
         }
     }
 }
