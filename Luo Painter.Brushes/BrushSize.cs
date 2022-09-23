b3ecbd985a9997caa920f705e755ae5abc87122f
ysdy44 <ysdy44@outlook.com>
2022-09-23 23:04:58 +0800

    Add BrushSize

---
+++ Luo Painter.Brushes/BrushSize.cs
@@ -0,0 +1,20 @@
+﻿namespace Luo_Painter.Brushes
+{
+    public sealed class BrushSize
+    {
+        public double Size { get; set; } //= 0.7f;
+        public string Text { get; set; } //= "0.7";
+
+        public int Number { get; set; } //= 7;
+        public double Preview { get; set; } //= 1.0764939309056;
+
+        //@Static
+        public static double Convert(double size) => BrushSize.ConvertCore(size * 10);
+        private static double ConvertCore(double number)
+        {
+            double a = (number + 100000.0) / 1020.0 - 97;
+            double b = (1.0 - 0.09 / (number)) * 600 - 590;
+            return (a + b) / 1.2 - 1.7;
+        }
+    }
+}
--- Luo Painter/Luo Painter.Menus/SizeMenu.xaml.cs
+++ Luo Painter/Luo Painter.Menus/SizeMenu.xaml.cs
@@ -1,25 +1,10 @@
-﻿using Luo_Painter.Elements;
+﻿using Luo_Painter.Brushes;
+using Luo_Painter.Elements;
 using System;
 using Windows.ApplicationModel.Resources;
 
 namespace Luo_Painter.Menus
 {
-    internal sealed class BrushSize
-    {
-        public double Size { get; set; }
-        public string Text { get; set; }
-
-        public int Number { get; set; }
-        public double Preview { get; set; }
-
-        //public double GetPreview()
-        //{
-        //    double a = (Number + 100000.0) / 1020.0 - 97;
-        //    double b = (1.0 - 0.09 / (Number)) * 600 - 590;
-        //    return (a + b) / 1.2 - 1.7;
-        //}
-    }
-
     public sealed partial class SizeMenu : Expander
     {
         //@Delegate
