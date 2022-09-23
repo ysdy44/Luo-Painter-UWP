995b93461923491192cb0156f85b71492c08d4e6
ysdy44 <ysdy44@outlook.com>
2022-09-23 23:52:28 +0800

    Add SliderToolTipPage

---
+++ Luo Painter.TestApp/SliderToolTipPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Luo_Painter.Brushes;
+using Windows.UI.Xaml.Controls;
+
+namespace Luo_Painter.TestApp
+{
+    public sealed partial class SliderToolTipPage : Page
+    {
+        //@Converter
+        private double SizeConverter(double value) => BrushSize.Convert(value) * 2;
+        
+        public SliderToolTipPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
