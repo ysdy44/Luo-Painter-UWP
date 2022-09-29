8e10d38692a084354f023bd84590ac55304da87b
ysdy44 <ysdy44@outlook.com>
2022-09-30 00:59:22 +0800

    Add IInkParameter

---
+++ Luo Painter.Brushes/IInkParameter.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Graphics.Canvas;
+using Windows.UI.Xaml.Navigation;
+
+namespace Luo_Painter.Brushes
+{
+    /// <summary>
+    /// Represents an interface which can be <see cref="NavigationEventArgs.Parameter"/> for Ink.
+    /// </summary>
+    public interface IInkParameter
+    {
+        CanvasDevice CanvasDevice { get; }
+        InkPresenter InkPresenter { get; }
+        InkType InkType { get; set; }
+    }
+}
