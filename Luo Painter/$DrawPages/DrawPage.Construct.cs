9df1117752a885e0d2b4dde5639b1fbccf0c5f48
ysdy44 <ysdy44@outlook.com>
2022-10-05 02:27:49 +0800

    Add TipType

--- Luo Painter/$DrawPages/DrawPage.Construct.cs
+++ Luo Painter/$DrawPages/DrawPage.Construct.cs
@@ -1,4 +1,5 @@
-﻿using Luo_Painter.Brushes;
+﻿using Luo_Painter.Blends;
+using Luo_Painter.Brushes;
 using Luo_Painter.Elements;
 using Luo_Painter.Layers;
 using Luo_Painter.Options;
@@ -24,6 +25,57 @@ namespace Luo_Painter
         public void Tip(string title, string subtitle)
         {
             this.ToastTip.Tip(title, subtitle);
+        }
+        public void Tip(TipType type)
+        {
+            switch (type)
+            {
+                case TipType.NoLayer:
+                    this.ToastTip.Tip("No Layer", "Create a new Layer?");
+                    break;
+                case TipType.NotBitmapLayer:
+                    this.ToastTip.Tip("Not Bitmap Layer", "Can only operate on Bitmap Layer.");
+                    break;
+                case TipType.NotCurveLayer:
+                    this.ToastTip.Tip("Not Curve Layer", "Can only operate on Curve Layer.");
+                    break;
+
+                case TipType.NoPixel:
+                    this.ToastTip.Tip("No Pixel", "The current Pixel is Transparent.");
+                    break;
+                case TipType.NoPixelForMarquee:
+                    this.ToastTip.Tip("No Pixel", "The Marquee is Transparent.");
+                    break;
+                case TipType.NoPixelForBitmapLayer:
+                    this.ToastTip.Tip("No Pixel", "The current Bitmap Layer is Transparent.");
+                    break;
+
+                case TipType.Spread:
+                    this.ToastTip.Tip("Spread", $"{this.AppBar.Rippler.Spread * 100:0.00}%");
+                    break;
+                case TipType.Zoom:
+                    this.ToastTip.Tip("Zoom", $"{this.Transformer.Scale * 100:0.00}%");
+                    break;
+                case TipType.Undo:
+                    this.ToastTip.Tip("Undo", $"{this.History.Index} / {this.History.Count}");
+                    break;
+                case TipType.Redo:
+                    this.ToastTip.Tip("Redo", $"{this.History.Index} / {this.History.Count}");
+                    break;
+
+                case TipType.Saving:
+                    this.ToastTip.Tip("Saving...", this.ApplicationView.Title);
+                    break;
+                case TipType.SaveSuccess:
+                    this.ToastTip.Tip("Saved successfully", this.ApplicationView.Title);
+                    break;
+                case TipType.SaveFailed:
+                    this.ToastTip.Tip("Failed to Save", "Try again?");
+                    break;
+
+                default:
+                    break;
+            }
         }
 
 
