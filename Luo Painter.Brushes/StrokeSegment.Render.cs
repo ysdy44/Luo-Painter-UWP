118b4a9b918bbd7b234acde30753549081281d10
ysdy44 <ysdy44@outlook.com>
2022-10-09 01:04:36 +0800

    Add StrokeSegment

---
+++ Luo Painter.Brushes/StrokeSegment.Render.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.Graphics.Canvas.Effects;
+using Microsoft.Graphics.Canvas;
+using System.Numerics;
+using Windows.UI;
+using Microsoft.Graphics.Canvas.Geometry;
+
+namespace Luo_Painter.Brushes
+{
+    public partial struct StrokeSegment
+    {
+
+        public static readonly CanvasStrokeStyle CanvasStrokeStyle = new CanvasStrokeStyle
+        {
+            StartCap = CanvasCapStyle.Round,
+            EndCap = CanvasCapStyle.Round,
+        };
+
+        public void IsometricFillCircle(CanvasDrawingSession ds, Color color, Stroke stroke, StrokeSegment segment)
+        {
+            ds.FillCircle(stroke.StartingPosition, segment.StartingSize, color);
+
+
+            float distance = segment.StartingDistance;
+            while (distance < segment.Distance)
+            {
+                float smooth = distance / segment.Distance;
+                float smoothR = 1 - smooth;
+
+                float sizePressureSmooth = segment.Size * (smooth * stroke.StartingPressure + smoothR * stroke.Pressure);
+                Vector2 positionSmooth = smooth * stroke.StartingPosition + smoothR * stroke.Position;
+
+                distance += segment.Spacing * sizePressureSmooth;
+
+
+                ds.FillCircle(positionSmooth, sizePressureSmooth, color);
+            }
+        }
+
+
+        /// <summary>
+        /// <see cref="ShaderType.BrushEdgeHardness"/>
+        /// </summary>
+        public void IsometricDrawShaderBrushEdgeHardness(CanvasDrawingSession ds, byte[] shaderCode, int hardness, Vector4 colorHdr, Stroke stroke, StrokeSegment segment)
+        {
+            ds.DrawImage(new PixelShaderEffect(shaderCode)
+            {
+                Properties =
+                {
+                    ["hardness"] = hardness,
+                    ["pressure"] = 1f,
+                    ["radius"] = segment.StartingSize,
+                    ["targetPosition"] = stroke.StartingPosition,
+                    ["color"] = colorHdr
+                }
+            });
+
+
+            float distance = segment.StartingDistance;
+            while (distance < segment.Distance)
+            {
+                float smooth = distance / segment.Distance;
+                float smoothR = 1 - smooth;
+
+                float sizePressureSmooth = segment.Size * (smooth * stroke.StartingPressure + smoothR * stroke.Pressure);
+                Vector2 positionSmooth = smooth * stroke.StartingPosition + smoothR * stroke.Position;
+
+                distance += segment.Spacing * sizePressureSmooth;
+
+
+                ds.DrawImage(new PixelShaderEffect(shaderCode)
+                {
+                    Properties =
+                    {
+                        ["hardness"] = hardness,
+                        ["pressure"] = 1f,
+                        ["radius"] = sizePressureSmooth,
+                        ["targetPosition"] = positionSmooth,
+                        ["color"] = colorHdr
+                    }
+                });
+            }
+        }
+
+
+        /// <summary>
+        /// <see cref="ShaderType.BrushEdgeHardnessWithTexture"/>
+        /// </summary>
+        public void IsometricDrawShaderBrushEdgeHardnessWithTexture(CanvasDrawingSession ds, byte[] shaderCode, int hardness, Vector4 colorHdr, CanvasBitmap texture, bool rotate, Stroke stroke, StrokeSegment segment)
+        {
+            Vector2 normalization = stroke.Normalize();
+            ds.DrawImage(new PixelShaderEffect(shaderCode)
+            {
+                Source1 = texture,
+                Properties =
+                {
+                    ["hardness"] = hardness,
+                    ["rotate"] = rotate,
+                    ["normalization"] = normalization,
+                    ["pressure"] = 1f,
+                    ["radius"] = segment.StartingSize,
+                    ["targetPosition"] = stroke.StartingPosition,
+                    ["color"] = colorHdr
+                }
+            });
+
+
+            float distance = segment.StartingDistance;
+            while (distance < segment.Distance)
+            {
+                float smooth = distance / segment.Distance;
+                float smoothR = 1 - smooth;
+
+                float sizePressureSmooth = segment.Size * (smooth * stroke.StartingPressure + smoothR * stroke.Pressure);
+                Vector2 positionSmooth = smooth * stroke.StartingPosition + smoothR * stroke.Position;
+
+                distance += segment.Spacing * sizePressureSmooth;
+
+
+                ds.DrawImage(new PixelShaderEffect(shaderCode)
+                {
+                    Source1 = texture,
+                    Properties =
+                    {
+                        ["hardness"] = hardness,
+                        ["rotate"] = rotate,
+                        ["normalization"] = normalization,
+                        ["pressure"] = 1f,
+                        ["radius"] = sizePressureSmooth,
+                        ["targetPosition"] = positionSmooth,
+                        ["color"] = colorHdr
+                    }
+                });
+            }
+        }
+
+    }
+}
---
+++ Luo Painter.Brushes/StrokeSegment.cs
@@ -0,0 +1,26 @@
+﻿namespace Luo_Painter.Brushes
+{
+    public partial struct StrokeSegment
+    {
+        public readonly float Spacing;
+
+        public readonly float Size;
+        public readonly float StartingSize;
+
+        public readonly float Distance;
+        public readonly float StartingDistance;
+
+        public bool InRadius() => this.Distance <= this.StartingDistance;
+
+        public StrokeSegment(Stroke stroke, float size = 22f, float spacing = 0.25f)
+        {
+            this.Spacing = spacing;
+            
+            this.Size = size;
+            this.StartingSize = System.Math.Max(1, this.Size * stroke.StartingPressure);
+
+            this.Distance = stroke.Distance();
+            this.StartingDistance = this.Spacing * this.StartingSize;
+        }
+    }
+}
