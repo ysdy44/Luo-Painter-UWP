874382a673fec13d306fd5f1dd3bc5a3494af03d
ysdy44 <ysdy44@outlook.com>
2022-10-15 18:36:44 +0800

    Add CompositeHistory

---
+++ Luo Painter.Historys/Models/CompositeHistory.cs
@@ -0,0 +1,22 @@
+﻿namespace Luo_Painter.Historys.Models
+{
+    public class CompositeHistory : IHistory
+    {
+        public HistoryMode Mode => HistoryMode.Composite;
+        public HistoryType Type => HistoryType.None;
+
+        public readonly IHistory[] Histories;
+        public CompositeHistory(IHistory[] histories)
+        {
+            this.Histories = histories;
+        }
+
+        public void Dispose()
+        {
+            foreach (IHistory item in this.Histories)
+            {
+                item.Dispose();
+            }
+        }
+    }
+}
