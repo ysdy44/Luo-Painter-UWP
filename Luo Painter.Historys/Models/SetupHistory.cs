7398b45b70528471562e45d5dad979f2f7d8d647
ysdy44 <ysdy44@outlook.com>
2022-10-15 18:37:10 +0800

    Add SetupHistory

---
+++ Luo Painter.Historys/Models/SetupHistory.cs
@@ -0,0 +1,23 @@
+﻿using Windows.Graphics.Imaging;
+
+namespace Luo_Painter.Historys.Models
+{
+    public class SetupHistory : IHistory
+    {
+        public HistoryMode Mode => HistoryMode.Setup;
+        public HistoryType Type => HistoryType.None;
+        public HistoryType PropertyType => HistoryType.None;
+
+        public readonly BitmapSize UndoParameter;
+        public readonly BitmapSize RedoParameter;
+        public SetupHistory(BitmapSize undoParameter, BitmapSize redoParameter)
+        {
+            this.UndoParameter = undoParameter;
+            this.RedoParameter = redoParameter;
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
