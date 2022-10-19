2194efbe4bb18c2f1f5aa3622ab086d4e2b240e8
ysdy44 <ysdy44@outlook.com>
2022-10-19 21:54:43 +0800

    Add StylePage

---
+++ Luo Painter/$StylePages/StylePage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Windows.Foundation;
+using Windows.UI.Xaml.Controls;
+
+namespace Luo_Painter
+{
+    public sealed partial class StylePage : Page
+    {
+        public StylePage()
+        {
+            this.InitializeComponent();
+            base.SizeChanged += (s, e) =>
+            {
+                if (e.NewSize == Size.Empty) return;
+                if (e.NewSize == e.PreviousSize) return;
+
+                this.AlignmentGrid.RebuildWithInterpolation(e.NewSize);
+            };
+            this.BackButton.Click += (s, e) =>
+            {
+                if (base.Frame.CanGoBack)
+                {
+                    base.Frame.GoBack();
+                }
+            };
+            this.ShowDialogButton.Click += async (s, e) => await this.ContentDialog.ShowAsync();
+        }
+    }
+}
