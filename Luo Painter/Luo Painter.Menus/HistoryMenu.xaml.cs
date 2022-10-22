0ab4691dfda46e79faec0783d9209f4b35ee70f7
ysdy44 <ysdy44@outlook.com>
2022-10-22 11:25:31 +0800

    HistoryMenu UI

--- Luo Painter/Luo Painter.Menus/HistoryMenu.xaml.cs
+++ Luo Painter/Luo Painter.Menus/HistoryMenu.xaml.cs
@@ -1,9 +1,10 @@
 ﻿using Luo_Painter.Elements;
 using Windows.ApplicationModel.Resources;
+using Windows.UI.Xaml.Controls;
 
 namespace Luo_Painter.Menus
 {
-    public sealed partial class HistoryMenu : Expander
+    public sealed partial class HistoryMenu : UserControl
     {
 
         public object ItemsSource { set => this.ListView.ItemsSource = value; }
