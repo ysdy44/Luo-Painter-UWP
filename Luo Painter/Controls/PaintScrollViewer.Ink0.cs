0493e35a6a3305fa3e9bb79ff662671115c21961
ysdy44 <ysdy44@outlook.com>
2022-10-23 18:36:27 +0800

    Add PaintScrollViewer.Ink0.cs

---
+++ Luo Painter/Controls/PaintScrollViewer.Ink0.cs
@@ -0,0 +1,77 @@
+﻿using Luo_Painter.Brushes;
+using Windows.UI.Xaml.Controls;
+
+namespace Luo_Painter.Controls
+{
+    public sealed partial class PaintScrollViewer : UserControl, IInkParameter
+    {
+
+        public void ConstructInk(InkPresenter presenter)
+        {
+            this.InkIsEnabled = false;
+            {
+                this.ComboBox.SelectedIndex = this.InkCollection.IndexOf(presenter.Type);
+
+
+                // 1.Minimum
+                this.SizeSlider.Minimum = this.SizeRange.XRange.Minimum;
+                this.OpacitySlider.Minimum = 0d;
+                this.SpacingSlider.Minimum = this.SpacingRange.XRange.Minimum;
+                this.FlowSlider.Minimum = 0d;
+
+                // 2.Value
+                this.SizeSlider.Value = this.SizeRange.ConvertYToX(presenter.Size);
+                this.OpacitySlider.Value = System.Math.Clamp(presenter.Opacity * 100d, 0d, 100d);
+                this.SpacingSlider.Value = this.SpacingRange.ConvertYToX(presenter.Spacing * 100);
+                this.FlowSlider.Value = System.Math.Clamp(presenter.Flow * 100d, 0d, 100d);
+
+                // 3.Maximum
+                this.SizeSlider.Maximum = this.SizeRange.XRange.Maximum;
+                this.OpacitySlider.Maximum = 100d;
+                this.SpacingSlider.Maximum = this.SpacingRange.XRange.Maximum;
+                this.FlowSlider.Maximum = 100d;
+
+
+                this.IgnoreSizePressureButton.IsOn = presenter.IgnoreSizePressure;
+                this.IgnoreFlowPressureButton.IsOn = presenter.IgnoreFlowPressure;
+
+                switch (presenter.Tip)
+                {
+                    case Windows.UI.Input.Inking.PenTipShape.Circle:
+                        this.TipListBox.SelectedIndex = presenter.IsStroke ? 1 : 0;
+                        break;
+                    case Windows.UI.Input.Inking.PenTipShape.Rectangle:
+                        this.TipListBox.SelectedIndex = presenter.IsStroke ? 3 : 2;
+                        break;
+                    default:
+                        break;
+                }
+
+
+                this.NoneRadioButton.IsChecked = presenter.Hardness is BrushEdgeHardness.None;
+                this.CosineRadioButton.IsChecked = presenter.Hardness is BrushEdgeHardness.Cosine;
+                this.QuadraticRadioButton.IsChecked = presenter.Hardness is BrushEdgeHardness.Quadratic;
+                this.CubeRadioButton.IsChecked = presenter.Hardness is BrushEdgeHardness.Cube;
+                this.QuarticRadioButton.IsChecked = presenter.Hardness is BrushEdgeHardness.Quartic;
+
+                this.BasisRadioButton.IsChecked = presenter.Mode is InkType.None;
+                this.MixRadioButton.IsChecked = presenter.Mode is InkType.Mix;
+                this.BlendRadioButton.IsChecked = presenter.Mode is InkType.Blend;
+
+                this.BlendModeComboBox.SelectedIndex = this.BlendCollection.IndexOf(presenter.BlendMode);
+
+
+                this.RotateButton.IsChecked = presenter.Rotate;
+                this.StepTextBox.Text = presenter.Step.ToString();
+
+                this.ShapeButton.IsOn = presenter.AllowShape;
+                this.ShapeImage.UriSource = string.IsNullOrEmpty(presenter.ShapeTexture) ? null : new System.Uri(presenter.ShapeTexture);
+
+                this.GrainButton.IsOn = presenter.AllowGrain;
+                this.GrainImage.UriSource = string.IsNullOrEmpty(presenter.GrainTexture) ? null : new System.Uri(presenter.GrainTexture);
+            }
+            this.InkIsEnabled = true;
+        }
+
+    }
+}
