3fd03951f3c30abda5c5c8e4c158a4aa02682fd9
ysdy44 <ysdy44@outlook.com>
2022-10-23 12:30:00 +0800

    Add InkExtensions

---
+++ Luo Painter.Brushes/InkExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml;
+
+namespace Luo_Painter.Brushes
+{
+    public static class InkExtensions
+    {
+
+        public static string GetResource(this InkType type) => $"ms-appx:///Luo Painter.Brushes/Icons/{type}Icon.xaml";
+        public static ControlTemplate GetTemplate(this InkType type, ResourceDictionary resource) => resource[$"{type}Icon"] as ControlTemplate;
+
+    }
+}
