5e2112a1dfe178ca94af51df70b554dd4738ec25
ysdy44 <ysdy44@outlook.com>
2022-10-27 00:07:01 +0800

    Add OffsetDialog

---
+++ Luo Painter/Controls/OffsetDialog.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace Luo_Painter.Controls
+{
+    public sealed partial class OffsetDialog : ContentDialog
+    {
+        //@Content
+        public System.Drawing.Point Offset => this.OffsetPicker.Offset;
+
+        //@Construct
+        public OffsetDialog()
+        {
+            this.InitializeComponent();
+        }
+
+        public void Resezing(int x, int y)
+        {
+            this.OffsetPicker.ResezingX(x);
+            this.OffsetPicker.ResezingY(y);
+        }
+    }
+}
