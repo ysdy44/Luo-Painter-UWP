4a7ee2f520dcb4d8954bf02aa89aac973e217789
ysdy44 <ysdy44@outlook.com>
2022-10-29 18:18:17 +0800

    Add StrokeCap

---
+++ Luo Painter.Layers/StrokeCap.cs
@@ -0,0 +1,39 @@
+﻿using System.Numerics;
+using Windows.Foundation;
+
+namespace Luo_Painter.Layers
+{
+    public partial struct StrokeCap
+    {
+        public readonly Vector2 StartingPosition;
+
+        public readonly float StartingPressure;
+
+
+        public readonly float StartingSize;
+        public readonly float Size;
+
+        public readonly Rect Bounds;
+
+
+        public StrokeCap(Vector2 startingPosition, float startingPressure = 1f, float size = 22f)
+        {
+            this.StartingPosition = startingPosition;
+
+            this.StartingPressure = startingPressure;
+
+
+            this.StartingSize = System.Math.Max(1f, size * startingPressure);
+            this.Size = size;
+
+            this.Bounds = new Rect
+            {
+                X = startingPosition.X - size,
+                Y = startingPosition.Y - size,
+                Width = startingPosition.X + size + size,
+                Height = startingPosition.Y + size + size,
+            };
+        }
+
+    }
+}
