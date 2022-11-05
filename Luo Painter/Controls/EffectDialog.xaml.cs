f004cbc2209daf6cea66c20effa81ae1f090d3d5
ysdy44 <ysdy44@outlook.com>
2022-11-05 20:24:37 +0800

    Add EffectDialog

---
+++ Luo Painter/Controls/EffectDialog.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Luo_Painter.Options;
+using System;
+using Windows.UI.Xaml.Controls;
+
+namespace Luo_Painter.Controls
+{
+    public sealed partial class EffectDialog : ContentDialog
+    {
+        //@Delegate
+        public event EventHandler<OptionType> ItemClick;
+   
+        //@Construct
+        public EffectDialog()
+        {
+            this.InitializeComponent();
+            this.ListView.ItemClick += (s, e) =>
+            {
+                if (e.ClickedItem is OptionType item)
+                {
+                    this.ItemClick?.Invoke(this, item);//Delegate
+                }
+            };
+        }
+
+    }
+}
