ab488d43cbb296fe42682da2e955296874797ea5
ysdy44 <ysdy44@outlook.com>
2022-11-05 23:58:28 +0800

    OptionExtensions Attache Type

--- Luo Painter.Options/OptionExtensions.cs
+++ Luo Painter.Options/OptionExtensions.cs
@@ -26,9 +26,43 @@ namespace Luo_Painter.Options
         public static bool IsGeometry(this OptionType type) => type.HasFlag(OptionType.Geometry);
         public static OptionType ToGeometryTransform(this OptionType type) => type.IsGeometry() ? (type | OptionType.WithTransform | OptionType.HasPreview | OptionType.AllowDrag) : type;
 
+        //@Resource
         public static string GetThumbnail(this OptionType type) => type.ExistThumbnail() ? $"ms-appx:///Luo Painter.Options/Thumbnails/{type}.jpg" : throw new NullReferenceException($"The {type} no Tumbnail.");
         public static string GetResource(this OptionType type) => type.ExistIcon() ? $"ms-appx:///Luo Painter.Options/Icons/{type}Icon.xaml" : throw new NullReferenceException($"The {type} no Icon.");
         public static ControlTemplate GetTemplate(this OptionType type, ResourceDictionary resource) => resource[$"{type}Icon"] as ControlTemplate;
-      
+
+        //@Attached
+        public static OptionType GetType(DependencyObject dp) => (OptionType)dp.GetValue(TypeProperty);
+        public static void SetType(DependencyObject dp, OptionType value) => dp.SetValue(TypeProperty, value);
+        public static readonly DependencyProperty TypeProperty = DependencyProperty.RegisterAttached("Type", typeof(OptionType), typeof(MenuFlyoutItem), new PropertyMetadata(default(OptionType), (sender, e) =>
+        {
+            MenuFlyoutItem control = (MenuFlyoutItem)sender;
+
+            if (e.NewValue is OptionType value)
+            {
+                if (value.IsItemClickEnabled())
+                {
+                    control.Text = value.ToString();
+                    control.CommandParameter = value;
+                }
+
+                if (value.ExistIcon())
+                {
+                    control.Resources.Source = new Uri(value.GetResource());
+                    control.Tag = new ContentControl
+                    {
+                        HorizontalAlignment = HorizontalAlignment.Center,
+                        VerticalAlignment = VerticalAlignment.Center,
+                        Template = value.GetTemplate(control.Resources)
+                    };
+                }
+
+                if (value.HasPreview())
+                {
+                    control.KeyboardAcceleratorTextOverride = "•";
+                }
+            }
+        }));
+
     }
 }
