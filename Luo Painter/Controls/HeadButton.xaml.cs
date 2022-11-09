b0842b11ce80cd28c4905edfae65bf83766712bf
ysdy44 <ysdy44@outlook.com>
2022-11-10 00:06:18 +0800

    Add HeadButton

---
+++ Luo Painter/Controls/HeadButton.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Luo_Painter.Options;
+using System;
+using Windows.ApplicationModel.Resources;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Luo_Painter.Controls
+{
+    public sealed partial class HeadButton : StackPanel
+    {
+        //@Delegate
+        public event EventHandler<OptionType> ItemClick { remove => this.Command.Click -= value; add => this.Command.Click += value; }
+        public event RoutedEventHandler MoreClick { remove => this.MoreItem.Click -= value; add => this.MoreItem.Click += value; }
+
+        //@Content
+        public bool PasteIsEnabled { get => this.PasteItem.IsEnabled; set => this.PasteItem.IsEnabled = value; }
+
+        //@Construct
+        public HeadButton()
+        {
+            this.InitializeComponent();
+        }
+
+        //@Strings
+        public void ConstructStrings(ResourceLoader resource)
+        {
+        }
+
+    }
+}
