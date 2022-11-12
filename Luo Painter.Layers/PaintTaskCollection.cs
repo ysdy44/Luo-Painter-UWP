a09194d5958c3e80aa2959b79e74aeda9d6e32c0
ysdy44 <ysdy44@outlook.com>
2022-11-12 23:43:26 +0800

    Add PaintTaskCollection

---
+++ Luo Painter.Layers/PaintTaskCollection.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+
+namespace Luo_Painter.Layers
+{
+    public enum PaintTaskState : byte
+    {
+        Finished,
+
+        Painting,
+        Painted,
+    }
+
+    public enum PaintTaskBehavior : byte
+    {
+        Dead,
+
+        WaitingWork,
+        Working,
+        WorkingBeforeDead,
+    }
+
+    public sealed class PaintTaskCollection : List<StrokeSegment>
+    {
+        public PaintTaskState State { get; set; } = PaintTaskState.Finished;
+        public PaintTaskBehavior GetBehavior()
+        {
+            if (base.Count is 0)
+            {
+                switch (this.State)
+                {
+                    case PaintTaskState.Painting:
+                        return PaintTaskBehavior.WaitingWork;
+                    default:
+                        return PaintTaskBehavior.Dead;
+                }
+            }
+            else
+            {
+                switch (this.State)
+                {
+                    case PaintTaskState.Painting:
+                        return PaintTaskBehavior.Working;
+                    default:
+                        return PaintTaskBehavior.WorkingBeforeDead;
+                }
+            }
+        }
+    }
+}
