b02251dadadfa0452df55b25712cde977e7af2c1
ysdy44 <ysdy44@outlook.com>
2022-11-14 01:10:31 +0800

    Add StrokeCap.Symmetry

---
+++ Luo Painter.Layers/Strokes/StrokeCap.Symmetry.cs
@@ -0,0 +1,63 @@
+﻿using System.Numerics;
+using Windows.UI.Xaml.Controls;
+
+namespace Luo_Painter.Layers
+{
+    public partial struct StrokeCap
+    {
+        public StrokeCap(StrokeCap mirror, Vector2 center, Orientation orientation)
+        {
+            // Paint
+            switch (orientation)
+            {
+                case Orientation.Vertical:
+                    this.StartingPosition.X = mirror.StartingPosition.X;
+                    this.StartingPosition.Y = center.Y + center.Y - mirror.StartingPosition.Y;
+                    break;
+                default:
+                    this.StartingPosition.X = center.X + center.X - mirror.StartingPosition.X;
+                    this.StartingPosition.Y = mirror.StartingPosition.Y;
+                    break;
+            }
+
+            this.StartingPressure = mirror.StartingPressure;
+
+            // Ink
+            this.StartingSize = mirror.StartingSize;
+            this.Size = mirror.Size;
+
+            // Stroke
+            this.Bounds = RectExtensions.GetRect(this.StartingPosition, this.Size);
+        }
+
+        public StrokeCap(StrokeCap symmetry, Vector2 center)
+        {
+            // Paint
+            this.StartingPosition = center + center - symmetry.StartingPosition;
+
+            this.StartingPressure = symmetry.StartingPressure;
+
+            // Ink
+            this.StartingSize = symmetry.StartingSize;
+            this.Size = symmetry.Size;
+
+            // Stroke
+            this.Bounds = RectExtensions.GetRect(this.StartingPosition, this.Size);
+        }
+
+        public StrokeCap(StrokeCap symmetry, Matrix3x2 matrix)
+        {
+            // Paint
+            this.StartingPosition = Vector2.Transform(symmetry.StartingPosition, matrix);
+
+            this.StartingPressure = symmetry.StartingPressure;
+
+            // Ink
+            this.StartingSize = symmetry.StartingSize;
+            this.Size = symmetry.Size;
+
+            // Stroke
+            this.Bounds = RectExtensions.GetRect(this.StartingPosition, this.Size);
+        }
+    }
+}
