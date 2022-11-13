37eb33b1fb008bac8069681966aedbbef10b1253
ysdy44 <ysdy44@outlook.com>
2022-11-14 01:13:52 +0800

    Add DrawPage.Tool.PaintStraw

---
+++ Luo Painter/$DrawPages/DrawPage.Tool.PaintStraw.cs
@@ -0,0 +1,23 @@
+﻿using Luo_Painter.Brushes;
+using Luo_Painter.Layers;
+using Windows.UI.Xaml.Controls;
+
+namespace Luo_Painter
+{
+    public sealed partial class DrawPage : Page, ILayerManager, IInkParameter
+    {
+
+        private void PaintStraw_Start()
+        {
+        }
+
+        private void PaintStraw_Delta()
+        {
+        }
+
+        private void PaintStraw_Complete()
+        {
+        }
+
+    }
+}
