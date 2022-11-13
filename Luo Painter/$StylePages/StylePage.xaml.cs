51e2efd4fcc3322d0a65930bb6a215a0d7186eff
ysdy44 <ysdy44@outlook.com>
2022-11-13 14:32:51 +0800

    StylePage Use SourcePageType

--- Luo Painter/$StylePages/StylePage.xaml.cs
+++ Luo Painter/$StylePages/StylePage.xaml.cs
@@ -1,11 +1,19 @@
 ﻿using System;
 using Windows.Foundation;
+using Windows.UI.Core;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
 
 namespace Luo_Painter
 {
     public sealed partial class StylePage : Page
     {
+        public bool Disabler
+        {
+            get => App.SourcePageType != SourcePageType.StylePage;
+            set => App.SourcePageType = value ? SourcePageType.Invalid : SourcePageType.StylePage;
+        }
+
         public StylePage()
         {
             this.InitializeComponent();
@@ -18,12 +26,47 @@ namespace Luo_Painter
             };
             this.BackButton.Click += (s, e) =>
             {
+                if (this.Disabler) return;
+
                 if (base.Frame.CanGoBack)
                 {
                     base.Frame.GoBack();
                 }
             };
             this.ShowDialogButton.Click += async (s, e) => await this.ContentDialog.ShowAsync();
+        }
+
+        //@BackRequested
+        /// <summary> The current page no longer becomes an active page. </summary>
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            if (SystemNavigationManager.GetForCurrentView() is SystemNavigationManager manager)
+            {
+                manager.BackRequested -= this.BackRequested;
+                manager.AppViewBackButtonVisibility = AppViewBackButtonVisibility.Collapsed;
+            }
+        }
+        /// <summary> The current page becomes the active page. </summary>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (SystemNavigationManager.GetForCurrentView() is SystemNavigationManager manager)
+            {
+                manager.AppViewBackButtonVisibility = AppViewBackButtonVisibility.Visible;
+                manager.BackRequested += this.BackRequested;
+            }
+
+            this.Disabler = false;
+        }
+        private void BackRequested(object sender, BackRequestedEventArgs e)
+        {
+            e.Handled = true;
+
+            if (this.Disabler) return;
+
+            if (base.Frame.CanGoBack)
+            {
+                base.Frame.GoBack();
+            }
         }
     }
 }
