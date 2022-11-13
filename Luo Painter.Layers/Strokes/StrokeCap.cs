4e486ad35d835e72659b4e3aefd98d4037abcf3c
ysdy44 <ysdy44@outlook.com>
2022-11-13 23:07:33 +0800

    StrokeCap Use RectExtensions

--- Luo Painter.Layers/Strokes/StrokeCap.cs
+++ Luo Painter.Layers/Strokes/StrokeCap.cs
@@ -5,35 +5,31 @@ namespace Luo_Painter.Layers
 {
     public partial struct StrokeCap
     {
+        // Paint
         public readonly Vector2 StartingPosition;
 
         public readonly float StartingPressure;
 
-
+        // Ink
         public readonly float StartingSize;
         public readonly float Size;
 
+        // Stroke
         public readonly Rect Bounds;
-
 
         public StrokeCap(Vector2 startingPosition, float startingPressure = 1f, float size = 22f)
         {
+            // Paint
             this.StartingPosition = startingPosition;
 
             this.StartingPressure = startingPressure;
 
-
+            // Ink
             this.StartingSize = System.Math.Max(1f, size * startingPressure);
             this.Size = size;
 
-            this.Bounds = new Rect
-            {
-                X = startingPosition.X - size,
-                Y = startingPosition.Y - size,
-                Width = size + size,
-                Height = size + size,
-            };
+            // Stroke
+            this.Bounds = this.StartingPosition.GetRect(this.Size);
         }
-
     }
 }
