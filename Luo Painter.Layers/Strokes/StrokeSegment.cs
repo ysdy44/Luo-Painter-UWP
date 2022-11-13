e08cf3a7414055d1d89e87fb4593384bb57e05fb
ysdy44 <ysdy44@outlook.com>
2022-11-13 23:08:03 +0800

    StrokeSegment Use RectExtensions

--- Luo Painter.Layers/Strokes/StrokeSegment.cs
+++ Luo Painter.Layers/Strokes/StrokeSegment.cs
@@ -5,14 +5,14 @@ namespace Luo_Painter.Layers
 {
     public partial struct StrokeSegment
     {
-
+        // Paint
         public readonly Vector2 StartingPosition;
         public readonly Vector2 Position;
 
         public readonly float StartingPressure;
         public readonly float Pressure;
 
-
+        // Ink
         public readonly float Spacing;
 
         public readonly float StartingSize;
@@ -21,22 +21,22 @@ namespace Luo_Painter.Layers
         public readonly float StartingDistance;
         public readonly float Distance;
 
-
+        // Stroke
         public readonly Rect Bounds;
         public readonly Vector2 Normalize;
         public readonly bool IsNaN;
         public readonly bool InRadius;
 
-
         public StrokeSegment(Vector2 startingPosition, Vector2 position, float startingPressure = 1f, float pressure = 1f, float size = 22f, float spacing = 0.25f)
         {
+            // Paint
             this.StartingPosition = startingPosition;
             this.Position = position;
 
             this.StartingPressure = startingPressure;
             this.Pressure = pressure;
 
-
+            // Ink
             this.Spacing = spacing;
 
             this.StartingSize = System.Math.Max(1f, size * startingPressure);
@@ -45,14 +45,8 @@ namespace Luo_Painter.Layers
             this.StartingDistance = spacing * this.StartingSize;
             this.Distance = Vector2.Distance(startingPosition, position);
 
-
-            this.Bounds = new Rect
-            {
-                X = System.Math.Min(startingPosition.X, position.X) - size,
-                Y = System.Math.Min(startingPosition.Y, position.Y) - size,
-                Width = System.Math.Abs(startingPosition.X - position.X) + size + size,
-                Height = System.Math.Abs(startingPosition.Y - position.Y) + size + size,
-            };
+            // Stroke
+            this.Bounds = RectExtensions.GetRect(this.StartingPosition, this.Position, this.Size);
 
             Vector2 vector = position - startingPosition;
             this.Normalize = Vector2.Normalize(vector);
@@ -64,6 +58,5 @@ namespace Luo_Painter.Layers
 
             this.InRadius = this.Distance <= this.StartingDistance;
         }
-
     }
 }
