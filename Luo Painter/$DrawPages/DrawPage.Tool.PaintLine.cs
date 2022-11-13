a3c2216fb7845999fd4a3313441bf90501b65892
ysdy44 <ysdy44@outlook.com>
2022-11-14 01:14:31 +0800

    Add DrawPage.Tool.PaintLine

---
+++ Luo Painter/$DrawPages/DrawPage.Tool.PaintLine.cs
@@ -0,0 +1,91 @@
+﻿using Luo_Painter.Blends;
+using Luo_Painter.Brushes;
+using Luo_Painter.Elements;
+using Luo_Painter.Layers;
+using Luo_Painter.Layers.Models;
+using Microsoft.Graphics.Canvas;
+using Windows.Foundation;
+using Windows.UI;
+using Windows.UI.Xaml.Controls;
+
+namespace Luo_Painter
+{
+    public sealed partial class DrawPage : Page, ILayerManager, IInkParameter
+    {
+
+        private void PaintLine_Start()
+        {
+            if (this.CanvasVirtualControl.ReadyToDraw is false) return;
+            if (this.InkType == default) return;
+            if (this.Tasks.State != default) return;
+
+            if (this.LayerSelectedItem is null)
+            {
+                this.Tip(TipType.NoLayer);
+                return;
+            }
+
+            this.BitmapLayer = this.LayerSelectedItem as BitmapLayer;
+            if (this.BitmapLayer is null)
+            {
+                this.Tip(TipType.NotBitmapLayer);
+                return;
+            }
+
+            this.CanvasControl.Invalidate(); // Invalidate
+        }
+
+        private void PaintLine_Delta()
+        {
+            if (this.CanvasVirtualControl.ReadyToDraw is false) return;
+            if (this.InkType == default) return;
+            if (this.BitmapLayer is null) return;
+
+            this.CanvasControl.Invalidate(); // Invalidate
+        }
+
+        private void PaintLine_Complete()
+        {
+            if (this.CanvasVirtualControl.ReadyToDraw is false) return;
+            if (this.InkType == default) return;
+            if (this.BitmapLayer is null) return;
+
+            this.CanvasControl.Invalidate(); // Invalidate
+
+            StrokeSegment segment = new StrokeSegment(this.StartingPosition, this.Position, this.StartingPressure, this.Pressure, this.InkPresenter.Size, this.InkPresenter.Spacing);
+            if (segment.InRadius) return;
+
+            //@Task
+            lock (this.Locker)
+            {
+                this.BitmapLayer.Hit(segment.Bounds);
+                this.PaintSegment(segment);
+
+                if (this.InkType is InkType.Liquefy is false)
+                {
+                    using (CanvasDrawingSession ds = this.BitmapLayer.CreateDrawingSession())
+                    {
+                        ds.Clear(Colors.Transparent);
+                        this.InkPresenter.Preview(ds, this.InkType, this.BitmapLayer[BitmapType.Origin], this.BitmapLayer[BitmapType.Temp]);
+                    }
+                }
+                this.BitmapLayer.Clear(Colors.Transparent, BitmapType.Temp);
+            }
+
+            // History
+            int removes = this.History.Push(this.BitmapLayer.GetBitmapHistory());
+            this.BitmapLayer.Flush();
+            this.BitmapLayer.RenderThumbnail();
+
+            this.BitmapLayer = null;
+            Rect? region = RectExtensions.TryGetRect(this.StartingPoint, this.Point, this.CanvasVirtualControl.Size, this.CanvasVirtualControl.Dpi.ConvertPixelsToDips(segment.Size * this.Transformer.Scale));
+            if (region.HasValue)
+            {
+                this.CanvasVirtualControl.Invalidate(region.Value);
+            }
+
+            this.RaiseHistoryCanExecuteChanged();
+        }
+
+    }
+}
