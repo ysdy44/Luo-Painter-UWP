fdc5560c8c9d710acb5504e27bb89a97f37a5014
ysdy44 <ysdy44@outlook.com>
2022-11-16 02:05:46 +0800

    DrawPage.Tool.Paint: PaintSegmentAsync 2.0.0

--- Luo Painter/$DrawPages/DrawPage.Tool.Paint.cs
+++ Luo Painter/$DrawPages/DrawPage.Tool.Paint.cs
@@ -12,87 +12,91 @@ using Windows.UI.Xaml.Controls;
 
 namespace Luo_Painter
 {
-	public sealed partial class DrawPage : Page, ILayerManager, IInkParameter
-	{
+    public sealed partial class DrawPage : Page, ILayerManager, IInkParameter
+    {
 
-		private void PaintCapAsync(StrokeCap cap) => this.PaintCapAsync(cap, this.ToPoint(cap.StartingPosition));
-		private void PaintCapAsync(StrokeCap cap, Vector2 startingPoint)
-		{
-			//@Task
-			lock (this.Locker)
-			{
-				this.BitmapLayer.Hit(cap.Bounds);
-				this.PaintCap(cap);
-			}
+        private void PaintCapAsync(StrokeCap cap) => this.PaintCapAsync(cap, this.ToPoint(cap.StartingPosition));
+        private void PaintCapAsync(StrokeCap cap, Vector2 startingPoint)
+        {
+            //@Task
+            lock (this.Locker)
+            {
+                this.BitmapLayer.Hit(cap.Bounds);
+                this.PaintCap(cap);
+            }
 
-			Rect? region = RectExtensions.TryGetRect(startingPoint, this.CanvasVirtualControl.Size, this.CanvasVirtualControl.Dpi.ConvertPixelsToDips(cap.Size * this.Transformer.Scale));
-			if (region.HasValue)
-				this.CanvasVirtualControl.Invalidate(region.Value); // Invalidate
-		}
+            Rect? region = RectExtensions.TryGetRect(startingPoint, this.CanvasVirtualControl.Size, this.CanvasVirtualControl.Dpi.ConvertPixelsToDips(cap.Size * this.Transformer.Scale));
+            if (region.HasValue)
+                this.CanvasVirtualControl.Invalidate(region.Value); // Invalidate
+        }
 
-		private async void PaintSegmentAsync()
-		{
-			while (true)
-			{
-				switch (this.Tasks.GetBehavior())
-				{
-					case PaintTaskBehavior.WaitingWork:
-						continue;
-					case PaintTaskBehavior.Working:
-					case PaintTaskBehavior.WorkingBeforeDead:
-						StrokeSegment segment = this.Tasks.First();
-						this.Tasks.Remove(segment);
+        private async void PaintSegmentAsync()
+        {
+            while (true)
+            {
+                switch (this.Tasks.GetBehavior())
+                {
+                    case PaintTaskBehavior.WaitingWork:
+                        continue;
+                    case PaintTaskBehavior.Working:
+                    case PaintTaskBehavior.WorkingBeforeDead:
+                        StrokeSegment segment = this.Tasks.First();
+                        this.Tasks.Remove(segment);
 
-						//@Task
-						lock (this.Locker)
-						{
-							this.BitmapLayer.Hit(segment.Bounds);
-							this.PaintSegment(segment);
-						}
+                        //@Task
+                        lock (this.Locker)
+                        {
+                            this.BitmapLayer.Hit(segment.Bounds);
+                            this.PaintSegment(segment);
+                        }
 
-						Rect? region = RectExtensions.TryGetRect(this.ToPoint(segment.StartingPosition), this.ToPoint(segment.Position), this.CanvasVirtualControl.Size, this.CanvasVirtualControl.Dpi.ConvertPixelsToDips(segment.Size * this.Transformer.Scale));
-						if (region.HasValue)
-						{
-							await this.CanvasVirtualControl.Dispatcher.RunAsync(CoreDispatcherPriority.Low, () =>
-							{
-								this.CanvasVirtualControl.Invalidate(region.Value);
-							});
-						}
-						break;
-					default:
-						//@Paint
-						this.Tasks.State = PaintTaskState.Finished;
+                        Rect? region = RectExtensions.TryGetRect(this.ToPoint(segment.StartingPosition), this.ToPoint(segment.Position), this.CanvasVirtualControl.Size, this.CanvasVirtualControl.Dpi.ConvertPixelsToDips(segment.Size * this.Transformer.Scale));
+                        if (region.HasValue)
+                        {
+                            await this.CanvasVirtualControl.Dispatcher.RunAsync(CoreDispatcherPriority.Low, () =>
+                            {
+                                this.CanvasVirtualControl.Invalidate(region.Value);
+                            });
+                        }
+                        break;
+                    default:
+                        //@Paint
+                        this.Tasks.State = PaintTaskState.Finished;
+                        this.Tasks.Clear();
 
-						await this.CanvasVirtualControl.Dispatcher.RunAsync(CoreDispatcherPriority.Low, () =>
-						{
-							//@Task
-							lock (this.Locker)
-							{
-								if (this.InkType is InkType.Liquefy is false)
-								{
-									using (CanvasDrawingSession ds = this.BitmapLayer.CreateDrawingSession())
-									{
-										ds.Clear(Colors.Transparent);
-										this.InkPresenter.Preview(ds, this.InkType, this.BitmapLayer[BitmapType.Origin], this.BitmapLayer[BitmapType.Temp]);
-									}
-								}
-								this.BitmapLayer.Clear(Colors.Transparent, BitmapType.Temp);
 
-								// History
-								int removes = this.History.Push(this.BitmapLayer.GetBitmapHistory());
-								this.BitmapLayer.Flush();
-								this.BitmapLayer.RenderThumbnail();
+                        var bitmapLayer = this.BitmapLayer;
 
-								this.BitmapLayer = null;
-								this.CanvasVirtualControl.Invalidate(); // Invalidate
+                        //@Task
+                        lock (this.Locker)
+                        {
+                            if (this.InkType is InkType.Liquefy is false)
+                            {
+                                using (CanvasDrawingSession ds = this.BitmapLayer.CreateDrawingSession())
+                                {
+                                    ds.Clear(Colors.Transparent);
+                                    this.InkPresenter.Preview(ds, this.InkType, this.BitmapLayer[BitmapType.Origin], this.BitmapLayer[BitmapType.Temp]);
+                                }
+                            }
+                            this.BitmapLayer.Clear(Colors.Transparent, BitmapType.Temp);
+                            this.BitmapLayer = null;
+                        }
 
-								this.RaiseHistoryCanExecuteChanged();
-							}
-						});
-						return;
-				}
-			}
-		}
+                        await this.CanvasVirtualControl.Dispatcher.RunAsync(CoreDispatcherPriority.Low, () =>
+                        {
+                            // History
+                            int removes = this.History.Push(bitmapLayer.GetBitmapHistory());
+                            bitmapLayer.Flush();
+                            bitmapLayer.RenderThumbnail();
 
-	}
+                            this.CanvasVirtualControl.Invalidate(); // Invalidate
+
+                            this.RaiseHistoryCanExecuteChanged();
+                        });
+                        return;
+                }
+            }
+        }
+
+    }
 }
