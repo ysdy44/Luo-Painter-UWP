f877b610477e465b82158544c9a136e5b151b2e8
ysdy44 <ysdy44@outlook.com>
2022-11-16 23:55:18 +0800

    Add StrawViewer

---
+++ Luo Painter/Controls/StrawViewer.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Graphics.Canvas;
+using System.Linq;
+using Windows.UI;
+using Windows.UI.Xaml.Controls;
+
+namespace Luo_Painter.Controls
+{
+    public sealed partial class StrawViewer : Canvas
+    {
+
+        CanvasRenderTarget StrawRender;
+
+        //@Content
+        public ICanvasImage StrawImage => this.StrawRender;
+        public int StrawCenterX { get; private set; }
+        public int StrawCenterY { get; private set; }
+     
+        //@Construct
+        public StrawViewer()
+        {
+            this.InitializeComponent();
+        }
+
+        public Color GetStraw() => this.StrawRender.GetPixelColors(this.StrawCenterX, this.StrawCenterY, 1, 1).Single();
+        public CanvasDrawingSession CreateDrawingSession() => this.StrawRender.CreateDrawingSession();
+        public void CreateResources(ICanvasResourceCreatorWithDpi resourceCreator)
+        {
+            this.StrawRender = new CanvasRenderTarget(resourceCreator, 112, 90);
+            this.StrawCenterX = (int)StrawRender.SizeInPixels.Width / 2;
+            this.StrawCenterY = (int)StrawRender.SizeInPixels.Height / 2;
+        }
+
+        public void Show() => this.StrawStoryboard.Begin(); // Storyboard
+        public void Hide() => this.HideStoryboard.Begin(); // Storyboard
+        public void Move(double x, double y)
+        {
+            Canvas.SetLeft(this, x - 65);
+            Canvas.SetTop(this, y - 130);
+        }
+
+    }
+}
