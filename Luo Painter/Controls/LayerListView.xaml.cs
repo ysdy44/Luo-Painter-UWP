7779bbbb4811ca5969391c615a420cce5419c66e
ysdy44 <ysdy44@outlook.com>
2022-11-17 23:08:26 +0800

    LayerListView Add Header and Footer

--- Luo Painter/Controls/LayerListView.xaml.cs
+++ Luo Painter/Controls/LayerListView.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using Luo_Painter.Elements;
+using Luo_Painter.Historys;
 using Luo_Painter.Layers;
+using Luo_Painter.Options;
 using System;
 using Windows.ApplicationModel.Resources;
 using Windows.UI.Xaml;
@@ -17,11 +19,39 @@ namespace Luo_Painter.Controls
             remove => this.VisualCommand.Click -= value;
             add => this.VisualCommand.Click += value;
         }
+        public event EventHandler<OptionType> ItemClick2
+        {
+            remove => this.Command.Click -= value;
+            add => this.Command.Click += value;
+        }
+        public event EventHandler<IHistory> History;
+        public event EventHandler<object> Invalidate;
+
+        //@Content
+        public bool PasteLayerIsEnabled { get; set; }
+        public bool IsOpen => this.Flyout.IsOpen;
+        public ImageSource Source { set => this.MarqueeImage.Source = value; }
 
         //@Construct
         public LayerListView()
         {
             this.InitializeComponent();
+            this.MenuFlyout.Opened += (s, e) =>
+            {
+                bool isEnabled = base.SelectedItem is null is false;
+
+                this.CutLayerItem.GoToState(isEnabled);
+                this.CopyLayerItem.GoToState(isEnabled);
+                this.PasteLayerItem.GoToState(this.PasteLayerIsEnabled);
+                this.RemoveItem.GoToState(isEnabled);
+                this.DuplicateItem.GoToState(isEnabled);
+                this.ExtractItem.GoToState(isEnabled);
+                this.MergeItem.GoToState(isEnabled);
+                this.FlattenItem.GoToState(isEnabled);
+                this.GroupItem.GoToState(isEnabled);
+                this.UngroupItem.GoToState(isEnabled);
+                this.ReleaseItem.GoToState(isEnabled);
+            };
         }
 
         //@Strings
