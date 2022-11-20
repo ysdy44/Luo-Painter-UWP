fe407d66cde8b30cd767da50aa6e317c881eb9eb
ysdy44 <ysdy44@outlook.com>
2022-11-20 20:31:56 +0800

    Add ColorButton.Tool.PaintBrush

---
+++ Luo Painter/Controls/ColorButton.Tool.PaintBrush.cs
@@ -0,0 +1,53 @@
+﻿using Luo_Painter.Blends;
+using Luo_Painter.Brushes;
+using Luo_Painter.Layers;
+using Luo_Painter.Layers.Models;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace Luo_Painter.Controls
+{
+    public sealed partial class ColorButton : Button, IInkParameter
+    {
+
+        private async void PaintBrush_Start()
+        {
+            if (this.CanvasControl.ReadyToDraw is false) return;
+            if (this.InkType == default) return;
+            if (this.Tasks.State != default) return;
+
+            //@Paint
+            StrokeCap cap = new StrokeCap(this.StartingPosition, this.StartingPressure, this.InkPresenter.Size);
+            this.PaintCapAsync(cap);
+
+            //@Paint
+            this.Tasks.State = PaintTaskState.Painting;
+            await Task.Run(this.PaintSegmentAsync);
+        }
+
+        private void PaintBrush_Delta()
+        {
+            if (this.CanvasControl.ReadyToDraw is false) return;
+            if (this.InkType == default) return;
+
+            StrokeSegment segment = new StrokeSegment(this.StartingPosition, this.Position, this.StartingPressure, this.Pressure, this.InkPresenter.Size, this.InkPresenter.Spacing);
+            if (segment.InRadius) return;
+
+            //@Paint
+            this.Tasks.Add(segment);
+
+            this.StartingPosition = this.Position;
+            this.StartingPressure = this.Pressure;
+        }
+
+        private void PaintBrush_Complete()
+        {
+            if (this.CanvasControl.ReadyToDraw is false) return;
+            if (this.InkType == default) return;
+
+            //@Paint
+            this.Tasks.State = PaintTaskState.Painted;
+        }
+
+    }
+}
