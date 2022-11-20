4510809e4365933dc5cfe704b731c1bbf3d66d80
ysdy44 <ysdy44@outlook.com>
2022-11-20 17:53:16 +0800

    DrawPage Use Symmetryer

--- Luo Painter/$DrawPages/DrawPage.Tool.PaintBrushMulti.cs
+++ Luo Painter/$DrawPages/DrawPage.Tool.PaintBrushMulti.cs
@@ -12,18 +12,32 @@ namespace Luo_Painter
     public sealed partial class DrawPage : Page, ILayerManager, IInkParameter
     {
 
-        int MultiMode => this.MultiComboBox.SelectedIndex;
-        int MultiMirrorMode => this.MultiMirrorComboBox.SelectedIndex;
-        int MultiCount => (int)this.MultiSlider.Value;
-
-        bool IsMultiSymmetry;
-        readonly IList<Matrix3x2> MultiMatrixs = new List<Matrix3x2>();
+        int SymmetryCount => (int)this.SymmetrySlider.Value;
+        SymmetryMode SymmetryMode
+        {
+            get
+            {
+                switch (this.SymmetryComboBox.SelectedIndex)
+                {
+                    case 0: return SymmetryMode.Horizontal;
+                    case 1: return SymmetryMode.Vertical;
+                    case 2: return SymmetryMode.Symmetry;
+                    case 3: return SymmetryMode.Mirror;
+                    default: return SymmetryMode.None;
+                }
+            }
+        }
 
         private async void PaintBrushMulti_Start()
         {
             if (this.CanvasVirtualControl.ReadyToDraw is false) return;
             if (this.InkType == default) return;
             if (this.Tasks.State != default) return;
+
+            //@Paint
+            int count = this.SymmetryCount;
+            this.SymmetryType = this.Symmetryer.GetType(this.SymmetryMode, count);
+            if (this.SymmetryMode == default) return;
 
             if (this.LayerSelectedItem is null)
             {
@@ -39,70 +53,10 @@ namespace Luo_Painter
             }
 
             //@Paint
-            StrokeCap cap = new StrokeCap(this.StartingPosition, this.StartingPressure, this.InkPresenter.Size);
-            switch (this.MultiMode)
+            this.Symmetryer.Construct(count, this.BitmapLayer.Center);
+            foreach (StrokeCap cap in this.Symmetryer.GetCaps(this.SymmetryType, new StrokeCap(this.StartingPosition, this.StartingPressure, this.InkPresenter.Size), this.BitmapLayer.Center))
             {
-                case 0:
-                    switch (this.MultiMirrorMode)
-                    {
-                        case 0:
-                            this.PaintCapAsync(cap);
-                            this.PaintCapAsync(new StrokeCap(cap, this.BitmapLayer.Center, Orientation.Horizontal));
-                            break;
-                        case 1:
-                            this.PaintCapAsync(cap);
-                            this.PaintCapAsync(new StrokeCap(cap, this.BitmapLayer.Center, Orientation.Vertical));
-                            break;
-                        case 2:
-                            this.PaintCapAsync(cap);
-                            this.PaintCapAsync(new StrokeCap(cap, this.BitmapLayer.Center, Orientation.Horizontal));
-                            this.PaintCapAsync(new StrokeCap(cap, this.BitmapLayer.Center, Orientation.Vertical));
-                            this.PaintCapAsync(new StrokeCap(cap, this.BitmapLayer.Center));
-                            break;
-                        default:
-                            break;
-                    }
-                    break;
-                case 1:
-                    {
-                        int count = this.MultiCount;
-
-                        this.IsMultiSymmetry = (count <= 2);
-                        if (this.IsMultiSymmetry) break;
-
-                        this.MultiMatrixs.Clear();
-                        for (int i = 1; i < count; i++)
-                        {
-                            this.MultiMatrixs.Add(Matrix3x2.CreateRotation(FanKit.Math.PiTwice / count * i, this.BitmapLayer.Center));
-                        }
-
-                        this.PaintCapAsync(cap);
-                        foreach (Matrix3x2 item in this.MultiMatrixs)
-                        {
-                            this.PaintCapAsync(new StrokeCap(cap, item));
-                        }
-                    }
-                    break;
-                case 2:
-                    {
-                        int count = this.MultiCount;
-
-                        this.MultiMatrixs.Clear();
-                        for (int i = 0; i < count; i++)
-                        {
-                            this.MultiMatrixs.Add(Matrix3x2.CreateRotation(FanKit.Math.PiTwice / count * i, this.BitmapLayer.Center));
-                        }
-
-                        foreach (Matrix3x2 item in this.MultiMatrixs)
-                        {
-                            StrokeCap cap2 = new StrokeCap(cap, item);
-                            this.PaintCapAsync(cap2);
-                            this.PaintCapAsync(new StrokeCap(cap2, this.BitmapLayer.Center, Orientation.Horizontal));
-                        }
-                    }
-                    break;
-                default:
-                    break;
+                this.PaintCapAsync(cap);
             }
 
             //@Paint
@@ -116,52 +70,13 @@ namespace Luo_Painter
         {
             if (this.CanvasVirtualControl.ReadyToDraw is false) return;
             if (this.InkType == default) return;
+            if (this.SymmetryMode == default) return;
             if (this.BitmapLayer is null) return;
 
             //@Paint
-            StrokeSegment segment = new StrokeSegment(this.StartingPosition, this.Position, this.StartingPressure, this.Pressure, this.InkPresenter.Size, this.InkPresenter.Spacing);
-            if (segment.InRadius) return;
-            this.Tasks.Add(segment);
-
-            switch (this.MultiMode)
+            foreach (StrokeSegment segment in this.Symmetryer.GetSegments(this.SymmetryType, new StrokeSegment(this.StartingPosition, this.Position, this.StartingPressure, this.Pressure, this.InkPresenter.Size, this.InkPresenter.Spacing), this.BitmapLayer.Center))
             {
-                case 0:
-                    switch (this.MultiMirrorMode)
-                    {
-                        case 0:
-                            this.Tasks.Add(new StrokeSegment(segment, this.BitmapLayer.Center, Orientation.Horizontal));
-                            break;
-                        case 1:
-                            this.Tasks.Add(new StrokeSegment(segment, this.BitmapLayer.Center, Orientation.Vertical));
-                            break;
-                        case 2:
-                            this.Tasks.Add(new StrokeSegment(segment, this.BitmapLayer.Center, Orientation.Horizontal));
-                            this.Tasks.Add(new StrokeSegment(segment, this.BitmapLayer.Center, Orientation.Vertical));
-                            this.Tasks.Add(new StrokeSegment(segment, this.BitmapLayer.Center));
-                            break;
-                        default:
-                            break;
-                    }
-                    break;
-                case 1:
-                    if (this.IsMultiSymmetry)
-                        this.Tasks.Add(new StrokeSegment(segment, this.BitmapLayer.Center));
-                    else
-                        foreach (Matrix3x2 item in this.MultiMatrixs)
-                        {
-                            this.Tasks.Add(new StrokeSegment(segment, item));
-                        }
-                    break;
-                case 2:
-                    foreach (Matrix3x2 item in this.MultiMatrixs)
-                    {
-                        StrokeSegment segment2 = new StrokeSegment(segment, item);
-                        this.Tasks.Add(segment2);
-                        this.Tasks.Add(new StrokeSegment(segment2, this.BitmapLayer.Center, Orientation.Horizontal));
-                    }
-                    break;
-                default:
-                    break;
+                this.Tasks.Add(segment);
             }
 
             this.StartingPosition = this.Position;
@@ -175,6 +90,7 @@ namespace Luo_Painter
         {
             if (this.CanvasVirtualControl.ReadyToDraw is false) return;
             if (this.InkType == default) return;
+            if (this.SymmetryMode == default) return;
             if (this.BitmapLayer is null) return;
 
             this.CanvasControl.Invalidate(); // Invalidate
