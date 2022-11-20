739d99ed21b4352d40ca1ff950e4b97025873d42
ysdy44 <ysdy44@outlook.com>
2022-11-20 20:28:45 +0800

    ColorButton Add PaintCapAsync and PaintSegmentAsync

--- Luo Painter/Controls/ColorButton.Tool.Paint.cs
+++ Luo Painter/Controls/ColorButton.Tool.Paint.cs
@@ -16,6 +16,75 @@ namespace Luo_Painter.Controls
     public sealed partial class ColorButton : Button, IInkParameter
     {
 
+        private void PaintCapAsync(StrokeCap cap)
+        {
+            //@Task
+            lock (this.Locker)
+            {
+                this.BitmapLayer.Hit(cap.Bounds);
+                this.PaintCap(cap);
+            }
+
+            this.CanvasControl.Invalidate(); // Invalidate
+        }
+
+        private async void PaintSegmentAsync()
+        {
+            while (true)
+            {
+                switch (this.Tasks.GetBehavior())
+                {
+                    case PaintTaskBehavior.WaitingWork:
+                        continue;
+                    case PaintTaskBehavior.Working:
+                    case PaintTaskBehavior.WorkingBeforeDead:
+                        StrokeSegment segment = this.Tasks.First();
+                        this.Tasks.RemoveAt(0);
+
+                        //@Task
+                        lock (this.Locker)
+                        {
+                            this.BitmapLayer.Hit(segment.Bounds);
+                            this.PaintSegment(segment);
+                        }
+
+                        await this.CanvasControl.Dispatcher.RunAsync(CoreDispatcherPriority.Low, () =>
+                        {
+                            this.CanvasControl.Invalidate();
+                        });
+                        break;
+                    default:
+                        //@Paint
+                        this.Tasks.State = PaintTaskState.Finished;
+                        this.Tasks.Clear();
+
+                        //@Task
+                        lock (this.Locker)
+                        {
+                            if (this.InkType is InkType.Liquefy is false)
+                            {
+                                using (CanvasDrawingSession ds = this.BitmapLayer.CreateDrawingSession())
+                                {
+                                    ds.Clear(Colors.Transparent);
+                                    this.InkPresenter.Preview(ds, this.InkType, this.BitmapLayer[BitmapType.Origin], this.BitmapLayer[BitmapType.Temp]);
+                                }
+                            }
+                            this.BitmapLayer.Clear(Colors.Transparent, BitmapType.Temp);
+                        }
+
+                        await this.CanvasControl.Dispatcher.RunAsync(CoreDispatcherPriority.Low, () =>
+                        {
+                            // History
+                            this.BitmapLayer.Flush();
+                            this.BitmapLayer.RenderThumbnail();
+
+                            this.CanvasControl.Invalidate(); // Invalidate
+                        });
+                        return;
+                }
+            }
+        }
+
         private async void Paint_Start()
         {
             if (this.CanvasControl.ReadyToDraw is false) return;
