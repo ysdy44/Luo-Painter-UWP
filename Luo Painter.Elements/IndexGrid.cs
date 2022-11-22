ce0ee39d27ae4e4e276ca831b6ec12d8e53f29d5
ysdy44 <ysdy44@outlook.com>
2022-11-23 02:26:35 +0800

    Add IndexGrid

---
+++ Luo Painter.Elements/IndexGrid.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Luo_Painter.Elements
+{
+    public class IndexGrid : Grid
+    {
+        #region DependencyProperty
+
+        /// <summary>
+        /// Gets or sets the index indicating the all Children.
+        /// </summary>     
+        public int Index
+        {
+            get => (int)base.GetValue(IndexProperty);
+            set => base.SetValue(IndexProperty, value);
+        }
+        /// <summary> Identifies the <see cref="Index"/> property. </summary>
+        public static readonly DependencyProperty IndexProperty = DependencyProperty.Register(nameof(Type), typeof(int), typeof(IndexGrid), new PropertyMetadata(-1, (sender, e) =>
+        {
+            IndexGrid control = (IndexGrid)sender;
+
+            if (e.NewValue is int value && value >= 0)
+            {
+                for (int i = 0; i < control.Children.Count; i++)
+                {
+                    control.Children[i].Visibility = i == value ? Visibility.Visible : Visibility.Collapsed;
+                }
+            }
+            else
+            {
+                foreach (UIElement item in control.Children)
+                {
+                    item.Visibility = Visibility.Collapsed;
+                }
+            }
+        }));
+
+        #endregion
+    }
+}
