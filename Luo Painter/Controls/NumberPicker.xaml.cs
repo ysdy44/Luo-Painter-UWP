a16e73da6df2ca372436aba40acb4d2e2cba30fa
ysdy44 <ysdy44@outlook.com>
2022-11-24 02:44:56 +0800

    Add NumberPicker

---
+++ Luo Painter/Controls/NumberPicker.xaml.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+
+namespace Luo_Painter.Controls
+{
+    public interface INumberSlider
+    {
+        /// <summary> <see cref="RangeBase.Value"/> </summary>
+        int Number { get; }
+        /// <summary> <see cref="RangeBase.Minimum"/> </summary>
+        int NumberMinimum { get; }
+        /// <summary> <see cref="RangeBase.Maximum"/> </summary>
+        int NumberMaximum { get; }
+
+        string Unit { get; }
+    }
+
+    //@Delegate
+    public delegate void NumberChangedHandler(object sender, int number);
+
+    public sealed partial class NumberPicker : UserControl
+    {
+        //@Delegate
+        public event NumberChangedHandler NumberChanging = null;
+        public event NumberChangedHandler NumberChanged = null;
+        public event NumberChangedHandler Close = null;
+
+        //@Content
+        public bool IsNegative { get; private set; }
+        public int Absnumber { get; private set; }
+
+        /// <summary> <see cref="RangeBase.Minimum"/> </summary>
+        public int Minimum { get; private set; }
+        /// <summary> <see cref="RangeBase.Maximum"/> </summary>
+        public int Maximum { get; private set; } = 100;
+
+        public string Unit { get; private set; }
+
+        int Timers;
+
+        //@Construct
+        public NumberPicker()
+        {
+            this.InitializeComponent();
+            this.OneButton.Click += (s, e) => this.Input(1);
+            this.TwoButton.Click += (s, e) => this.Input(2);
+            this.ThreeButton.Click += (s, e) => this.Input(3);
+            this.FourButton.Click += (s, e) => this.Input(4);
+            this.FiveButton.Click += (s, e) => this.Input(5);
+            this.SixButton.Click += (s, e) => this.Input(6);
+            this.SevenButton.Click += (s, e) => this.Input(7);
+            this.EightButton.Click += (s, e) => this.Input(8);
+            this.NineButton.Click += (s, e) => this.Input(9);
+            this.ZeroButton.Click += (s, e) =>
+            {
+                if (this.Timers is 0)
+                {
+                    this.Invalidate(); // Invalidate
+                }
+                else
+                {
+                    if (this.IsZero()) return;
+                    if (this.CanInput() is false) return;
+                    this.Absnumber *= 10;
+
+                    this.NumberChanging?.Invoke(this, this.ToValue()); // Delegate
+                    this.Invalidate(); // Invalidate
+                }
+            };
+
+            this.ClearButton.Click += (s, e) =>
+            {
+                if (this.Timers is 0)
+                {
+                    this.Invalidate(); // Invalidate
+                }
+                else
+                {
+                    if (this.IsZero()) return;
+                    this.IsNegative = false;
+                    this.Absnumber = 0;
+
+                    this.NumberChanging?.Invoke(this, this.ToValue()); // Delegate
+                    this.Invalidate(); // Invalidate
+                }
+            };
+            this.BackButton.Click += (s, e) =>
+            {
+                if (this.Timers is 0)
+                {
+                    this.Invalidate(); // Invalidate
+                }
+                else
+                {
+                    if (this.IsZero()) return;
+                    this.Absnumber /= 10;
+
+                    this.NumberChanging?.Invoke(this, this.ToValue()); // Delegate
+                    this.Invalidate(); // Invalidate
+                }
+            };
+            this.NegativeButton.Click += (s, e) =>
+            {
+                if (this.Timers is 0)
+                {
+                    this.Invalidate(); // Invalidate
+                }
+                else
+                {
+                    if (this.IsZero()) return;
+                    this.IsNegative = !this.IsNegative;
+
+                    this.NumberChanging?.Invoke(this, this.ToValue()); // Delegate
+                    this.Invalidate(); // Invalidate
+                }
+            };
+
+            this.OKButton.Click += (s, e) =>
+            {
+                this.NumberChanged?.Invoke(this, this.ToValue()); // Delegate
+            };
+            this.CancelButton.Click += (s, e) =>
+            {
+                this.Close?.Invoke(this, this.ToValue()); // Delegate
+            };
+
+            this.PasteButton.Click += async (s, e) =>
+            {
+                DataPackageView view = Clipboard.GetContent();
+                if (view.Contains(StandardDataFormats.Text))
+                {
+                    string text = await view.GetTextAsync();
+
+                    if (int.TryParse(text, out int value))
+                    {
+                        this.IsNegative = value < 0;
+                        this.Absnumber = System.Math.Abs(value);
+
+                        this.NumberChanging?.Invoke(this, this.ToValue()); // Delegate
+                        this.Invalidate(); // Invalidate
+                    }
+                    else if (double.TryParse(text, out double value2))
+                    {
+                        int value3 = (int)value2;
+                        this.IsNegative = value3 < 0;
+                        this.Absnumber = System.Math.Abs(value3);
+
+                        this.NumberChanging?.Invoke(this, this.ToValue()); // Delegate
+                        this.Invalidate(); // Invalidate
+                    }
+                }
+            };
+        }
+
+        public void Construct(INumberSlider placementTarget)
+        {
+            this.IsNegative = placementTarget.Number < 0;
+            this.Absnumber = System.Math.Abs(placementTarget.Number);
+
+            this.Minimum = placementTarget.NumberMinimum;
+            this.Maximum = placementTarget.NumberMaximum;
+
+            this.Unit = placementTarget.Unit;
+
+            // UI
+            this.Timers = 0;
+            VisualStateManager.GoToState(this, this.InRange() ? nameof(Normal) : nameof(Disabled), true);
+            this.TitleTextBlock.Text = this.ToString();
+            this.TitleTextBlock.SelectAll();
+        }
+        private void Invalidate()
+        {
+            // UI
+            this.Timers++;
+            VisualStateManager.GoToState(this, this.InRange() ? nameof(Normal) : nameof(Disabled), true);
+            this.TitleTextBlock.Text = this.ToString();
+        }
+        private void Input(int value)
+        {
+            if (this.Timers is 0)
+            {
+                this.Absnumber = value;
+            }
+            else
+            {
+                if (this.CanInput() is false) return;
+                this.Absnumber = this.Absnumber * 10 + value;
+            }
+
+            this.NumberChanging?.Invoke(this, this.ToValue()); // Delegate
+            this.Invalidate(); // Invalidate
+        }
+
+
+        //@Override   
+        /// <summary> <see cref="Int32.MaxValue"/> / 100 </summary>
+        public bool CanInput() => this.Absnumber < 21474836;
+        public bool IsZero() => this.Absnumber is 0;
+        public bool InRange()
+        {
+            int value = this.IsNegative ? -this.Absnumber : this.Absnumber;
+            if (value < this.Minimum) return false;
+            if (value > this.Maximum) return false;
+            return true;
+        }
+        public int ToValue()
+        {
+            int value = this.IsNegative ? -this.Absnumber : this.Absnumber;
+            if (value <= this.Minimum) return this.Minimum;
+            if (value >= this.Maximum) return this.Maximum;
+            return value;
+        }
+        public override string ToString()
+        {
+            if (string.IsNullOrEmpty(this.Unit))
+            {
+                if (this.IsNegative)
+                    return $"- {this.Absnumber}";
+                else
+                    return $"{this.Absnumber}";
+            }
+            else
+            {
+                if (this.IsNegative)
+                    return $"- {this.Absnumber} {this.Unit}";
+                else
+                    return $"{this.Absnumber} {this.Unit}";
+            }
+        }
+
+    }
+}
