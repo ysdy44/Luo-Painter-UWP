849ca1fb63ce5baaefdd67b33cc4e9e4c1c45c18
ysdy44 <ysdy44@outlook.com>
2022-11-26 19:51:35 +0800

    File: Split ToastExtensions

---
+++ Luo Painter.Elements/ToastAudioType.cs
@@ -0,0 +1,74 @@
+﻿namespace Luo_Painter.Elements
+{
+    public enum ToastAudioType : int
+    {
+        Default,
+
+        IM,
+        Mail,
+        Reminder,
+        SMS,
+
+        Looping_Alarm,
+        Looping_Alarm2,
+        Looping_Alarm3,
+        Looping_Alarm4,
+        Looping_Alarm5,
+        Looping_Alarm6,
+        Looping_Alarm7,
+        Looping_Alarm8,
+        Looping_Alarm9,
+        Looping_Alarm10,
+
+        Looping_Call,
+        Looping_Call2,
+        Looping_Call3,
+        Looping_Call4,
+        Looping_Call5,
+        Looping_Call6,
+        Looping_Call7,
+        Looping_Call8,
+        Looping_Call9,
+        Looping_Call10,
+    }
+
+    public static partial class ToastExtensions
+    {
+
+        public static string ToSource(this ToastAudioType audioName)
+        {
+            switch (audioName)
+            {
+                case ToastAudioType.IM: return "ms-winsoundevent:Notification.IM";
+                case ToastAudioType.Mail: return "ms-winsoundevent:Notification.Mail";
+                case ToastAudioType.Reminder: return "ms-winsoundevent:Notification.Reminder";
+                case ToastAudioType.SMS: return "ms-winsoundevent:Notification.SMS";
+
+                case ToastAudioType.Looping_Alarm: return "ms-winsoundevent:Notification.Looping.Alarm";
+                case ToastAudioType.Looping_Alarm2: return "ms-winsoundevent:Notification.Looping.Alarm2";
+                case ToastAudioType.Looping_Alarm3: return "ms-winsoundevent:Notification.Looping.Alarm3";
+                case ToastAudioType.Looping_Alarm4: return "ms-winsoundevent:Notification.Looping.Alarm4";
+                case ToastAudioType.Looping_Alarm5: return "ms-winsoundevent:Notification.Looping.Alarm5";
+                case ToastAudioType.Looping_Alarm6: return "ms-winsoundevent:Notification.Looping.Alarm6";
+                case ToastAudioType.Looping_Alarm7: return "ms-winsoundevent:Notification.Looping.Alarm7";
+                case ToastAudioType.Looping_Alarm8: return "ms-winsoundevent:Notification.Looping.Alarm8";
+                case ToastAudioType.Looping_Alarm9: return "ms-winsoundevent:Notification.Looping.Alarm9";
+                case ToastAudioType.Looping_Alarm10: return "ms-winsoundevent:Notification.Looping.Alarm10";
+
+                case ToastAudioType.Looping_Call: return "ms-winsoundevent:Notification.Looping.Call";
+                case ToastAudioType.Looping_Call2: return "ms-winsoundevent:Notification.Looping.Call2";
+                case ToastAudioType.Looping_Call3: return "ms-winsoundevent:Notification.Looping.Call3";
+                case ToastAudioType.Looping_Call4: return "ms-winsoundevent:Notification.Looping.Call4";
+                case ToastAudioType.Looping_Call5: return "ms-winsoundevent:Notification.Looping.Call5";
+                case ToastAudioType.Looping_Call6: return "ms-winsoundevent:Notification.Looping.Call6";
+                case ToastAudioType.Looping_Call7: return "ms-winsoundevent:Notification.Looping.Call7";
+                case ToastAudioType.Looping_Call8: return "ms-winsoundevent:Notification.Looping.Call8";
+                case ToastAudioType.Looping_Call9: return "ms-winsoundevent:Notification.Looping.Call9";
+                case ToastAudioType.Looping_Call10: return "ms-winsoundevent:Notification.Looping.Call10";
+
+                default: return "ms-winsoundevent:Notification.Default";
+            }
+        }
+
+    }
+}
---
+++ Luo Painter.Elements/ToastExtensions.LaunchFile.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using Windows.ApplicationModel.Core;
+using Windows.Data.Xml.Dom;
+using Windows.Storage;
+using Windows.Storage.AccessCache;
+using Windows.System;
+using Windows.UI.Notifications;
+
+namespace Luo_Painter.Elements
+{
+    public static partial class ToastExtensions
+    {
+
+        public static void Show(string textString, string assetsImageFile, ToastAudioType audioName = default)
+        {
+            XmlDocument doc = ToastNotificationManager.GetTemplateContent(default);
+            doc.SetText(textString);
+            doc.SetImage(assetsImageFile);
+            doc.SetToast(audioName);
+
+            ToastNotification toast = new ToastNotification(doc);
+            ToastNotificationManager.CreateToastNotifier().Show(toast);
+        }
+        public static void Show(StorageFile item)
+        {
+            XmlDocument doc = ToastNotificationManager.GetTemplateContent(default);
+            doc.SetText(item.Name);
+            doc.SetImage(item.Path);
+            doc.SetToast(default);
+
+            ToastNotification toast = new ToastNotification(doc)
+            {
+                Data = new NotificationData(ToastExtensions.ToastValues(item))
+            };
+
+            toast.Activated += ToastExtensions.Toast_Activated;
+            toast.Dismissed += ToastExtensions.Toast_Dismissed;
+            toast.Failed += ToastExtensions.Toast_Failed;
+
+            ToastNotificationManager.CreateToastNotifier().Show(toast);
+        }
+        private static IEnumerable<KeyValuePair<string, string>> ToastValues(IStorageItem item)
+        {
+            yield return new KeyValuePair<string, string>("File", StorageApplicationPermissions.FutureAccessList.Add(item));
+        }
+
+        private static void Toast_Failed(ToastNotification sender, ToastFailedEventArgs args)
+        {
+            sender.Activated -= ToastExtensions.Toast_Activated;
+            sender.Dismissed -= ToastExtensions.Toast_Dismissed;
+            sender.Failed -= ToastExtensions.Toast_Failed;
+        }
+        private static void Toast_Dismissed(ToastNotification sender, ToastDismissedEventArgs args)
+        {
+            sender.Activated -= ToastExtensions.Toast_Activated;
+            sender.Dismissed -= ToastExtensions.Toast_Dismissed;
+            sender.Failed -= ToastExtensions.Toast_Failed;
+        }
+        private static async void Toast_Activated(ToastNotification sender, object args)
+        {
+            sender.Activated -= ToastExtensions.Toast_Activated;
+            sender.Dismissed -= ToastExtensions.Toast_Dismissed;
+            sender.Failed -= ToastExtensions.Toast_Failed;
+
+            if (sender.Data.Values.ContainsKey("File"))
+            {
+                string token = sender.Data.Values["File"];
+
+                StorageFile item = await StorageApplicationPermissions.FutureAccessList.GetFileAsync(token);
+                StorageApplicationPermissions.FutureAccessList.Remove(token);
+
+                await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(default, () => ToastExtensions.LaunchFileAsync(item));
+            }
+        }
+
+        private static async void LaunchFileAsync(StorageFile item)
+        {
+            try
+            {
+                await Launcher.LaunchFolderAsync(await item.GetParentAsync(), new FolderLauncherOptions
+                {
+                    ItemsToSelect = { item }
+                });
+            }
+            catch (Exception)
+            {
+                try
+                {
+                    await Launcher.LaunchFileAsync(item);
+                }
+                catch (Exception)
+                {
+                }
+            }
+        }
+
+    }
+}
