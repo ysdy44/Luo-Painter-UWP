8a4c0e5d1c18a91f4a54b6e97b9ce37b9afeda54
ysdy44 <ysdy44@outlook.com>
2022-12-03 00:21:44 +0800

    Add DrawPage.Picker.Color

---
+++ Luo Painter/$DrawPages/DrawPage.Picker.Color.cs
@@ -0,0 +1,36 @@
+﻿using Luo_Painter.Options;
+using Luo_Painter.Brushes;
+using Luo_Painter.Elements;
+using Luo_Painter.Layers;
+using Windows.UI.Xaml.Controls;
+
+namespace Luo_Painter
+{
+    internal enum ColorPickerMode : byte
+    {
+        None,
+
+        Case0,
+        Case1,
+        Case2,
+        Case3,
+        Case4,
+        Case5,
+        Case6,
+        Case7,
+    }
+
+    public sealed partial class DrawPage : Page, ILayerManager, IInkParameter
+    {
+
+        ColorPickerMode ColorPickerMode;
+
+        private void ColorShowAt(IColorBase color, ColorPickerMode mode = default)
+        {
+            this.ColorPickerMode = mode;
+            this.ColorPicker.Color = color.Color;
+            this.ColorFlyout.ShowAt(color.PlacementTarget);
+        }
+
+    }
+}
