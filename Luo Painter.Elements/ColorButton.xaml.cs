258df3d950295e33429d771694464f4c327ad23a
ysdy44 <ysdy44@outlook.com>
2022-12-03 00:04:50 +0800

    Add ColorButton

---
+++ Luo Painter.Elements/ColorButton.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System.Numerics;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+
+namespace Luo_Painter.Elements
+{
+    public interface IColorBase
+    {
+        /// <summary> <see cref="FlyoutBase.Target"/> </summary>
+        FrameworkElement PlacementTarget { get; }
+
+        Color Color { get; }
+        void SetColor(Color color);
+        void SetColor(Vector4 colorHdr);
+    }
+
+    public interface IColorHdrBase
+    {
+        /// <summary> <see cref="FlyoutBase.Target"/> </summary>
+        FrameworkElement PlacementTarget { get; }
+
+        Vector4 ColorHdr { get; }
+        void SetColorHdr(Vector4 colorHdr);
+        void SetColorHdr(Color color);
+    }
+
+    public sealed partial class ColorButtonBase : Button, IColorHdrBase, IColorBase
+    {
+        //@Content
+        public FrameworkElement PlacementTarget => this;
+
+        public Color Color => this.SolidColorBrush.Color;
+        public Vector4 ColorHdr { get; private set; }
+
+        //@Construct
+        public ColorButtonBase()
+        {
+            this.InitializeComponent();
+        }
+
+        public void SetColor(Color color) => this.SolidColorBrush.Color = color;
+        public void SetColor(Vector4 colorHdr) => this.SetColor(Color.FromArgb((byte)(colorHdr.W * 255f), (byte)(colorHdr.X * 255f), (byte)(colorHdr.Y * 255f), (byte)(colorHdr.Z * 255f)));
+
+        public void SetColorHdr(Vector4 colorHdr) => this.ColorHdr = colorHdr;
+        public void SetColorHdr(Color color) => this.SetColorHdr(new Vector4(color.R, color.G, color.B, color.A) / 255f); // 0~1
+
+        public void Reverse(ColorButtonBase other)
+        {
+            Color color = this.Color;
+            Vector4 colorHdr = this.ColorHdr;
+
+            this.SetColor(other.Color);
+            this.SetColorHdr(other.ColorHdr);
+
+            other.SetColor(color);
+            other.SetColorHdr(colorHdr);
+        }
+    }
+}
