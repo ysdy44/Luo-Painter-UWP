6833d33f984ed15311c708551d45aba7a298b152
ysdy44 <ysdy44@outlook.com>
2022-12-03 00:00:20 +0800

    Add DrawPage.View

--- Luo Painter/$DrawPages/DrawPage.Tool.Vector.cs
+++ Luo Painter/$DrawPages/DrawPage.Tool.Vector.cs
@@ -16,71 +16,8 @@ namespace Luo_Painter
 
         int StrawMode => this.StrawComboBox.SelectedIndex;
 
-        bool IsViewEnabled;
-
         private void ConstructVector()
         {
-            this.RadianClearButton.Tapped += (s, e) => this.RadianStoryboard.Begin(); // Storyboard
-            this.RadianSlider.ValueChanged += (s, e) =>
-            {
-                if (this.IsViewEnabled is false) return;
-                double radian = e.NewValue / 180 * System.Math.PI;
-
-                this.Transformer.Radian = (float)radian;
-                this.Transformer.ReloadMatrix();
-
-                this.CanvasControl.Invalidate(); // Invalidate
-                this.CanvasVirtualControl.Invalidate(); // Invalidate
-            };
-
-            this.ScaleClearButton.Tapped += (s, e) => this.ScaleStoryboard.Begin(); // Storyboard
-            this.ScaleSlider.Minimum = this.ScaleRange.XRange.Minimum;
-            this.ScaleSlider.Maximum = this.ScaleRange.XRange.Maximum;
-            this.ScaleSlider.ValueChanged += (s, e) =>
-            {
-                if (this.IsViewEnabled is false) return;
-                double scale = this.ScaleRange.ConvertXToY(e.NewValue);
-
-                this.Transformer.Scale = (float)scale;
-                this.Transformer.ReloadMatrix();
-
-                this.CanvasControl.Invalidate(); // Invalidate
-                this.CanvasVirtualControl.Invalidate(); // Invalidate
-            };
-
-            this.RemoteControl.Moved += (s, vector) =>
-            {
-                this.Transformer.Position += vector * 20;
-                this.Transformer.ReloadMatrix();
-
-                this.CanvasControl.Invalidate(); // Invalidate
-                this.CanvasVirtualControl.Invalidate(); // Invalidate
-            };
-            this.RemoteControl.ValueChangeStarted += (s, value) => this.Transformer.CacheMove(Vector2.Zero);
-            this.RemoteControl.ValueChangeDelta += (s, value) =>
-            {
-                this.Transformer.Move(value);
-
-                this.CanvasControl.Invalidate(); // Invalidate
-                this.CanvasVirtualControl.Invalidate(); // Invalidate
-            };
-            this.RemoteControl.ValueChangeCompleted += (s, value) =>
-            {
-                this.Transformer.Move(value);
-
-                this.CanvasControl.Invalidate(); // Invalidate
-                this.CanvasVirtualControl.Invalidate(); // Invalidate
-            };
-        }
-
-        public void ConstructView(CanvasTransformer transformer)
-        {
-            this.IsViewEnabled = false;
-
-            this.RadianSlider.Value = transformer.Radian * 180 / System.Math.PI;
-            this.ScaleSlider.Value = this.ScaleRange.ConvertYToX(transformer.Scale);
-
-            this.IsViewEnabled = true;
         }
 
         private void View_Start()
---
+++ Luo Painter/$DrawPages/DrawPage.View.cs
@@ -0,0 +1,95 @@
+﻿using FanKit.Transformers;
+using Luo_Painter.Brushes;
+using Luo_Painter.Layers;
+using System.Numerics;
+using Windows.UI.Xaml.Controls;
+
+namespace Luo_Painter
+{
+    public sealed partial class DrawPage : Page, ILayerManager, IInkParameter
+    {
+
+        bool IsViewEnabled;
+
+        private void ConstructRadian()
+        {
+            this.RadianClearButton.Click += (s, e) => this.RadianStoryboard.Begin(); // Storyboard
+
+            this.RadianRemoveButton.Click += (s, e) => this.RadianSlider.Value --;
+            this.RadianAddButton.Click += (s, e) => this.RadianSlider.Value ++;
+
+            this.RadianSlider.ValueChanged += (s, e) =>
+            {
+                if (this.IsViewEnabled is false) return;
+                double radian = e.NewValue / 180 * System.Math.PI;
+
+                this.Transformer.Radian = (float)radian;
+                this.Transformer.ReloadMatrix();
+
+                this.CanvasControl.Invalidate(); // Invalidate
+                this.CanvasVirtualControl.Invalidate(); // Invalidate
+            };
+        }
+
+        private void ConstructScale()
+        {
+            this.ScaleClearButton.Click += (s, e) => this.ScaleStoryboard.Begin(); // Storyboard
+
+            this.ScaleRemoveButton.Click += (s, e) => this.ScaleSlider.Value--;
+            this.ScaleAddButton.Click += (s, e) => this.ScaleSlider.Value++;
+
+            this.ScaleSlider.Minimum = this.ScaleRange.XRange.Minimum;
+            this.ScaleSlider.Maximum = this.ScaleRange.XRange.Maximum;
+            this.ScaleSlider.ValueChanged += (s, e) =>
+            {
+                if (this.IsViewEnabled is false) return;
+                double scale = this.ScaleRange.ConvertXToY(e.NewValue);
+
+                this.Transformer.Scale = (float)scale;
+                this.Transformer.ReloadMatrix();
+
+                this.CanvasControl.Invalidate(); // Invalidate
+                this.CanvasVirtualControl.Invalidate(); // Invalidate
+            };
+        }
+
+        private void ConstructRemote()
+        {
+            this.RemoteControl.Moved += (s, vector) =>
+            {
+                this.Transformer.Position += vector * 20;
+                this.Transformer.ReloadMatrix();
+
+                this.CanvasControl.Invalidate(); // Invalidate
+                this.CanvasVirtualControl.Invalidate(); // Invalidate
+            };
+
+            this.RemoteControl.ValueChangeStarted += (s, value) => this.Transformer.CacheMove(Vector2.Zero);
+            this.RemoteControl.ValueChangeDelta += (s, value) =>
+            {
+                this.Transformer.Move(value);
+
+                this.CanvasControl.Invalidate(); // Invalidate
+                this.CanvasVirtualControl.Invalidate(); // Invalidate
+            };
+            this.RemoteControl.ValueChangeCompleted += (s, value) =>
+            {
+                this.Transformer.Move(value);
+
+                this.CanvasControl.Invalidate(); // Invalidate
+                this.CanvasVirtualControl.Invalidate(); // Invalidate
+            };
+        }
+
+        public void ConstructView(CanvasTransformer transformer)
+        {
+            this.IsViewEnabled = false;
+
+            this.RadianSlider.Value = transformer.Radian * 180 / System.Math.PI;
+            this.ScaleSlider.Value = this.ScaleRange.ConvertYToX(transformer.Scale);
+
+            this.IsViewEnabled = true;
+        }
+
+    }
+}
