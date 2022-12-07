e366f9658dd14b0d36d07b7e4cb1c0cec1342dd7
ysdy44 <ysdy44@outlook.com>
2022-12-07 22:07:19 +0800

    IColorPicker Add BoxSize and RectangleSize

--- Luo Painter.Elements/IColorPicker.cs
+++ Luo Painter.Elements/IColorPicker.cs
@@ -25,6 +25,27 @@ namespace Luo_Painter.Elements
         void ZoomIn();
     }
 
+    public readonly struct BoxSize
+    {
+        public readonly double S; // = 20;
+
+        public readonly double W; // = 320;
+        public readonly double H; // = 280;
+        public readonly double O; // = 40;
+
+        public double Y1 => (this.H + this.W + this.S) / 2;
+        public double Y2 => (this.H + this.W - this.S) / 2;
+
+        public BoxSize(double size, double slider = 20)
+        {
+            this.W = size; // Width
+            this.H = size - 10 - slider - 10; // Height
+            this.O = this.W - this.H; // Other
+
+            this.S = slider; // Slider
+        }
+    }
+
     public readonly struct WheelSize
     {
         public readonly double R; // = 100;
@@ -35,13 +56,13 @@ namespace Luo_Painter.Elements
 
         public Point C => new Point(this.R, this.R); // = new Point(100, 100);
 
-        public WheelSize(double size, double hole = 0.8f)
+        public WheelSize(double size, double hole = 0.87d)
         {
-            this.R = size / 2;
-            this.D = size;
+            this.R = size / 2; // Radius
+            this.D = size; // Diameter
 
-            this.HR = hole * this.R;
-            this.HD = hole * this.D;
+            this.HR = hole * this.R; // HoleRadius
+            this.HD = hole * this.D; // HoleDiameter
         }
 
         public double XY1(double cossin) => cossin * this.R + this.R;
@@ -60,13 +81,13 @@ namespace Luo_Painter.Elements
 
         public Vector2 C => new Vector2(this.R, this.R); // = new Point(100, 100);
 
-        public WheelSizeF(float size, float hole = 0.8f)
+        public WheelSizeF(float size, float hole = 0.87f)
         {
-            this.R = size / 2;
-            this.D = size;
+            this.R = size / 2; // Radius
+            this.D = size; // Diameter
 
-            this.HR = hole * this.R;
-            this.HD = hole * this.D;
+            this.HR = hole * this.R; // HoleRadius
+            this.HD = hole * this.D; // HoleDiameter
         }
 
         public float XY1(float cossin) => cossin * this.R + this.R;
@@ -113,6 +134,11 @@ namespace Luo_Painter.Elements
         public double Y(double v) => v * this.AH + this.AT;
 
         public Point Triangle(Point position) => new Point(position.X - this.AL, position.Y - this.AT);
+        public Point Triangle(Vector4 hsv) => new Point
+        {
+            X = hsv.Y == 0d ? this.Size.R : Math.Clamp(this.AL + this.AW * hsv.X, this.AL, this.AR),
+            Y = this.AT + this.AH * hsv.Y
+        };
 
         public double V(Point triangle) => Math.Clamp(triangle.Y / this.AH, 0d, 1d);
         public double S(Point triangle, double v)
@@ -122,5 +148,38 @@ namespace Luo_Painter.Elements
             double vh = v / 2d;
             return Math.Clamp(triangle.X / this.AW, 0.5d - vh, 0.5d + vh);
         }
+    }
+
+    public readonly struct RectangleSize
+    {
+        public readonly WheelSize Size;
+
+        public readonly double RD; // = 280 / Math.Sqrt(2);
+
+        public readonly double RT; // = C - R;
+        public readonly double RB; // = C + R;
+
+        public RectangleSize(WheelSize size)
+        {
+            this.Size = size;
+
+            double rr = size.HR / Math.Sqrt(2); // RectangleRadius
+            this.RD = rr * 2; // RectangleDiameter
+
+            this.RT = size.R - rr; // RectangleTop
+            this.RB = size.R + rr; // RectangleBottom
+        }
+
+        public double X(double s) => s * this.RD + this.RT;
+        public double Y(double v) => (1d - v) * this.RD + this.RT;
+
+        public Point Rectangle(Vector4 hsv) => new Point
+        {
+            X = this.RD * hsv.X,
+            Y = this.RD * (1d - hsv.Y)
+        };
+
+        public double V(Point rectangle) => Math.Clamp(1d - rectangle.Y / this.RD, 0d, 1d);
+        public double S(Point rectangle) => Math.Clamp(rectangle.X / this.RD, 0d, 1d);
     }
 }
