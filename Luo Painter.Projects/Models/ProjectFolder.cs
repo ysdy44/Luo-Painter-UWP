ed01f9c3793eba1f7c4a7b44c34dfa986ec45c23
ysdy44 <ysdy44@outlook.com>
2022-12-20 23:47:00 +0800

    ProjectFolder 2.0.0

--- Luo Painter.Projects/Models/ProjectFolder.cs
+++ Luo Painter.Projects/Models/ProjectFolder.cs
@@ -2,20 +2,23 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Windows.Storage;
+using Windows.System;
 
 namespace Luo_Painter.Projects.Models
 {
     public sealed class ProjectFolder : Project
     {
 
-        //@Static
-        const string LoadFaill = @"ms-appx:///Icons\LoadFaill.jpg";
-
+        //@Content
         public string ThumbnailLeft { get; private set; }
         public string ThumbnailRight { get; private set; }
 
+        //@Construct
         public ProjectFolder(StorageFolder item, IReadOnlyList<StorageFile> images = null) : base(StorageItemTypes.Folder)
         {
+            //@Static
+            const string fallbackImage = @"ms-appx:///Icons\LoadFaill.jpg";
+
             base.Path = item.Path;
             base.Name = item.Name;
             base.DisplayName = item.DisplayName;
@@ -23,14 +26,14 @@ namespace Luo_Painter.Projects.Models
 
             if (images is null)
             {
-                base.Thumbnail = this.ThumbnailLeft = this.ThumbnailRight = ProjectFolder.LoadFaill;
+                base.Thumbnail = this.ThumbnailLeft = this.ThumbnailRight = fallbackImage;
                 return;
             }
 
             switch (images.Count)
             {
                 case 0:
-                    base.Thumbnail = this.ThumbnailLeft = this.ThumbnailRight = ProjectFolder.LoadFaill;
+                    base.Thumbnail = this.ThumbnailLeft = this.ThumbnailRight = fallbackImage;
                     break;
                 case 1:
                     base.Thumbnail = this.ThumbnailLeft = this.ThumbnailRight = images[0].Path;
@@ -47,29 +50,53 @@ namespace Luo_Painter.Projects.Models
             }
         }
 
-        public async Task RenameAsync(string name)
+        public async Task<bool> RenameAsync(string name)
         {
-            StorageFolder zipFolder = await StorageFolder.GetFolderFromPathAsync(base.Path);
-            await zipFolder.RenameAsync(name, NameCollisionOption.GenerateUniqueName);
+            string path = this.Path;
 
-            base.Path = zipFolder.Path;
-            base.Name = zipFolder.Name;
-            base.DisplayName = zipFolder.DisplayName;
-            base.DateCreated = zipFolder.DateCreated;
+            StorageFolder item = null;
+            try { item = await StorageFolder.GetFolderFromPathAsync(path); }
+            catch (Exception) { }
+            if (item is null) return false;
+
+            await item.RenameAsync(name, NameCollisionOption.GenerateUniqueName);
+
+            base.Path = item.Path;
+            base.Name = item.Name;
+            base.DisplayName = item.DisplayName;
+            base.DateCreated = item.DateCreated;
             this.OnPropertyChanged(nameof(Name)); // Notify 
             this.OnPropertyChanged(nameof(DisplayName)); // Notify 
             this.OnPropertyChanged(nameof(DateCreated)); // Notify 
 
-            string thumbnail = System.IO.Path.Combine(zipFolder.Path, "Thumbnail.png");
-            this.Thumbnail = thumbnail;
+            string thumbnail = System.IO.Path.Combine(item.Path, "Thumbnail.png");
+            base.Thumbnail = thumbnail;
             this.ThumbnailLeft = thumbnail;
             this.ThumbnailRight = thumbnail;
+            this.OnPropertyChanged(nameof(Thumbnail)); // Notify 
+            this.OnPropertyChanged(nameof(ThumbnailLeft)); // Notify 
+            this.OnPropertyChanged(nameof(ThumbnailRight)); // Notify 
+
+            return true;
         }
 
-        public async Task DeleteAsync()
+        public async Task<bool> DeleteAsync()
         {
-            StorageFolder item = await StorageFolder.GetFolderFromPathAsync(this.Path);
+            string path = this.Path;
+
+            StorageFolder item = null;
+            try { item = await StorageFolder.GetFolderFromPathAsync(path); }
+            catch (Exception) { }
+            if (item is null) return false;
+
             await item.DeleteAsync();
+
+            return true;
+        }
+
+        public async void LocalAsync()
+        {
+            await Launcher.LaunchFolderPathAsync(base.Path);
         }
 
     }
