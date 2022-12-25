af8212d040eea2802ce0552b9d0ab8b4339cd681
ysdy44 <ysdy44@outlook.com>
2022-12-26 03:02:58 +0800

    Add CurveStop

---
+++ Luo Painter.HSVColorPickers/Selectors/CurveStop.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+
+namespace Luo_Painter.HSVColorPickers
+{    
+    public class CurveStop : INotifyPropertyChanged
+    {
+        public float Offset
+        {
+            get => this.offset;
+            set
+            {
+                this.offset = value;
+                this.OnPropertyChanged(nameof(Offset)); // Notify 
+            }
+        }
+        private float offset;
+
+        //@Notify 
+        /// <summary> Multicast event for property change notifications. </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+        /// <summary>
+        /// Notifies listeners that a property value has changed.
+        /// </summary>
+        /// <param name="propertyName"> Name of the property used to notify listeners. </param>
+        protected void OnPropertyChanged(string propertyName) => this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
