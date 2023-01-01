e1627cb50f809d422ef1e235ea23296c86911977
ysdy44 <ysdy44@outlook.com>
2023-01-01 16:31:51 +0800

    Add RandomAccessStreamExtensions

---
+++ Luo Painter.Projects/RandomAccessStreamExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Graphics.Imaging;
+using Windows.Storage;
+using Windows.Storage.Streams;
+
+namespace Luo_Painter.Projects
+{
+    public static class RandomAccessStreamExtensions
+    {
+        public static async Task<SoftwareBitmap> ToSoftwareBitmap(this IRandomAccessStream stream)
+        {
+            // Create the decoder from the stream
+            BitmapDecoder decoder = await BitmapDecoder.CreateAsync(stream);
+
+            // Get the SoftwareBitmap representation of the file
+            return await decoder.GetSoftwareBitmapAsync(BitmapPixelFormat.Bgra8, BitmapAlphaMode.Premultiplied);
+        }
+        public static async Task<IRandomAccessStream> OpenAsync(this StorageFolder folder, string fileName)
+        {            
+            StorageFile file = await folder.CreateFileAsync(fileName, CreationCollisionOption.ReplaceExisting);
+
+            return await file.OpenAsync(FileAccessMode.ReadWrite);
+        }
+    }
+}
