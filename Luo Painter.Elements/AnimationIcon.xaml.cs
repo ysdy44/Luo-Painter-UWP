52ed3d0e77104e13235023c17891ce2621e55648
ysdy44 <ysdy44@outlook.com>
2023-01-10 02:00:03 +0800

    Add AnimationIcon

---
+++ Luo Painter.Elements/AnimationIcon.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace Luo_Painter.Elements
+{
+    public sealed partial class AnimationIcon : Grid
+    {
+        public AnimationIcon()
+        {
+            this.InitializeComponent();
+        }
+        public void Begin()=>this.SaveStoryboard.Begin(); // Storyboard
+    }
+}
