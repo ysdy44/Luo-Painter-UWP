6476d64dea5d90db5f7834959118fda761f6154e
ysdy44 <ysdy44@outlook.com>
2023-01-22 01:41:57 +0800

    Add Tiles Folder

--- Luo Painter.Brushes/InkExtensions.cs
+++ Luo Painter.Brushes/InkExtensions.cs
@@ -9,6 +9,9 @@ namespace Luo_Painter.Brushes
         public static string GetResource(this InkType type) => $"ms-appx:///Luo Painter.Brushes/Icons/{type}Icon.xaml";
         public static ControlTemplate GetTemplate(this InkType type, ResourceDictionary resource) => resource[$"{type}Icon"] as ControlTemplate;
 
+        public static string GetTile(this uint tile) => $"ms-appx:///Luo Painter.Brushes/Tiles/{tile}.jpg";
+        public const string DeaultTile = "ms-appx:///Luo Painter.Brushes/Tiles/0000000000.jpg";
+
         public static string GetThumbnail(this string path) => $"ms-appx:///Luo Painter.Brushes/Thumbnails/{path}/Thumbnail.png";
         public static string GetThumbnailRender(this string path) => $"ms-appx:///Luo Painter.Brushes/Thumbnails/{path}/Render.png";
 
