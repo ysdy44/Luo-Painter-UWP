e5868dd802492e312164065006dd9db5079534e8
ysdy44 <ysdy44@outlook.com>
2023-01-24 00:07:16 +0800

    ToolListView Remove SizeListView

--- Luo Painter/Controls/ToolListView.xaml.cs
+++ Luo Painter/Controls/ToolListView.xaml.cs
@@ -1,16 +1,8 @@
 ﻿using Luo_Painter.Options;
 using Windows.ApplicationModel.Resources;
-using Windows.UI.Xaml;
 
 namespace Luo_Painter.Controls
 {
-    internal sealed class ItemSize
-    {
-        public int Width { get; set; }
-        public int Height { get; set; }
-        public override string ToString() => $"{this.Width} × {this.Height}";
-    }
-
     public sealed partial class ToolListView : XamlGridView
     {
 
@@ -21,30 +13,10 @@ namespace Luo_Painter.Controls
             set => base.SelectedIndex = this.Collection.IndexOf(value);
         }
 
-        #region DependencyProperty
-
-        /// <summary> Gets or set the size for <see cref="ToolListView"/>'s item. </summary>
-        public int ItemSize
-        {
-            get => (int)base.GetValue(ItemSizeProperty);
-            set => base.SetValue(ItemSizeProperty, value);
-        }
-        /// <summary> Identifies the <see cref = "ToolListView.ItemSize" /> dependency property. </summary>
-        public static readonly DependencyProperty ItemSizeProperty = DependencyProperty.Register(nameof(ItemSize), typeof(int), typeof(ToolListView), new PropertyMetadata(70));
-
-        #endregion
-
         //@Construct
         public ToolListView()
         {
             this.InitializeComponent();
-            this.SizeListView.ItemClick += (s, e) =>
-            {
-                if (e.ClickedItem is ItemSize item)
-                {
-                    this.ItemSize = item.Width;
-                }
-            };
         }
 
         //@Strings
