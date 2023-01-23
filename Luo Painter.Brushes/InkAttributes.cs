674ecb2d39316f63658b91c30b17be9a46c96931
ysdy44 <ysdy44@outlook.com>
2023-01-23 17:35:03 +0800

    InkAttributes Add Tile

--- Luo Painter.Brushes/InkAttributes.cs
+++ Luo Painter.Brushes/InkAttributes.cs
@@ -21,7 +21,10 @@ namespace Luo_Painter.Brushes
         /// </summary>
         public InkType Type { get; set; } = InkType.General; /// <see cref="InkPresenter.GetType"/>
 
+        public uint Tile { get; set; } = 0000000000;
 
+
+        // Property
         public float Size { get; set; }
         public float Opacity { get; set; } /// <see cref="InkPresenter.GetType"/>
         public float Spacing { get; set; }
@@ -31,25 +34,29 @@ namespace Luo_Painter.Brushes
         public bool IgnoreFlowPressure { get; set; } = true;
 
 
+        // Shape
         public PenTipShape Tip { get; set; }
         public bool IsStroke { get; set; }
+
         public BrushEdgeHardness Hardness { get; set; }
 
+        public bool Rotate { get; set; }
+        public string Shape { get; set; }  /// <see cref="InkPresenter.GetType"/>
+        public bool RecolorShape { get; set; } = true;
 
+
+        // Grain
         public BlendEffectMode BlendMode { get; set; } = (BlendEffectMode)(-1); /// <see cref="InkPresenter.GetType"/>
+
+        public int Step { get; set; } = 1024;
+        public string Grain { get; set; }  /// <see cref="InkPresenter.GetType"/>
+        public bool RecolorGrain { get; set; } = true;
+
+
+        // Mix
         public float Mix { get; set; } /// <see cref="InkPresenter.GetType"/>
         public float Wet { get; set; } = 10;
         public float Persistence { get; set; }
-
-
-        public bool Rotate { get; set; }
-        public int Step { get; set; } = 1024;
-
-        public string Shape { get; set; }  /// <see cref="InkPresenter.GetType"/>
-        public string Grain { get; set; }  /// <see cref="InkPresenter.GetType"/>
-
-        public bool RecolorShape { get; set; } = true;
-        public bool RecolorGrain { get; set; } = true;
 
 
         public void CopyWith(InkAttributes attributes)
@@ -57,6 +64,7 @@ namespace Luo_Painter.Brushes
             this.Type = attributes.Type;
 
 
+            // Property
             this.Size = attributes.Size;
             this.Opacity = attributes.Opacity;
             this.Spacing = attributes.Spacing;
@@ -66,26 +74,29 @@ namespace Luo_Painter.Brushes
             this.IgnoreFlowPressure = attributes.IgnoreFlowPressure;
 
 
+            // Shape
             this.Tip = attributes.Tip;
             this.IsStroke = attributes.IsStroke;
 
+            this.Rotate = attributes.Rotate;
+            this.Shape = attributes.Shape;
+            this.RecolorShape = attributes.RecolorShape;
 
-            this.Mix = attributes.Mix;
-            this.Wet = attributes.Wet;
-            this.Persistence = attributes.Persistence;
-
-            this.BlendMode = attributes.BlendMode;
             this.Hardness = attributes.Hardness;
 
 
-            this.Rotate = attributes.Rotate;
+            // Grain
             this.Step = attributes.Step;
-
-            this.Shape = attributes.Shape;
             this.Grain = attributes.Grain;
-
-            this.RecolorShape = attributes.RecolorShape;
             this.RecolorGrain = attributes.RecolorGrain;
+
+            this.BlendMode = attributes.BlendMode;
+
+
+            // Mix
+            this.Mix = attributes.Mix;
+            this.Wet = attributes.Wet;
+            this.Persistence = attributes.Persistence;
         }
 
     }
--- Luo Painter/Controls/BrushGridView.xaml.cs
+++ Luo Painter/Controls/BrushGridView.xaml.cs
@@ -1,12 +1,15 @@
 ﻿using Luo_Painter.Brushes;
-using Luo_Painter.Options;
-using System;
+using System.Collections.Generic;
+using System.Linq;
 using Windows.ApplicationModel.Resources;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
 
 namespace Luo_Painter.Controls
 {
+    internal sealed class PaintBrushGroupingList : List<PaintBrushGrouping>, IList<PaintBrushGrouping> { }
+    internal class PaintBrushGrouping : List<PaintBrush>, IList<PaintBrush>, IGrouping<string, PaintBrush>
+    {
+        public string Key { set; get; }
+    }
     internal sealed class PaintBrush : InkAttributes
     {
         public double Size2 { get => base.Size; set => base.Size = (float)value; }
@@ -15,22 +18,12 @@ namespace Luo_Painter.Controls
         public double Spacing2 { get => base.Spacing; set => base.Spacing = (float)value; }
         public double Flow2 { get => base.Flow; set => base.Flow = (float)value; }
 
-        public string Path { get; set; } // = "Flash/00";
-        public string Render => this.Path.GetThumbnailRender();
-        public string Thumbnail => this.Path.GetThumbnail();
-
-        public string Title { get; set; }
-        public string Subtitle => ((int)this.Size).ToString();
-
-        public string ImageSource => this.Tile == default ? InkExtensions.DeaultTile : this.Tile.GetTile();
-        public int Tile2 { get => (int)this.Tile; set => this.Tile = (uint)value; }
-        public uint Tile { get; set; } // = 0000000000;
+        public string ImageSource => base.Tile == default ? InkExtensions.DeaultTile : base.Tile.GetTile();
+        public int Tile2 { get => (int)base.Tile; set => base.Tile = (uint)value; }
     }
 
     public sealed partial class BrushGridView : XamlGridView
     {
-        //@Delegate
-        public event RoutedEventHandler Add { remove => this.AddButton.Click -= value; add => this.AddButton.Click += value; }
 
         //@Construct
         public BrushGridView()
