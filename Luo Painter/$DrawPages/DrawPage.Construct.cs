420cabaec45d9154248efdfb752e32bdcbca22c6
ysdy44 <ysdy44@outlook.com>
2023-01-26 20:43:32 +0800

    DrawPage Add WheelImageSource and BrushPage

--- Luo Painter/$DrawPages/DrawPage.Construct.cs
+++ Luo Painter/$DrawPages/DrawPage.Construct.cs
@@ -1,23 +1,31 @@
 ﻿using Luo_Painter.Blends;
 using Luo_Painter.Brushes;
 using Luo_Painter.Elements;
+using Luo_Painter.HSVColorPickers;
 using Luo_Painter.Layers;
 using Luo_Painter.Options;
 using System;
-using System.Threading.Tasks;
+using Windows.ApplicationModel.Core;
 using Windows.ApplicationModel.Resources;
+using Windows.Graphics.Display;
 using Windows.Storage;
 using Windows.Storage.Pickers;
+using Windows.UI.Core;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
 
 namespace Luo_Painter
 {
     public sealed partial class DrawPage
     {
 
-        //@Content
-        public bool PasteIsEnabled { get; set; }
+        WheelImageSource WheelImageSource;
+
+        int BrushPageId;
+
+        bool PasteIsEnabled;
 
         //@Strings
         public void ConstructStrings(ResourceLoader resource)
@@ -26,11 +34,51 @@ namespace Luo_Painter
 
         private void ConstructDraw()
         {
+            float dpi = DisplayInformation.GetForCurrentView().LogicalDpi;
+            this.WheelImageSource = new WheelImageSource(this.CanvasDevice, new CircleTemplateSettingsF(300), dpi);
+
+            base.Unloaded += (s, e) => CompositionTarget.SurfaceContentsLost -= this.SurfaceContentsLost;
+            base.Loaded += (s, e) => CompositionTarget.SurfaceContentsLost += this.SurfaceContentsLost;
+
+
             this.EditMenuFlyout.Opened += (s, e) =>
             {
                 this.PasteItem.GoToState(this.PasteIsEnabled);
             };
         }
+
+        private void SurfaceContentsLost(object sender, object e)
+        {
+            this.WheelImageSource.Redraw();
+        }
+
+
+        private async void TryShowBrushPage()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                if (this.BrushPageId == default)
+                {
+                    await CoreApplication.CreateNewView().Dispatcher.RunAsync(CoreDispatcherPriority.Normal, this.CreateBrushPage);
+                }
+                else
+                {
+                    if (await ApplicationViewSwitcher.TryShowAsStandaloneAsync(this.BrushPageId)) return;
+                }
+            }
+        }
+        private void CreateBrushPage()
+        {
+            Frame frame = new Frame();
+            IInkParameter parameter = this;
+            frame.Navigate(typeof(BrushPage), parameter);
+
+            Window.Current.Content = frame;
+            Window.Current.Activate();
+
+            this.BrushPageId = Windows.UI.ViewManagement.ApplicationView.GetForCurrentView().Id;
+        }
+
 
         public void RaiseHistoryCanExecuteChanged()
         {
