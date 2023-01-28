a2e7c4e0afd46a2d994f501dc237060ad8229ec8
ysdy44 <ysdy44@outlook.com>
2023-01-29 00:10:32 +0800

    File Rename: InkPresenter.Paint -> InkPresenter.Draw

---
+++ Luo Painter.Brushes/InkPresenter.Shape.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Graphics.Canvas;
+
+namespace Luo_Painter.Brushes
+{
+    public sealed partial class InkPresenter
+    {
+        public CanvasBitmap GrainSource { get; private set; }
+
+        public void ConstructShape(string path, CanvasBitmap source)
+        {
+            this.Shape = path;
+            this.ShapeSource?.Dispose();
+            this.ShapeSource = source;
+        }
+        public void ClearShape()
+        {
+            this.Shape = null;
+            this.ShapeSource?.Dispose();
+            this.ShapeSource = null;
+        }
+
+        public bool AllowShape // GetType
+        {
+            get
+            {
+                if (this.Shape is null) return false;
+                if (this.ShapeSource is null) return false;
+                return true;
+            }
+        }
+    }
+}
