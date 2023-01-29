2a996acd97f7078e385d24036bfb208a731625c3
ysdy44 <ysdy44@outlook.com>
2023-01-30 01:11:02 +0800

    Add BrushEasePressure

---
+++ Luo Painter.Brushes/BrushEasePressure.cs
@@ -0,0 +1,35 @@
+﻿namespace Luo_Painter.Brushes
+{
+    /// <summary>
+    /// Pressure of the brush easing.
+    /// </summary>
+    public enum BrushEasePressure : byte
+    {
+        /// <summary> f (x) = 1 </summary>
+        None,
+
+        /// <summary> f (x) = x </summary>
+        Linear,
+        /// <summary> f (x) = 1 - x </summary>
+        LinearFlip,
+
+        /// <summary> f (x) = x * x </summary>
+        Quadratic,
+        /// <summary> f (x) = 1 - x * x </summary>
+        QuadraticFlip,
+        /// <summary> f (x) = (1 - x) * (1 - x) </summary>
+        QuadraticReverse,
+        /// <summary> f (x) = 1 - (1 - x) * (1 - x) </summary>
+        QuadraticFlipReverse,
+
+        /// <summary> f (x) = (x / 2 - 1) * (x / 2 - 1) </summary>
+        Mirror,
+        /// <summary> f (x) = 1 - (x / 2 - 1) * (x / 2 - 1) </summary>
+        MirrorFlip,
+
+        /// <summary> 2f (x) = 2x * 2x </summary>
+        Symmetry,
+        /// <summary> 2f (x) = 1 - 2x * 2x </summary>
+        SymmetryFlip,
+    }
+}
