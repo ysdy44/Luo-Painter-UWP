e167fddff8fe492a86ffa6d5d52816e881297969
ysdy44 <ysdy44@outlook.com>
2023-02-02 03:08:55 +0800

    Add DrawPage.Effect.Other.HSB

---
+++ Luo Painter/$DrawPage.Option/DrawPage.Effect.Other.HSB.cs
@@ -0,0 +1,47 @@
+﻿using Luo_Painter.Brushes;
+using Luo_Painter.HSVColorPickers;
+using Luo_Painter.Layers;
+using Microsoft.Graphics.Canvas.Effects;
+using System.Numerics;
+using Windows.UI;
+using Windows.UI.Xaml.Controls;
+
+namespace Luo_Painter
+{
+    public sealed partial class DrawPage
+    {
+        float HSBHue = 0;
+        float HSBSaturation = 1;
+        float HSBBrightness = 0;
+
+        Matrix5x4 HSBMatrix = Matrix5x4Extension.Identity;
+
+        public void ConstructHSB()
+        {
+            this.HSBHueSlider.Click += (s, e) => this.NumberShowAt(this.HSBHueSlider, NumberPickerMode.Case0);
+            this.HSBHueSlider.ValueChanged += (s, e) =>
+            {
+                this.HSBHue = (float)(e.NewValue / 120d);
+                this.HSBMatrix = Matrix5x4Extension.HSB(this.HSBHue, this.HSBSaturation, this.HSBBrightness);
+                this.CanvasVirtualControl.Invalidate(); // Invalidate
+            };
+
+            this.HSBSaturationSlider.Click += (s, e) => this.NumberShowAt(this.HSBSaturationSlider, NumberPickerMode.Case1);
+            this.HSBSaturationSlider.ValueChanged += (s, e) =>
+            {
+                this.HSBSaturation = (float)(e.NewValue / 100d);
+                this.HSBMatrix = Matrix5x4Extension.HSB(this.HSBHue, this.HSBSaturation, this.HSBBrightness);
+                this.CanvasVirtualControl.Invalidate(); // Invalidate
+            };
+
+            this.HSBBrightnessSlider.Click += (s, e) => this.NumberShowAt(this.HSBBrightnessSlider, NumberPickerMode.Case2);
+            this.HSBBrightnessSlider.ValueChanged += (s, e) =>
+            {
+                this.HSBBrightness = (float)(e.NewValue / 100d);
+                this.HSBMatrix = Matrix5x4Extension.HSB(this.HSBHue, this.HSBSaturation, this.HSBBrightness);
+                this.CanvasVirtualControl.Invalidate(); // Invalidate
+            };
+        }
+
+    }
+}
