d5486741a20980cf8c00e3373316c2d3cf7dc4b6
ysdy44 <ysdy44@outlook.com>
2023-02-04 15:14:56 +0800

    InkAttributes Add BrushEasePressure

--- Luo Painter.Brushes/InkAttributes.Export.cs
+++ Luo Painter.Brushes/InkAttributes.Export.cs
@@ -1,121 +1,195 @@
 ﻿using Microsoft.Graphics.Canvas.Effects;
-using System;
+using System.Runtime.CompilerServices;
 using System.Xml.Linq;
+using Windows.UI.Input.Inking;
 
 namespace Luo_Painter.Brushes
 {
     public partial class InkAttributes
     {
 
-        public XElement Save()
+        public XElement Save() => new XElement("Brush",
+            new XAttribute("Type", this.Type),
+
+            new XAttribute("Tile", this.Tile),
+
+            // Property
+            new XAttribute("Size", this.Size),
+            new XAttribute("Opacity", this.Opacity),
+            new XAttribute("Spacing", this.Spacing),
+            new XAttribute("Flow", this.Flow),
+
+            new XAttribute("MinSize", this.MinSize),
+            new XAttribute("MinFlow", this.MinFlow),
+
+            new XAttribute("SizePressure", this.SizePressure),
+            new XAttribute("FlowPressure", this.FlowPressure),
+
+            new XAttribute("IgnoreSizePressure", this.IgnoreSizePressure),
+            new XAttribute("IgnoreFlowPressure", this.IgnoreFlowPressure),
+
+            new XAttribute("Tip", this.Tip),
+            new XAttribute("IsStroke", this.IsStroke),
+
+            new XAttribute("Hardness", this.Hardness),
+
+            // Texture
+            new XAttribute("Rotate", this.Rotate),
+            new XAttribute("RecolorShape", this.RecolorShape),
+            new XAttribute("Shape", this.SaveTexture(this.Shape)),
+
+            new XAttribute("Step", this.Step),
+            new XAttribute("RecolorGrain", this.RecolorGrain),
+            new XAttribute("Grain", this.SaveTexture(this.Grain)),
+
+            new XAttribute("BlendMode", this.SaveBlendMode(this.BlendMode)),
+
+            // Mix
+            new XAttribute("Mix", this.Mix),
+            new XAttribute("Wet", this.Wet),
+            new XAttribute("Persistence", this.Persistence)
+        );
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private string SaveBlendMode(BlendEffectMode value)
         {
-            XElement element = new XElement("Brush",
-                new XAttribute("Type", this.Type),
-
-
-                new XAttribute("Size", this.Size),
-                new XAttribute("Opacity", this.Opacity),
-                new XAttribute("Spacing", this.Spacing),
-                new XAttribute("Flow", this.Flow),
-
-                new XAttribute("IgnoreSizePressure", this.IgnoreSizePressure),
-                new XAttribute("IgnoreFlowPressure", this.IgnoreFlowPressure),
-
-
-                new XAttribute("Tip", this.Tip),
-                new XAttribute("IsStroke", this.IsStroke),
-                new XAttribute("Hardness", this.Hardness),
-
-
-                new XAttribute("BlendMode", this.BlendMode),
-                new XAttribute("Mix", this.Mix),
-                new XAttribute("Wet", this.Wet),
-                new XAttribute("Persistence", this.Persistence),
-
-
-                new XAttribute("Rotate", this.Rotate),
-                new XAttribute("Step", this.Step),
-
-                new XAttribute("RecolorShape", this.RecolorShape),
-                new XAttribute("RecolorGrain", this.RecolorGrain));
-
-            if (string.IsNullOrEmpty(this.Shape) is false) element.Add(new XAttribute("Shape", this.Shape));
-            if (string.IsNullOrEmpty(this.Grain) is false) element.Add(new XAttribute("Grain", this.Grain));
-
-            return element;
+            if (System.Enum.IsDefined(typeof(BlendEffectMode), value))
+                return this.BlendMode.ToString();
+            else
+                return "None";
         }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private string SaveTexture(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+                return string.Empty;
+            else
+                return value;
+        }
+
 
         public void Load(XElement element)
         {
-            if (element.Attribute("Type") is XAttribute type)
-            {
-                switch (type.Value)
-                {
-                    case "General": this.Type = InkType.General; break;
-                    case "Tip": this.Type = InkType.Tip; break;
-                    case "Line": this.Type = InkType.Line; break;
-                    case "Blur": this.Type = InkType.Blur; break;
-                    case "Mosaic": this.Type = InkType.Mosaic; break;
-                    case "Erase": this.Type = InkType.Erase; break;
-                    case "Liquefy": this.Type = InkType.Liquefy; break;
-                    default: this.Type = InkType.General; break;
-                }
-            }
-            else this.Type = InkType.General;
+            if (element.Attribute("Type") is XAttribute type) this.Type = this.LoadType(type.Value);
 
-
+            // Property
             if (element.Attribute("Size") is XAttribute size) this.Size = (float)size;
             if (element.Attribute("Opacity") is XAttribute opacity) this.Opacity = (float)opacity;
             if (element.Attribute("Spacing") is XAttribute spacing) this.Spacing = (float)spacing;
             if (element.Attribute("Flow") is XAttribute flow) this.Flow = (float)flow;
 
+            if (element.Attribute("MinSize") is XAttribute minSize) this.MinSize = (float)minSize;
+            if (element.Attribute("MinFlow") is XAttribute minFlow) this.MinFlow = (float)minFlow;
+
+            if (element.Attribute("SizePressure") is XAttribute sizePressure) this.SizePressure = this.LoadPressure(sizePressure.Value);
+            if (element.Attribute("FlowPressure") is XAttribute flowPressure) this.FlowPressure = this.LoadPressure(flowPressure.Value);
+
             if (element.Attribute("IgnoreSizePressure") is XAttribute ignoreSizePressure) this.IgnoreSizePressure = (bool)ignoreSizePressure;
             if (element.Attribute("IgnoreFlowPressure") is XAttribute ignoreFlowPressure) this.IgnoreFlowPressure = (bool)ignoreFlowPressure;
 
-
-            if (element.Attribute("Tip") is XAttribute tip)
-            {
-                switch (tip.Value)
-                {
-                    case "Circle": this.Tip = Windows.UI.Input.Inking.PenTipShape.Circle; break;
-                    case "Rectangle": this.Tip = Windows.UI.Input.Inking.PenTipShape.Rectangle; break;
-                    default: this.Tip = default; break;
-                }
-            }
+            if (element.Attribute("Tip") is XAttribute tip) this.Tip = this.LoadTip(tip.Value);
             if (element.Attribute("IsStroke") is XAttribute isStroke) this.IsStroke = (bool)isStroke;
 
-            if (element.Attribute("Hardness") is XAttribute hardness)
-            {
-                switch (hardness.Value)
-                {
-                    case "None": this.Hardness = BrushEdgeHardness.None; break;
-                    case "Cosine": this.Hardness = BrushEdgeHardness.Cosine; break;
-                    case "Quadratic": this.Hardness = BrushEdgeHardness.Quadratic; break;
-                    case "Cube": this.Hardness = BrushEdgeHardness.Cube; break;
-                    case "Quartic": this.Hardness = BrushEdgeHardness.Quartic; break;
-                    default: this.Hardness = BrushEdgeHardness.None; break;
-                }
-            }
+            if (element.Attribute("Hardness") is XAttribute hardness) this.Hardness = this.LoadHardness(hardness.Value);
 
+            // Texture
+            if (element.Attribute("Rotate") is XAttribute rotate) this.Rotate = (bool)rotate;
+            if (element.Attribute("RecolorShape") is XAttribute recolorShape) this.RecolorShape = (bool)recolorShape;
+            if (element.Attribute("Shape") is XAttribute shape) this.Shape = shape.Value;
 
-            if (element.Attribute("BlendMode") is XAttribute blendMode)
-                if (string.IsNullOrEmpty(blendMode.Value))
-                    if (blendMode.Value is "None" is false)
-                        this.BlendMode = (BlendEffectMode)Enum.Parse(typeof(BlendEffectMode), blendMode.Value);
+            if (element.Attribute("Step") is XAttribute step) this.Step = (int)step;
+            if (element.Attribute("RecolorGrain") is XAttribute recolorGrain) this.RecolorGrain = (bool)recolorGrain;
+            if (element.Attribute("Grain") is XAttribute grain) this.Grain = grain.Value;
+         
+            if (element.Attribute("BlendMode") is XAttribute blendMode) this.BlendMode = this.LoadBlendMode(blendMode.Value);
 
+            // Mix
             if (element.Attribute("Mix") is XAttribute mix) this.Mix = (float)mix;
             if (element.Attribute("Wet") is XAttribute wet) this.Wet = (float)wet;
             if (element.Attribute("Persistence") is XAttribute persistence) this.Persistence = (float)persistence;
+        }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private InkType LoadType(string value)
+        {
+            switch (value)
+            {
+                case "General": return InkType.General;
+                case "Tip": return InkType.Tip;
+                case "Line": return InkType.Line;
+                case "Blur": return InkType.Blur;
+                case "Mosaic": return InkType.Mosaic;
+                case "Erase": return InkType.Erase;
+                case "Liquefy": return InkType.Liquefy;
+                default: return InkType.General;
+            }
+        }
 
-            if (element.Attribute("Rotate") is XAttribute rotate) this.Rotate = (bool)rotate;
-            if (element.Attribute("Step") is XAttribute step) this.Step = (int)step;
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private BrushEasePressure LoadPressure(string value)
+        {
+            switch (value)
+            {
+                case "None": return BrushEasePressure.None;
 
-            if (element.Attribute("Shape") is XAttribute shape) this.Shape = shape.Value;
-            if (element.Attribute("Grain") is XAttribute grain) this.Grain = grain.Value;
+                case "Linear": return BrushEasePressure.Linear;
+                case "LinearFlip": return BrushEasePressure.LinearFlip;
 
-            if (element.Attribute("RecolorShape") is XAttribute recolorShape) this.RecolorShape = (bool)recolorShape;
-            if (element.Attribute("RecolorGrain") is XAttribute recolorGrain) this.RecolorGrain = (bool)recolorGrain;
+                case "Quadratic": return BrushEasePressure.Quadratic;
+                case "QuadraticFlip": return BrushEasePressure.QuadraticFlip;
+                case "QuadraticReverse": return BrushEasePressure.QuadraticReverse;
+                case "QuadraticFlipReverse": return BrushEasePressure.QuadraticFlipReverse;
+
+                case "Mirror": return BrushEasePressure.Mirror;
+                case "MirrorFlip": return BrushEasePressure.MirrorFlip;
+
+                case "Symmetry": return BrushEasePressure.Symmetry;
+                case "SymmetryFlip": return BrushEasePressure.SymmetryFlip;
+
+                default: return BrushEasePressure.None;
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private PenTipShape LoadTip(string value)
+        {
+            switch (value)
+            {
+                case "Circle": return PenTipShape.Circle;
+                case "Rectangle": return PenTipShape.Rectangle;
+                default: return PenTipShape.Circle;
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private BrushEdgeHardness LoadHardness(string value)
+        {
+            switch (value)
+            {
+                case "None": return BrushEdgeHardness.None;
+                case "Cosine": return BrushEdgeHardness.Cosine;
+                case "Quadratic": return BrushEdgeHardness.Quadratic;
+                case "Cube": return BrushEdgeHardness.Cube;
+                case "Quartic": return BrushEdgeHardness.Quartic;
+                default: return BrushEdgeHardness.None;
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private BlendEffectMode LoadBlendMode(string value)
+        {
+            if (string.IsNullOrEmpty(value)) return (BlendEffectMode)(-1);
+            if (value is "None" is false) return (BlendEffectMode)(-1);
+
+            if (System.Enum.TryParse(typeof(BlendEffectMode), value, out object obj))
+            {
+                if (obj is BlendEffectMode blendMode)
+                {
+                    return blendMode;
+                }
+            }
+            return (BlendEffectMode)(-1);
         }
 
     }
--- Luo Painter.Brushes/InkAttributes.cs
+++ Luo Painter.Brushes/InkAttributes.cs
@@ -23,46 +23,45 @@ namespace Luo_Painter.Brushes
 
         public uint Tile { get; set; } = 0000000000;
 
-
         // Property
         public float Size { get; set; }
         public float Opacity { get; set; } /// <see cref="InkPresenter.GetType"/>
         public float Spacing { get; set; }
         public float Flow { get; set; } /// <see cref="InkPresenter.GetType"/>
 
+        public float MinSize { get; set; }
+        public float MinFlow { get; set; }
+
+        public BrushEasePressure SizePressure { get; set; }
+        public BrushEasePressure FlowPressure { get; set; }
+
         public bool IgnoreSizePressure { get; set; }
         public bool IgnoreFlowPressure { get; set; } = true;
 
-
-        // Shape
         public PenTipShape Tip { get; set; }
         public bool IsStroke { get; set; }
 
         public BrushEdgeHardness Hardness { get; set; }
 
+        // Texture
         public bool Rotate { get; set; }
         public string Shape { get; set; }  /// <see cref="InkPresenter.GetType"/>
         public bool RecolorShape { get; set; } = true;
-
-
-        // Grain
-        public BlendEffectMode BlendMode { get; set; } = (BlendEffectMode)(-1); /// <see cref="InkPresenter.GetType"/>
 
         public int Step { get; set; } = 1024;
         public string Grain { get; set; }  /// <see cref="InkPresenter.GetType"/>
         public bool RecolorGrain { get; set; } = true;
 
+        public BlendEffectMode BlendMode { get; set; } = (BlendEffectMode)(-1); /// <see cref="InkPresenter.GetType"/>
 
         // Mix
         public float Mix { get; set; } /// <see cref="InkPresenter.GetType"/>
         public float Wet { get; set; } = 10;
         public float Persistence { get; set; }
 
-
         public void CopyWith(InkAttributes attributes)
         {
             this.Type = attributes.Type;
-
 
             // Property
             this.Size = attributes.Size;
@@ -70,28 +69,30 @@ namespace Luo_Painter.Brushes
             this.Spacing = attributes.Spacing;
             this.Flow = attributes.Flow;
 
+            this.MinSize = attributes.MinSize;
+            this.MinFlow = attributes.MinFlow;
+
+            this.SizePressure = attributes.SizePressure;
+            this.FlowPressure = attributes.FlowPressure;
+
             this.IgnoreSizePressure = attributes.IgnoreSizePressure;
             this.IgnoreFlowPressure = attributes.IgnoreFlowPressure;
 
-
-            // Shape
             this.Tip = attributes.Tip;
             this.IsStroke = attributes.IsStroke;
 
+            this.Hardness = attributes.Hardness;
+
+            // Texture
             this.Rotate = attributes.Rotate;
             this.Shape = attributes.Shape;
             this.RecolorShape = attributes.RecolorShape;
 
-            this.Hardness = attributes.Hardness;
-
-
-            // Grain
             this.Step = attributes.Step;
             this.Grain = attributes.Grain;
             this.RecolorGrain = attributes.RecolorGrain;
 
             this.BlendMode = attributes.BlendMode;
-
 
             // Mix
             this.Mix = attributes.Mix;
