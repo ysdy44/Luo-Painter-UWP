10aa26efba369a0fdcf859142f437425439ad017
ysdy44 <ysdy44@outlook.com>
2023-02-05 01:51:21 +0800

    Add ActionType

---
+++ Luo Painter/$MainPages/ActionType.cs
@@ -0,0 +1,38 @@
+﻿namespace Luo_Painter
+{
+    internal enum ActionType : int
+    {
+        /// <summary> Open Project </summary>
+        File,
+        /// <summary> Open Folder </summary>
+        Folder,
+
+        /// <summary> Add Empty Project </summary>
+        Add,
+        /// <summary> Add Image Project </summary>
+        Image,
+
+        DupliateShow,
+        DupliateHide,
+        DeleteShow,
+        DeleteHide,
+        SelectShow,
+        SelectHide,
+        SelectToMove,
+        MoveHide,
+
+        /// <summary> Dupliate Project </summary>
+        Dupliate,
+        /// <summary> Delete Project or Folder </summary>
+        Delete,
+        /// <summary> Move Project </summary>
+        Move,
+
+        /// <summary> New Folder </summary>
+        New,
+        /// <summary> Rename Project </summary>
+        Rename,
+        /// <summary> Open Local Folder </summary>
+        Local,
+    }
+}
