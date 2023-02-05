c69b0e3a6543d259750fce1dd3315f628991c98c
ysdy44 <ysdy44@outlook.com>
2023-02-05 23:42:26 +0800

    Add DrawPage.Pattern

---
+++ Luo Painter/$DrawPage.Option/DrawPage.Pattern.cs
@@ -0,0 +1,45 @@
+﻿using FanKit.Transformers;
+using Luo_Painter.Blends;
+using Luo_Painter.Layers;
+using Luo_Painter.Layers.Models;
+using Luo_Painter.Options;
+using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.Geometry;
+using Windows.UI;
+
+namespace Luo_Painter
+{
+    public sealed partial class DrawPage
+    {
+
+        public void ConstructPattern()
+        {
+        }
+
+        private void PatternInvalidate()
+        {
+        }
+
+        private void CancelPatternTransform()
+        {
+        }
+        private void PrimaryPatternTransform()
+        {
+        }
+
+
+        private void PatternTransform_Delta()
+        {
+        }
+
+
+        private void Pattern_Start()
+        {
+        }
+
+        public void DrawPattern(CanvasDrawingSession ds, OptionType type, ITransformerLTRB transformerLTRB, Color color)
+        {
+        }
+
+    }
+}
