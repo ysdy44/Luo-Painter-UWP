0b1a10479cc19e4721aef4e9390771510f504e9b
ysdy44 <ysdy44@outlook.com>
2023-02-19 02:06:42 +0800

    ExtendDialog 2.0.0

--- Luo Painter/Controls/ExtendDialog.xaml.cs
+++ Luo Painter/Controls/ExtendDialog.xaml.cs
@@ -8,17 +8,33 @@ namespace Luo_Painter.Controls
     {
         //@Content
         public System.Drawing.Size Size => this.SizePicker.Size;
-        public IndicatorMode Indicator => this.IndicatorControl.Mode;
+        public IndicatorMode Indicator
+        {
+            get
+            {
+                switch (this.IndicatorPanel.Index)
+                {
+                    case -4: return IndicatorMode.LeftTop;
+                    case -3: return IndicatorMode.Top;
+                    case -2: return IndicatorMode.RightTop;
+
+                    case -1: return IndicatorMode.Left;
+                    case 0: return IndicatorMode.Center;
+                    case 1: return IndicatorMode.Right;
+
+                    case 2: return IndicatorMode.LeftBottom;
+                    case 3: return IndicatorMode.Bottom;
+                    case 4: return IndicatorMode.RightBottom;
+
+                    default: return IndicatorMode.None;
+                }
+            }
+        }
 
         //@Construct
         public ExtendDialog()
         {
             this.InitializeComponent();
-            this.ModeRun.Text = this.IndicatorControl.Mode.ToString();
-            this.IndicatorControl.ModeChanged += (sender, mode) =>
-            {
-                this.ModeRun.Text = mode.ToString();
-            };
         }
 
         public void Resezing(int width, int height)
