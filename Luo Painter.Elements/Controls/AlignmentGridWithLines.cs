8cb0cdd84160af0a6fe02177e98f3db1be140970
ysdy44 <ysdy44@outlook.com>
2023-02-19 19:49:53 +0800

    File Split AlignmentGrid

---
+++ Luo Painter.Elements/Controls/AlignmentGridWithLines.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using Windows.UI.Xaml.Shapes;
+
+namespace Luo_Painter.Elements
+{
+    /// <summary>
+    /// AlignmentGrid with Lines.
+    /// </summary>
+    public sealed class AlignmentGridWithLines : AlignmentGrid
+    {
+        protected override IEnumerable<Line> Lines(int width, int height)
+        {
+            for (int x = 0; x < base.Column; x++)
+            {
+                int left = x * AlignmentGrid.Step;
+                yield return new Line
+                {
+                    X1 = left,
+                    X2 = left,
+                    Y1 = 0,
+                    Y2 = height,
+                    StrokeThickness = 1,
+                    Stroke = base.LineBrush
+                };
+            }
+
+            for (int y = 0; y < base.Row; y++)
+            {
+                int top = y * AlignmentGrid.Step;
+                yield return new Line
+                {
+                    X1 = 0,
+                    X2 = width,
+                    Y1 = top,
+                    Y2 = top,
+                    StrokeThickness = 1,
+                    Stroke = this.LineBrush
+                };
+            }
+        }
+    }
+}
---
+++ Luo Painter.Elements/Controls/AlignmentGridWithSpot.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Shapes;
+
+namespace Luo_Painter.Elements
+{
+    /// <summary>
+    /// AlignmentGrid with Spot.
+    /// </summary>
+    public sealed class AlignmentGridWithSpot : AlignmentGrid
+    {
+        protected override IEnumerable<Line> Lines(int width, int height)
+        {
+            for (int y = 1; y < base.Row; y++)
+            {
+                int top = y * AlignmentGrid.Step;
+                yield return new Line
+                {
+                    X1 = 0,
+                    Y1 = top,
+                    X2 = width,
+                    Y2 = top,
+                    Stroke = this.LineBrush,
+                    StrokeStartLineCap = PenLineCap.Flat,
+                    StrokeEndLineCap = PenLineCap.Flat,
+                    StrokeDashCap = PenLineCap.Flat,
+                    StrokeThickness = 3,
+                    StrokeDashArray =
+                    {
+                        1,
+                        AlignmentGrid.Step / 3 - 1,
+                    }
+                };
+            }
+        }
+    }
+}
