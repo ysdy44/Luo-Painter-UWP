2b6cb11b63d5ceee207cfa09547895921a761252
ysdy44 <ysdy44@outlook.com>
2023-02-24 03:15:58 +0800

    Add Icons

---
+++ Luo Painter/Strings/Ico.cs
@@ -0,0 +1,105 @@
+﻿using Luo_Painter.Blends;
+using Luo_Painter.Brushes;
+using Luo_Painter.Elements;
+using Luo_Painter.Models;
+using Luo_Painter.Options;
+using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.Effects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Windows.Graphics.Imaging;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Markup;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace Luo_Painter
+{
+    internal sealed class BlendList : List<BlendEffectMode> { }
+    internal sealed class BlendGroupingList : List<BlendEffectMode> { }
+    internal class BlendItem : TItem<BlendEffectMode>
+    {
+        protected override void OnTypeChanged(BlendEffectMode value)
+        {
+            base.TextBlock.Text = App.Resource.GetString($"Blends_{value.GetTitle()}");
+            base.Icon.Content = value.GetIcon();
+        }
+    }
+
+    internal sealed class HardnessList : List<BrushEdgeHardness> { }
+    internal sealed class HardnessIcon : TIcon<BrushEdgeHardness>
+    {
+        protected override void OnTypeChanged(BrushEdgeHardness value)
+        {
+            base.Content = App.Resource.GetString($"Hardness_{value}");
+        }
+    }
+
+    internal sealed class InterpolationItem : TItem<CanvasImageInterpolation>
+    {
+        protected override void OnTypeChanged(CanvasImageInterpolation value)
+        {
+            base.TextBlock.Text = App.Resource.GetString($"Interpolation_{value}");
+            base.Icon.Content = value.ToString().First().ToString();
+        }
+    }
+
+    internal sealed class OptionGroupingList : GroupingList<OptionGrouping, OptionType, OptionType> { }
+    internal class OptionGrouping : Grouping<OptionType, OptionType> { }
+
+    internal class OptionIcon : TIcon<OptionType>
+    {
+        protected override void OnTypeChanged(OptionType value)
+        {
+            base.Content = value;
+            base.Resources.Source = new Uri(value.GetResource());
+            base.Template = value.GetTemplate(base.Resources);
+        }
+    }
+
+    internal sealed class OptionThumbnail : TIcon<OptionType>
+    {
+        protected override void OnTypeChanged(OptionType value)
+        {
+            base.Content = App.Resource.GetString(value.ToString());
+
+            // https://docs.microsoft.com/en-us/windows/uwp/debug-test-perf/optimize-animations-and-media
+            if (value.ExistThumbnail())
+            {
+                BitmapImage bitmap = new BitmapImage();
+                base.Background = new ImageBrush
+                {
+                    ImageSource = bitmap
+                };
+                bitmap.UriSource = new Uri(value.GetThumbnail());
+            }
+        }
+    }
+
+    [ContentProperty(Name = nameof(Content))]
+    internal class OptionCase : DependencyObject, ICase<OptionType>
+    {
+        public object Content
+        {
+            get => (object)base.GetValue(ContentProperty);
+            set => base.SetValue(ContentProperty, value);
+        }
+        /// <summary> Identifies the <see cref="Content"/> property. </summary>
+        public static readonly DependencyProperty ContentProperty = DependencyProperty.Register(nameof(Content), typeof(object), typeof(OptionCase), new PropertyMetadata(null));
+
+        public OptionType Value
+        {
+            get => (OptionType)base.GetValue(ValueProperty);
+            set => base.SetValue(ValueProperty, value);
+        }
+        /// <summary> Identifies the <see cref="Value"/> property. </summary>
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register(nameof(Value), typeof(OptionType), typeof(OptionCase), new PropertyMetadata(default(OptionType)));
+
+        public void OnNavigatedTo() { }
+
+        public void OnNavigatedFrom() { }
+    }
+    [ContentProperty(Name = nameof(SwitchCases))]
+    internal sealed class OptionSwitchPresenter : SwitchPresenter<OptionType> { }
+}
