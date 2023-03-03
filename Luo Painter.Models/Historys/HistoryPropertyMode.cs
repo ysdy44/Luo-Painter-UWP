8375c00f7f92fcdab6d3f269361df81625b24dcc
ysdy44 <ysdy44@outlook.com>
2023-03-04 01:31:50 +0800

    Add HistoryPropertyMode

---
+++ Luo Painter.Models/Historys/HistoryPropertyMode.cs
@@ -0,0 +1,31 @@
+﻿using Luo_Painter.Models.Historys;
+using System.Collections.Generic;
+using Windows.Storage.Streams;
+
+namespace Luo_Painter.Models
+{
+    /// <summary>
+    /// Mode of <see cref="PropertyHistory"/>
+    /// </summary>
+    public enum HistoryPropertyMode
+    {
+        /// <summary> Normal </summary>
+        None,
+
+        /// <summary> <see cref="string"/> </summary>
+        Name,
+        /// <summary> <see cref="double"/> </summary>
+        Opacity,
+        /// <summary> <see cref="Microsoft.Graphics.Canvas.Effects.BlendEffectMode"/> </summary>
+        BlendMode,
+        /// <summary> <see cref="Windows.UI.Xaml.Visibility"/> </summary>
+        Visibility,
+
+        /// <summary> <see cref="IDictionary{int, IBuffer}"/> </summary>
+        Bitmap,
+        /// <summary> <see cref="IBuffer"/> to <see cref="Windows.UI.Color"/> </summary>
+        BitmapClear,
+        /// <summary> <see cref="IBuffer"/> </summary>
+        BitmapReset,
+    }
+}
