5b210316c784753c20013953a9522c9f3c4baac6
ysdy44 <ysdy44@outlook.com>
2023-03-04 13:35:33 +0800

    Add HistogramDialog

---
+++ Luo Painter/Controls/HistogramDialog.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Luo_Painter.Models;
+using Luo_Painter.Options;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Markup;
+
+namespace Luo_Painter.Controls
+{
+    [ContentProperty(Name = nameof(Child))]
+    public sealed partial class HistogramDialog : ContentDialog
+    {
+        //@String
+        private string Back => App.Resource.GetString(UIType.Back.ToString());
+        private string Histogram => App.Resource.GetString(OptionType.HistogramMenu.ToString());
+        
+        //@Content
+        public UIElement Child { get => this.ChildBorder.Child; set => this.ChildBorder.Child = value; }
+
+        //@Construct
+        public HistogramDialog()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
