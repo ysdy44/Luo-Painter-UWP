9dab27b2a72ea61cee0e5fd09b28e5290acada8f
ysdy44 <ysdy44@outlook.com>
2023-03-05 21:08:28 +0800

    File Move: Brushes Images without Folder

--- Luo Painter.Brushes/BrushExtensions.cs
+++ Luo Painter.Brushes/BrushExtensions.cs
@@ -5,10 +5,10 @@
         public static string GetTile(this uint tile) => $"ms-appx:///Luo Painter.Brushes/Tiles/{tile}.jpg";
         public const string DeaultTile = "ms-appx:///Luo Painter.Brushes/Tiles/0000000000.jpg";
 
-        public static string GetThumbnail(this string path) => $"ms-appx:///Luo Painter.Brushes/Thumbnails/{path}/Thumbnail.png";
-        public static string GetThumbnailRender(this string path) => $"ms-appx:///Luo Painter.Brushes/Thumbnails/{path}/Render.png";
+        public static string GetBrush(this string path) => $"ms-appx:///Luo Painter.Brushes/Brushes/{path}Brush.png";
+        public static string GetBrushSource(this string path) => $"ms-appx:///Luo Painter.Brushes/Brushes/{path}.png";
 
-        public static string GetTexture(this string path) => $@"ms-appx:///Luo Painter.Brushes/Textures/{path}/Texture.png";
-        public static string GetTextureSource(this string path) => $@"Luo Painter.Brushes/Textures/{path}/Source.png";
+        public static string GetTexture(this string path) => $@"ms-appx:///Luo Painter.Brushes/Textures/{path}Texture.png";
+        public static string GetTextureSource(this string path) => $@"Luo Painter.Brushes/Textures/{path}.png";
     }
 }
