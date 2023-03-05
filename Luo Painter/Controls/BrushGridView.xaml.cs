cf304ac6a45d4a5476e2163af57d917ccd152a3f
ysdy44 <ysdy44@outlook.com>
2023-03-05 22:13:53 +0800

    Add RetouchBrush

--- Luo Painter/Controls/BrushGridView.xaml.cs
+++ Luo Painter/Controls/BrushGridView.xaml.cs
@@ -1,24 +1,30 @@
 ﻿using Luo_Painter.Brushes;
 using System.Collections.Generic;
 using System.Linq;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
 
 namespace Luo_Painter.Controls
 {
     internal enum InkGroupingType
     {
-        Erase,
-        Paint,
-        Fx,
-        Others,
-    }
+        Erase = 8,
+        Paint = 16,
+        Fx = 32,
+        Others = 64,
 
+        Flash = 1 | Others,
+        Splodge = 2 | Others,
+        Scratch = 3 | Others,
+    }
     internal sealed class InkGroupingList : List<InkGrouping>, IList<InkGrouping> { }
     internal class InkGrouping : List<InkAttributes>, IList<InkAttributes>, IGrouping<InkGroupingType, InkAttributes>
     {
         public InkGroupingType Key { set; get; }
-        public string KeyString => App.Resource.GetString($"Brush_{this.Key}");
+        public string KeyString => App.Resource.GetString($"Brush_{(this.Key.HasFlag(InkGroupingType.Others) ? InkGroupingType.Others : this.Key)}");
     }
-    internal sealed class PaintBrush : InkAttributes
+
+    internal class BrushBase : InkAttributes
     {
         public double Size2 { get => base.Size; set => base.Size = (float)value; }
         public double Opacity2 { get => base.Opacity; set => base.Opacity = (float)value; }
@@ -28,9 +34,38 @@ namespace Luo_Painter.Controls
 
         public double MinSize2 { get => base.MinSize; set => base.MinSize = (float)value; }
         public double MinFlow2 { get => base.MinFlow; set => base.MinFlow = (float)value; }
-
+    }
+    internal sealed class PaintBrush : BrushBase
+    {
         public string ImageSource => base.Tile == default ? BrushExtensions.DeaultTile : base.Tile.GetTile();
         public int Tile2 { get => (int)base.Tile; set => base.Tile = (uint)value; }
+    }
+    internal sealed class RetouchBrush : BrushBase
+    {
+        public string Path { get; set; }
+        public string Brush => this.Path.GetBrush();
+        public string BrushSource => this.Path.GetBrushSource();
+
+        public string Title { get; set; }
+        public string Subtitle => ((int)this.Size).ToString();
+    }
+
+    internal class InkTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate PaintTemplate { get; set; }
+        public DataTemplate RetouchTemplate { get; set; }
+        protected override DataTemplate SelectTemplateCore(object item)
+        {
+            if (item is PaintBrush)
+            {
+                return this.PaintTemplate;
+            }
+            else if (item is RetouchBrush)
+            {
+                return this.RetouchTemplate;
+            }
+            else return null;
+        }
     }
 
     public sealed partial class BrushGridView : XamlGridView
