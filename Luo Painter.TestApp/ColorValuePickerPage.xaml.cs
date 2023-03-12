2a5ceb064b79f8dfe14fd80724bee06c366f4ba8
ysdy44 <ysdy44@outlook.com>
2023-03-12 13:36:13 +0800

    Fix bugs

--- Luo Painter.TestApp/ColorValuePickerPage.xaml.cs
+++ Luo Painter.TestApp/ColorValuePickerPage.xaml.cs
@@ -25,11 +25,7 @@ namespace Luo_Painter.TestApp
             base.Unloaded += (s, e) => CompositionTarget.SurfaceContentsLost -= this.SurfaceContentsLost;
             base.Loaded += (s, e) => CompositionTarget.SurfaceContentsLost += this.SurfaceContentsLost;
             
-            this.RGBPicker.Recolor(Colors.DodgerBlue);
-            this.HSVPicker.Recolor(Colors.DodgerBlue);
             this.HexPicker.Recolor(Colors.DodgerBlue);
-            this.RGBPicker.ColorChanged += (s, e) => this.SolidColorBrush.Color = e;
-            this.HSVPicker.ColorChanged += (s, e) => this.SolidColorBrush.Color = e;
             this.HexPicker.ColorChanged += (s, e) => this.SolidColorBrush.Color = e;
         }
         private void SurfaceContentsLost(object sender, object e)
