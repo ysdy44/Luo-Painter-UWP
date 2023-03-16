01adc3b4291ab2439bcdff01ccf591917126cdb2
ysdy44 <ysdy44@outlook.com>
2023-03-17 00:01:21 +0800

    Fix BrushEasePressure

--- Luo Painter.Brushes/BrushEasePressure.cs
+++ Luo Painter.Brushes/BrushEasePressure.cs
@@ -3,33 +3,33 @@
     /// <summary>
     /// Pressure of the brush easing.
     /// </summary>
-    public enum BrushEasePressure : byte
+    public enum BrushEasePressure : int
     {
         /// <summary> f (x) = 1 </summary>
-        None,
+        None = 0,
 
         /// <summary> f (x) = x </summary>
-        Linear,
+        Linear = 1,
         /// <summary> f (x) = 1 - x </summary>
-        LinearFlip,
+        LinearFlip = 2,
 
         /// <summary> f (x) = x * x </summary>
-        Quadratic,
+        Quadratic = 3,
         /// <summary> f (x) = 1 - x * x </summary>
-        QuadraticFlip,
+        QuadraticFlip = 4,
         /// <summary> f (x) = (1 - x) * (1 - x) </summary>
-        QuadraticReverse,
+        QuadraticReverse = 5,
         /// <summary> f (x) = 1 - (1 - x) * (1 - x) </summary>
-        QuadraticFlipReverse,
+        QuadraticFlipReverse = 6,
 
         /// <summary> f (x) = (x / 2 - 1) * (x / 2 - 1) </summary>
-        Mirror,
+        Mirror = 7,
         /// <summary> f (x) = 1 - (x / 2 - 1) * (x / 2 - 1) </summary>
-        MirrorFlip,
+        MirrorFlip = 8,
 
         /// <summary> 2f (x) = 2x * 2x </summary>
-        Symmetry,
+        Symmetry = 9,
         /// <summary> 2f (x) = 1 - 2x * 2x </summary>
-        SymmetryFlip,
+        SymmetryFlip = 10,
     }
 }
