a2b070bd275e79afed9105980a7256d3d234016b
ysdy44 <ysdy44@outlook.com>
2023-03-17 00:06:38 +0800

    BrushPage UI

--- Luo Painter/$BrushPages/BrushPage.Canvas.cs
+++ Luo Painter/$BrushPages/BrushPage.Canvas.cs
@@ -12,6 +12,20 @@ namespace Luo_Painter
 
         private void ConstructCanvas()
         {
+            this.Canvas.SizeChanged += (s, e) =>
+            {
+                if (e.NewSize == Size.Empty) return;
+                if (e.NewSize == e.PreviousSize) return;
+                if (this.AlignmentGrid.RebuildWithInterpolation(e.NewSize) is false) return;
+
+                this.CanvasControl.Width = e.NewSize.Width;
+                this.CanvasControl.Height = e.NewSize.Height;
+
+                Vector2 size = this.CanvasControl.Dpi.ConvertDipsToPixels(e.NewSize.ToVector2());
+                this.CreateResources((int)size.X, (int)size.Y);
+            };
+
+
             this.CanvasControl.CreateResources += (sender, args) =>
             {
                 float width = sender.Dpi.ConvertDipsToPixels((float)sender.ActualWidth);
