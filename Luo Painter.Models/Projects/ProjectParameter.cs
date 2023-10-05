eeb408c56e9d0cafcdc0e03a70ac795512f4ee27
ysdy44 <ysdy44@outlook.com>
2023-10-05 19:55:09 +0800

    File Add: ProjectParameterType

--- Luo Painter.Models/Projects/ProjectParameter.cs
+++ Luo Painter.Models/Projects/ProjectParameter.cs
@@ -4,23 +4,6 @@ using Windows.Storage.Streams;
 
 namespace Luo_Painter.Models
 {
-    public enum ProjectParameterType
-    {
-        NewProject,
-
-        /// <summary>
-        /// <see cref="ProjectParameter.Bitmap"/>
-        /// </summary>
-        NewImage,
-
-        /// <summary>
-        /// <see cref="ProjectParameter.DocProject"/>
-        /// <see cref="ProjectParameter.DocLayers"/>
-        /// <see cref="ProjectParameter.Bitmaps"/>
-        /// <see cref="ProjectParameter.Photos"/>
-        /// </summary>
-        OpenProject
-    }
 
     public class ProjectParameter
     {
---
+++ Luo Painter.Models/Projects/ProjectParameterType.cs
@@ -0,0 +1,20 @@
+﻿namespace Luo_Painter.Models
+{
+    public enum ProjectParameterType
+    {
+        NewProject,
+
+        /// <summary>
+        /// <see cref="ProjectParameter.Bitmap"/>
+        /// </summary>
+        NewImage,
+
+        /// <summary>
+        /// <see cref="ProjectParameter.DocProject"/>
+        /// <see cref="ProjectParameter.DocLayers"/>
+        /// <see cref="ProjectParameter.Bitmaps"/>
+        /// <see cref="ProjectParameter.Photos"/>
+        /// </summary>
+        OpenProject
+    }
+}
