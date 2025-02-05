44dd2fce3b2aa81da5c6e8e9496f0bb515dd9916
ysdy44 <ysdy44@outlook.com>
2025-02-06 00:45:28 +0800

    Fix UI Number Picker for GammaTransfer

--- Luo Painter/$DrawPage.Effect/DrawPage.Effect.GammaTransfer.cs
+++ Luo Painter/$DrawPage.Effect/DrawPage.Effect.GammaTransfer.cs
@@ -29,21 +29,21 @@ namespace Luo_Painter
                 this.GTAA = (float)Math.Clamp(e.NewValue / 100, 0, 1);
                 this.CanvasVirtualControl.Invalidate(); // Invalidate
             };
-            this.GTAASlider.Click += (s, e) => this.NumberShowAt(this.GTAASlider, (NumberPickerMode)0);
+            this.GTAASlider.Click += (s, e) => this.NumberShowAt(this.GTAASlider, NumberPickerMode.GTAASlider);
 
             this.GTEASlider.ValueChanged += (s, e) =>
             {
                 this.GTEA = (float)Math.Clamp(e.NewValue / 100, 0, 1);
                 this.CanvasVirtualControl.Invalidate(); // Invalidate
             };
-            this.GTEASlider.Click += (s, e) => this.NumberShowAt(this.GTEASlider, (NumberPickerMode)1);
+            this.GTEASlider.Click += (s, e) => this.NumberShowAt(this.GTEASlider, NumberPickerMode.GTEASlider);
 
             this.GTOASlider.ValueChanged += (s, e) =>
             {
                 this.GTOA = (float)Math.Clamp(e.NewValue / 100, 0, 1);
                 this.CanvasVirtualControl.Invalidate(); // Invalidate
             };
-            this.GTOASlider.Click += (s, e) => this.NumberShowAt(this.GTOASlider, (NumberPickerMode)2);
+            this.GTOASlider.Click += (s, e) => this.NumberShowAt(this.GTOASlider, NumberPickerMode.GTOASlider);
 
 
 
@@ -52,21 +52,21 @@ namespace Luo_Painter
                 this.GTAR = (float)Math.Clamp(e.NewValue / 100, 0, 1);
                 this.CanvasVirtualControl.Invalidate(); // Invalidate
             };
-            this.GTARSlider.Click += (s, e) => this.NumberShowAt(this.GTARSlider, (NumberPickerMode)3);
+            this.GTARSlider.Click += (s, e) => this.NumberShowAt(this.GTARSlider, NumberPickerMode.GTARSlider);
 
             this.GTERSlider.ValueChanged += (s, e) =>
             {
                 this.GTER = (float)Math.Clamp(e.NewValue / 100, 0, 1);
                 this.CanvasVirtualControl.Invalidate(); // Invalidate
             };
-            this.GTERSlider.Click += (s, e) => this.NumberShowAt(this.GTERSlider, (NumberPickerMode)4);
+            this.GTERSlider.Click += (s, e) => this.NumberShowAt(this.GTERSlider, NumberPickerMode.GTERSlider);
 
             this.GTORSlider.ValueChanged += (s, e) =>
             {
                 this.GTOR = (float)Math.Clamp(e.NewValue / 100, 0, 1);
                 this.CanvasVirtualControl.Invalidate(); // Invalidate
             };
-            this.GTORSlider.Click += (s, e) => this.NumberShowAt(this.GTORSlider, (NumberPickerMode)5);
+            this.GTORSlider.Click += (s, e) => this.NumberShowAt(this.GTORSlider, NumberPickerMode.GTORSlider);
 
 
 
@@ -75,21 +75,21 @@ namespace Luo_Painter
                 this.GTAG = (float)Math.Clamp(e.NewValue / 100, 0, 1);
                 this.CanvasVirtualControl.Invalidate(); // Invalidate
             };
-            this.GTAGSlider.Click += (s, e) => this.NumberShowAt(this.GTAGSlider, (NumberPickerMode)6);
+            this.GTAGSlider.Click += (s, e) => this.NumberShowAt(this.GTAGSlider, NumberPickerMode.GTAGSlider);
 
             this.GTEGSlider.ValueChanged += (s, e) =>
             {
                 this.GTEG = (float)Math.Clamp(e.NewValue / 100, 0, 1);
                 this.CanvasVirtualControl.Invalidate(); // Invalidate
             };
-            this.GTEGSlider.Click += (s, e) => this.NumberShowAt(this.GTEGSlider, (NumberPickerMode)7);
+            this.GTEGSlider.Click += (s, e) => this.NumberShowAt(this.GTEGSlider, NumberPickerMode.GTEGSlider);
 
             this.GTOGSlider.ValueChanged += (s, e) =>
             {
                 this.GTOG = (float)Math.Clamp(e.NewValue / 100, 0, 1);
                 this.CanvasVirtualControl.Invalidate(); // Invalidate
             };
-            this.GTOGSlider.Click += (s, e) => this.NumberShowAt(this.GTOGSlider, (NumberPickerMode)8);
+            this.GTOGSlider.Click += (s, e) => this.NumberShowAt(this.GTOGSlider, NumberPickerMode.GTOGSlider);
 
 
 
@@ -98,64 +98,64 @@ namespace Luo_Painter
                 this.GTAB = (float)Math.Clamp(e.NewValue / 100, 0, 1);
                 this.CanvasVirtualControl.Invalidate(); // Invalidate
             };
-            this.GTABSlider.Click += (s, e) => this.NumberShowAt(this.GTABSlider, (NumberPickerMode)9);
+            this.GTABSlider.Click += (s, e) => this.NumberShowAt(this.GTABSlider, NumberPickerMode.GTABSlider);
 
             this.GTEBSlider.ValueChanged += (s, e) =>
             {
                 this.GTEB = (float)Math.Clamp(e.NewValue / 100, 0, 1);
                 this.CanvasVirtualControl.Invalidate(); // Invalidate
             };
-            this.GTEBSlider.Click += (s, e) => this.NumberShowAt(this.GTEBSlider, (NumberPickerMode)10);
+            this.GTEBSlider.Click += (s, e) => this.NumberShowAt(this.GTEBSlider, NumberPickerMode.GTEBSlider);
 
             this.GTOBSlider.ValueChanged += (s, e) =>
             {
                 this.GTOB = (float)Math.Clamp(e.NewValue / 100, 0, 1);
                 this.CanvasVirtualControl.Invalidate(); // Invalidate
             };
-            this.GTOBSlider.Click += (s, e) => this.NumberShowAt(this.GTOBSlider, (NumberPickerMode)11);
+            this.GTOBSlider.Click += (s, e) => this.NumberShowAt(this.GTOBSlider, NumberPickerMode.GTOBSlider);
         }
 
         private void SetGammaTransfer(NumberPickerMode mode, float e)
         {
-            switch ((int)mode)
+            switch (mode)
             {
-                case 0:
+                case NumberPickerMode.GTAASlider:
                     this.GTAASlider.Value = e;
                     break;
-                case 1:
+                case NumberPickerMode.GTEASlider:
                     this.GTEASlider.Value = e;
                     break;
-                case 2:
+                case NumberPickerMode.GTOASlider:
                     this.GTOASlider.Value = e;
                     break;
 
-                case 3:
+                case NumberPickerMode.GTARSlider:
                     this.GTARSlider.Value = e;
                     break;
-                case 4:
+                case NumberPickerMode.GTERSlider:
                     this.GTERSlider.Value = e;
                     break;
-                case 5:
+                case NumberPickerMode.GTORSlider:
                     this.GTORSlider.Value = e;
                     break;
 
-                case 6:
+                case NumberPickerMode.GTAGSlider:
                     this.GTAGSlider.Value = e;
                     break;
-                case 7:
+                case NumberPickerMode.GTEGSlider:
                     this.GTEGSlider.Value = e;
                     break;
-                case 8:
+                case NumberPickerMode.GTOGSlider:
                     this.GTOGSlider.Value = e;
                     break;
 
-                case 9:
+                case NumberPickerMode.GTABSlider:
                     this.GTABSlider.Value = e;
                     break;
-                case 10:
+                case NumberPickerMode.GTEBSlider:
                     this.GTEBSlider.Value = e;
                     break;
-                case 11:
+                case NumberPickerMode.GTOBSlider:
                     this.GTOBSlider.Value = e;
                     break;
 
