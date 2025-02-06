b4524e4a77898d1eb834ba75b250f6259a2cf652
ysdy44 <ysdy44@outlook.com>
2025-02-06 16:18:13 +0800

    Add InkTouchMode

---
+++ Luo Painter/UI/InkTouchMode.cs
@@ -0,0 +1,20 @@
+﻿using FanKit.Transformers;
+
+namespace Luo_Painter.UI
+{
+    public enum InkTouchMode
+    {
+        /// <summary>
+        /// <see cref="CanvasOperator.TouchMode"/> is <see cref="TouchMode.SingleFinger"/>
+        /// </summary>
+        Draw = 0,
+        /// <summary>
+        /// <see cref="CanvasOperator.TouchMode"/> is <see cref="TouchMode.RightButton"/>
+        /// </summary>
+        Move = 1,
+        /// <summary>
+        /// <see cref="CanvasOperator.TouchMode"/> is <see cref="TouchMode.Disable"/>
+        /// </summary>
+        Disable = 2,
+    }
+}
